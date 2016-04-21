98ba3a9c3844dfe94975527fdf396b10b6f1cd06
luics <luics.king@gmail.com>
2016-04-21 23:07:02 +0800

    All feature branchhook (#41)
    + [all] use pre-push hook to validate branch name

--- bin/commit-msg.sh
+++ bin/commit-msg.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
+
+# ISSUE merge
 commit_regex='^Merge.+|[+*-] \[android|ios|jsfm|html5|doc|website|example|test|all\] [^\n]{1,50}'
 
 if ! grep -iqE "$commit_regex" "$1"; then
-    echo "ERROR: commit log format is not correct! Please use this format:
-
-{action} [{module}] {description}
-
-    {action}:
-        + add
-        * update or bugfix
-        - remove
-    {module}
-        Including: android, ios, jsfm, html5, doc, website, example, test, all
-
-for example:
-    + [android] add refreshing for WebView
-    * [doc] update video auto-play property
-    - [example] remove abc
+    echo "\nERROR: commit log format is not correct!
         
-See https://github.com/alibaba/weex/blob/dev/CONTRIBUTING.md#commit-log"
+See https://github.com/alibaba/weex/blob/dev/CONTRIBUTING.md#commit-log\n"
 
     exit 1
 fi
