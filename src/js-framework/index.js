61eabbd20d13fc9c73800fbd840d9108d00608bb
terrykingcha <terrykingcha@gmail.com>
2016-04-22 12:27:20 +0800

    + [jsfm] upgrade transform && log the erros

--- src/js-framework/index.js
+++ src/js-framework/index.js
@@ -2,7 +2,23 @@ import './polyfill'
 import * as framework from './lib/framework'
 import {version, optionalDependencies} from './package.json'
 
-Object.assign(global, framework, {
+for (let methodName in framework) {
+    global[methodName] = function (...args) {
+        const ret = framework[methodName](...args)
+        if (ret instanceof Error) {
+            console.error(ret.toString())
+        } else (ret instanceof Array) {
+            ret.forEach(r => {
+                if (ret instanceof Error) {
+                    console.error(r.toString())
+                }
+            })
+        }
+        return ret
+    }
+}
+
+Object.assign(global, {
     frameworkVersion: version,
     needTransformerVersion: optionalDependencies['weex-transformer']
 })
