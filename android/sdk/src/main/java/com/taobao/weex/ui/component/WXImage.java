4780b0655c3801985744e89e3942490b4a9b9269
xkli <569664668@qq.com>
2016-05-09 11:41:27 +0800

    * [android] update WXimage.java add new property

--- android/sdk/src/main/java/com/taobao/weex/ui/component/WXImage.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/WXImage.java
@@ -324,6 +324,12 @@ public class WXImage extends WXComponent {
       scaleType = ScaleType.CENTER_INSIDE;
     } else if (resizeMode.equals("stretch")) {
       scaleType = ScaleType.FIT_XY;
+    }else if (resizeMode.equals("center")){
+      scaleType = ScaleType.CENTER;
+    }else if(resizeMode.equals("start")){
+      scaleType = ScaleType.FIT_START;
+    }else if(resizeMode.equals("end")){
+      scaleType = ScaleType.FIT_END;
     }
     return scaleType;
   }
