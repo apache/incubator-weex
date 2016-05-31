551b9e9741dcd94e2f3f17bcee8f1cf54623e519
xkli <569664668@qq.com>
2016-05-31 14:50:42 +0800

    * [android] update rename package

--- android/playground/app/src/main/java/com/alibaba/weex/WXApplication.java
+++ android/playground/app/src/main/java/com/alibaba/weex/WXApplication.java
@@ -2,11 +2,10 @@ package com.alibaba.weex;
 
 import android.app.Application;
 
-import com.alibaba.weex.extend.Components.WTRichText;
-import com.alibaba.weex.extend.ImageAdapter;
-import com.alibaba.weex.extend.Modules.RenderModule;
-import com.alibaba.weex.extend.Modules.WXEventModule;
-import com.taobao.weex.WXEnvironment;
+import com.alibaba.weex.extend.components.WTRichText;
+import com.alibaba.weex.extend.adapter.ImageAdapter;
+import com.alibaba.weex.extend.modules.RenderModule;
+import com.alibaba.weex.extend.modules.WXEventModule;
 import com.taobao.weex.WXSDKEngine;
 import com.taobao.weex.common.WXException;
 
