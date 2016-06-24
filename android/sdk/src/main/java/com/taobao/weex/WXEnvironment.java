2dbd97c8d3070b6c9ec77297f8d8bab91e925c6b
xkli <569664668@qq.com>
2016-06-24 10:15:58 +0800

    * [android] update 1.5.2.2

--- android/sdk/src/main/java/com/taobao/weex/WXEnvironment.java
+++ android/sdk/src/main/java/com/taobao/weex/WXEnvironment.java
@@ -231,7 +231,7 @@ public class WXEnvironment {
 
   public static String JS_LIB_SDK_VERSION = "v0.13.10";
 
-  public static String WXSDK_VERSION = "1.5.1.3";
+  public static String WXSDK_VERSION = "1.5.2.2";
   public static Application sApplication;
   public static final String DEV_Id = getDevId();
   public static int sDefaultWidth = 750;
