5b38e44215764c35e37eee44df97e7fe98d5be14
xkli <569664668@qq.com>
2016-06-26 10:22:38 +0800

    current ip

--- android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
+++ android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
@@ -30,7 +30,7 @@ public class IndexActivity extends AbstractWeexActivity {
 
   private static final String TAG = "IndexActivity";
   private static final String DEFAULT_IP = "your_current_IP";
-  private static String CURRENT_IP= "30.10.216.8"; // your_current_IP
+  private static String CURRENT_IP= DEFAULT_IP; // your_current_IP
   private static final String WEEX_INDEX_URL = "http://"+CURRENT_IP+":12580/examples/build/index.js";
 
   private ProgressBar mProgressBar;
