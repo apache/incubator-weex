aff0b0acd07749e664e43a5157940a653f0a6dea
MrRaindrop <tekkahs@gmail.com>
2016-06-27 15:43:51 +0800

    * [html5] cp subversion to dist version

--- build/webpack.browser.config.js
+++ build/webpack.browser.config.js
@@ -1,11 +1,21 @@
+var fs = require('fs')
+var path = require('path')
 var webpack = require('webpack')
+
 var pkg = require('../package.json')
+var basePath = '../dist/weex-html5'
+var distPkgPath = path.join(__dirname, basePath, 'package.json')
+var distPkg = require(distPkgPath)
 
 var sourceMapPlugin = new webpack.SourceMapDevToolPlugin({
   test: /\.js$/
 })
 
 var version = pkg.subversion.browser
+
+distPkg.version = version
+fs.writeFileSync(distPkgPath, JSON.stringify(distPkg, null, 2))
+
 var date = new Date().toISOString().split('T')[0].replace(/\-/g, '')
 var banner = `(this.nativeLog || function(s) {console.log(s)})` +
   `('START WEEX HTML5: ${version} Build ${date}');`;
