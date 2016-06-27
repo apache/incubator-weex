fe47f1da510edf3728afb8ed0ca588237ea5af4e
MrRaindrop <tekkahs@gmail.com>
2016-06-27 15:23:06 +0800

    * [html5] add commonjs wrapper

--- bin/dist-browser.sh
+++ bin/dist-browser.sh
@@ -4,7 +4,7 @@ echo ''
 echo ''
 echo 'packing weex-html5 for distribution...'
 
-src_dir=./html5
+# src_dir=./html5
 
 base_dir=./dist/weex-html5
 dist_dir=${base_dir}/dist
@@ -16,11 +16,13 @@ fi
 mkdir $dist_dir
 
 cp ./dist/browser.js ${dist_dir}/weex.js
+cp ${dist_dir}/weex.js ${dist_dir}/weex.common.js
+echo -e "\nmodule.exports = global.weex;" >> ${dist_dir}/weex.common.js
 
 uglifyjs ${dist_dir}/weex.js -o ${dist_dir}/weex.min.js
 
-rm -rf ${base_dir}/src
-cp -fR ${src_dir} ${base_dir}/src
+# rm -rf ${base_dir}/src
+# cp -fR ${src_dir} ${base_dir}/src
 
 echo 'packing finished.'
 echo ''
