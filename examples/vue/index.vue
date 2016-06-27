5c1c92392c9110cda82d1ddd34c224462c8d418d
Jinjiang <zhaojinjiang@me.com>
2016-06-28 02:42:02 +0800

    + [example] added vue 2.0 examples

---
+++ examples/vue/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <example-list :items="items" dir="examples"></example-list>
+</template>
+
+<script>
+  module.exports = {
+    components: {
+      'example-list': require('./include/example-list.vue')
+    },
+    data: {
+      items: [
+        // `name` key is the example filename without '.vue'
+        {name: 'vue/syntax/hello-world', title: 'Hello World'},
+        {name: 'vue/showcase/itemlist', title: 'List'},
+        {name: 'vue/showcase/calculator', title: 'Calculator'},
+        {name: 'vue/showcase/new-fashion', title: 'Activity Page'}
+      ]
+    }
+  }
+</script>
