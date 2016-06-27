0e89e9e9bcd64e4263d9de3886828926366e4024
MrRaindrop <tekkahs@gmail.com>
2016-06-27 14:36:21 +0800

    * [html5] pack weex-html5 for distribution

---
+++ bin/dist-browser.sh
@@ -0,0 +1,28 @@
+#!/user/bin/env bash
+
+echo ''
+echo ''
+echo 'packing weex-html5 for distribution...'
+
+
+src_dir=./html5
+
+base_dir=./dist/weex-html5
+dist_dir=${base_dir}/dist
+
+if [ -e $dist_dir ]
+	then rm -rf $dist_dir
+fi
+
+mkdir $dist_dir
+
+cp ./dist/browser.js ${dist_dir}/weex.js
+
+uglifyjs ${dist_dir}/weex.js -o ${dist_dir}/weex.min.js
+
+rm -rf ${base_dir}/src
+cp -fR ${src_dir} ${base_dir}/src
+
+echo 'packing finished.'
+echo ''
+echo ''
