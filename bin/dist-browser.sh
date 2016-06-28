a59bd3d4e343ba5a7ec1f1c5b77943808559bf43
MrRaindrop <tekkahs@gmail.com>
2016-06-28 14:19:30 +0800

    * [html5] update pkg main

--- bin/dist-browser.sh
+++ bin/dist-browser.sh
@@ -4,6 +4,7 @@ echo ''
 echo ''
 echo 'packing weex-html5 for distribution...'
 
+# src_dir=./html5
 base_dir=./dist/weex-html5
 dist_dir=${base_dir}/dist
 
@@ -30,6 +31,9 @@ cp ${dist_dir}/weex.js ${dist_dir}/weex.common.js
 echo -e "\nmodule.exports = global.weex;" >> ${dist_dir}/weex.common.js
 
 uglifyjs ${dist_dir}/weex.js -o ${dist_dir}/weex.min.js
+
+# rm -rf ${base_dir}/src
+# cp -fR ${src_dir} ${base_dir}/src
 
 echo 'packing finished.'
 echo ''
