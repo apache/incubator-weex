f6bb2e9bfcfde6c03d15d974c45ffe1d8e85bcbe
boboning <ningli928@163.com>
2016-06-29 16:14:56 +0800

    * [ios] addObserver for instance state

--- ios/sdk/WeexSDK/Sources/View/WXErrorView.h
+++ ios/sdk/WeexSDK/Sources/View/WXErrorView.h
@@ -1,10 +1,10 @@
-//
-//  WXErrorView.h
-//  Pods
-//
-//  Created by bobning on 16/6/29.
-//
-//
+/**
+ * Created by Weex.
+ * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
+ */
 
 #import <UIKit/UIKit.h>
 
