f9d326515394fc7086da900adb6de378de84500b
miomin <691292118@qq.com>
2016-07-06 10:31:18 +0800

    * [android] sticky bug fix

--- android/sdk/src/main/java/com/taobao/weex/ui/component/list/WXListComponent.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/list/WXListComponent.java
@@ -218,12 +218,16 @@ import android.widget.ImageView;
 import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.WXSDKInstance;
 import com.taobao.weex.WXSDKManager;
-import com.taobao.weex.common.Component;
 import com.taobao.weex.common.OnWXScrollListener;
-import com.taobao.weex.common.WXDomPropConstant;
 import com.taobao.weex.common.WXRuntimeException;
 import com.taobao.weex.dom.WXDomObject;
-import com.taobao.weex.ui.component.*;
+import com.taobao.weex.ui.component.Scrollable;
+import com.taobao.weex.ui.component.WXComponent;
+import com.taobao.weex.ui.component.WXEventType;
+import com.taobao.weex.ui.component.WXHeader;
+import com.taobao.weex.ui.component.WXLoading;
+import com.taobao.weex.ui.component.WXRefresh;
+import com.taobao.weex.ui.component.WXVContainer;
 import com.taobao.weex.ui.view.listview.adapter.IOnLoadMoreListener;
 import com.taobao.weex.ui.view.listview.adapter.IRecyclerAdapterListener;
 import com.taobao.weex.ui.view.listview.adapter.ListBaseViewHolder;
@@ -763,16 +767,11 @@ public class WXListComponent extends WXVContainer implements
         for (int i = 0, len = childCount(); i < len; i++) {
           WXComponent value = getChild(i);
           if ((i == firstVisible+1 && directionY <= 0)) {
-            if ((value.getDomObject().style.get(WXDomPropConstant.WX_POSITION) != null && value.getDomObject
-                ().style
-                .get(WXDomPropConstant.WX_POSITION).equals(WXDomPropConstant.WX_POSITION_STICKY)) ||
-                value instanceof WXHeader) {
+            if ((value.getDomObject() != null && value.getDomObject().isSticky()) || value instanceof WXHeader) {
               bounceRecyclerView.onStickyAppear((WXCell) value, i);
             }
           } else if ((i == firstVisible && directionY >= 0)) {
-            if ((value.getDomObject().style.get(WXDomPropConstant.WX_POSITION) != null && value.getDomObject().style
-                .get
-                (WXDomPropConstant.WX_POSITION).equals(WXDomPropConstant.WX_POSITION_STICKY)) || value instanceof WXHeader) {
+            if ((value.getDomObject() != null && value.getDomObject().isSticky()) || value instanceof WXHeader) {
               bounceRecyclerView.onStickyDisappear((WXCell) value, i);
             }
           }
