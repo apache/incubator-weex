f939302540b06abc4edded4a05c36bd9f5bc850c
Jinjiang <zhaojinjiang@me.com>
2016-07-12 15:54:45 +0800

    * [jsfm] added a promies test case

--- html5/shared/promise.js
+++ html5/shared/promise.js
@@ -1,6 +1,6 @@
 // fix Promise Problem on JSContext of iOS7~8
 // @see https://bugs.webkit.org/show_bug.cgi?id=135866
-global.Promise = null
+// global.Promise = null
 require('core-js/modules/es6.object.to-string')
 require('core-js/modules/es6.string.iterator')
 require('core-js/modules/web.dom.iterable')
