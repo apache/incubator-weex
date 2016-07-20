e727482b3ab966876bee3f46b1089eb194d6a948
acton393 <zhangxing610321@gmail.com>
2016-07-20 17:16:42 +0800

    * [ios] add status text  for http status code

--- ios/sdk/WeexSDK/Sources/Module/WXStreamModule.m
+++ ios/sdk/WeexSDK/Sources/Module/WXStreamModule.m
@@ -80,11 +80,9 @@ WX_EXPORT_METHOD(@selector(fetch:callback:progressCallback:))
     NSURL *url = [NSURL URLWithString:urlStr];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPMethod:method];
+    
     if ([options valueForKey:@"timeout"]){
         [request setTimeoutInterval:[[options valueForKey:@"timeout"] floatValue]];
-    } else {
-        //default timeout is 30.0s
-        [request setTimeoutInterval:30.0];
     }
     
     for (NSString *header in headers) {
@@ -109,11 +107,7 @@ WX_EXPORT_METHOD(@selector(fetch:callback:progressCallback:))
                     [callbackRsp setObject:@{ @"HEADERS_RECEIVED" : @2  } forKey:@"readyState"];
                     [callbackRsp setObject:[NSNumber numberWithInteger:httpResponse.statusCode] forKey:@"status"];
                     [callbackRsp setObject:httpResponse.allHeaderFields forKey:@"headers"];
-                    if (httpResponse.statusCode == 200) {
-                        statusText = @"OK";
-                    }else {
-                        statusText = [NSHTTPURLResponse localizedStringForStatusCode:httpResponse.statusCode].capitalizedString;
-                    }
+                    statusText = [self.class getStatusText:httpResponse.statusCode];
                     [callbackRsp setObject:statusText forKey:@"statusText"];
                     [callbackRsp setObject:[NSNumber numberWithInteger:received] forKey:@"length"];
                     
@@ -167,6 +161,143 @@ WX_EXPORT_METHOD(@selector(fetch:callback:progressCallback:))
                     }
                     callback([WXUtility JSONString:callbackRsp]);
                 }];
+}
+
++ (NSString*)getStatusText:(NSInteger)code {
+    
+    switch (code) {
+        case -1:
+            return @"ERR_INVALID_REQUEST";
+        case 100:
+            return @"Continue";
+            break;
+        case 101:
+            return @"Switching Protocol";
+        case 102:
+            return @"Processing";
+            
+        case 200:
+            return @"OK";
+        case 201:
+            return @"Created";
+        case 202:
+            return @"Accepted";
+        case 203:
+            return @"Non-Authoritative Information";
+        case 204:
+            return @"No Content";
+        case 205:
+            return @" Reset Content";
+        case 206:
+            return @"Partial Content";
+        case 207:
+            return @"Multi-Status";
+        case 208:
+            return @"Already Reported";
+        case 226:
+            return @"IM Used";
+            
+        case 300:
+            return @"Multiple Choices";
+        case 301:
+            return @"Moved Permanently";
+        case 302:
+            return @"Found";
+        case 303:
+            return @"See Other";
+        case 304:
+            return @"Not Modified";
+        case 305:
+            return @"Use Proxy";
+        case 306:
+            return @"Switch Proxy";
+        case 307:
+            return @"Temporary Redirect";
+        case 308:
+            return @"Permanent Redirect";
+            
+        case 400:
+            return @"Bad Request";
+        case 401:
+            return @"Unauthorized";
+        case 402:
+            return @"Payment Required";
+        case 403:
+            return @"Forbidden";
+        case 404:
+            return @"Not Found";
+        case 405:
+            return @"Method Not Allowed";
+        case 406:
+            return @"Not Acceptable";
+        case 407:
+            return @"Proxy Authentication Required";
+        case 408:
+            return @"Request Timeout";
+        case 409:
+            return @"Conflict";
+        case 410:
+            return @"Gone";
+        case 411:
+            return @"Length Required";
+        case 412:
+            return @"Precondition Failed";
+        case 413:
+            return @"Payload Too Large";
+        case 414:
+            return @"URI Too Long";
+        case 415:
+            return @"Unsupported Media Type";
+        case 416:
+            return @"Range Not Satisfiable";
+        case 417:
+            return @"Expectation Failed";
+        case 418:
+            return @"I'm a teapot";
+        case 421:
+            return @"Misdirected Request";
+        case 422:
+            return @"Unprocessable Entity";
+        case 423:
+            return @"Locked";
+        case 424:
+            return @"Failed Dependency";
+        case 426:
+            return @"Upgrade Required";
+        case 428:
+            return @"Precondition Required";
+        case 429:
+            return @"Too Many Requests";
+        case 431:
+            return @"Request Header Fields Too Large";
+        case 451:
+            return @"Unavailable For Legal Reasons";
+            
+        case 500:
+            return @"Internal Server Error";
+        case 501:
+            return @"Not Implemented";
+        case 502:
+            return @"Bad Gateway";
+        case 503:
+            return @"Service Unavailable";
+        case 504:
+            return @"Gateway Timeout";
+        case 505:
+            return @"HTTP Version Not Supported";
+        case 506:
+            return @"Variant Also Negotiates";
+        case 507:
+            return @"Insufficient Storage";
+        case 508:
+            return @"Loop Detected";
+        case 510:
+            return @"Not Extended";
+        case 511:
+            return @"Network Authentication Required";
+        default:
+            break;
+    }
 }
 
 @end
