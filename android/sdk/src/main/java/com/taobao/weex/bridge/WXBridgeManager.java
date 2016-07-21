ed30fbc0ef966e7074c293aecf32d3c62a46d0d3
xkli <569664668@qq.com>
2016-07-21 16:24:58 +0800

    * [android] update synchronized

--- android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
+++ android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
@@ -496,7 +496,7 @@ public class WXBridgeManager implements Callback {
   }
 
 
-  private void addJSTask(String method,String instanceId, Object... args) {
+  private synchronized void addJSTask(String method,String instanceId, Object... args) {
     if (args == null || args.length == 0) {
       return;
     }
