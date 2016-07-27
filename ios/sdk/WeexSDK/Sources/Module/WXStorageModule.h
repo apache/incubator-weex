40cebb050ac4db90c7e4cc8d8de25347a9e7c78e
acton393 <zhangxing610321@gmail.com>
2016-07-27 21:17:52 +0800

    * [ios] 1. generate public header 2. image placeholder and image load event

--- ios/sdk/WeexSDK/Sources/Module/WXStorageModule.h
+++ ios/sdk/WeexSDK/Sources/Module/WXStorageModule.h
@@ -6,7 +6,6 @@
  * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
  */
 
-#import <WeexSDK/WeexSDK.h>
 #import <WeexSDK/WXModuleProtocol.h>
 
 @interface WXStorageModule : NSObject <WXModuleProtocol>
