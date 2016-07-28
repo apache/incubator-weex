bd3f86e771443186824846dde81edb451662b006
chuyi <jianjian.sjj@alibaba-inc.com>
2016-07-28 18:44:32 +0800

    * [html5] fix conflicts

---
+++ html5/browser/extend/api/storage.js
@@ -0,0 +1,158 @@
+'use strict'
+
+const supportLocalStorage = typeof localStorage !== 'undefined'
+const logger = require('../logger')
+
+const SUCCESS = 'success'
+const FAILED = 'failed'
+const INVALID_PARAM = 'invalid_param'
+const UNDEFINED = 'undefined'
+
+const storage = {
+
+  /**
+   * When passed a key name and value, will add that key to the storage,
+   * or update that key's value if it already exists.
+   * @param {string} key
+   * @param {string} value
+   * @param {function} callbackId
+   */
+  setItem: function(key,value,callbackId){
+      if(!supportLocalStorage){
+        logger.error('your browser is not support localStorage yet.')
+        return
+      }
+      const sender = this.sender
+      if(!key || !value){
+        sender.performCallback(callbackId,{
+          result:'failed',
+          data:INVALID_PARAM
+        })
+        return
+      }
+      try{
+        localStorage.setItem(key,value)
+        sender.performCallback(callbackId,{
+          result:SUCCESS,
+          data:UNDEFINED
+        })
+      }catch(e){
+        //accept any exception thrown during a storage attempt as a quota error
+        sender.performCallback(callbackId,{
+          result:FAILED,
+          data:UNDEFINED
+        })
+      }
+
+  },
+
+
+  /**
+   * When passed a key name, will return that key's value.
+   * @param {string} key
+   * @param {function} callbackId
+   */
+  getItem: function(key,callbackId){
+    if(!supportLocalStorage){
+      logger.error('your browser is not support localStorage yet.')
+      return
+    }
+    const sender = this.sender
+    if(!key){
+      sender.performCallback(callbackId,{
+        result:FAILED,
+        data:INVALID_PARAM
+      })
+    }
+    let val = localStorage.getItem(key)
+    sender.performCallback(callbackId,{
+        result:val ? SUCCESS:FAILED,
+        data:val ? val:UNDEFINED
+    })
+  },
+
+
+  /**
+   *When passed a key name, will remove that key from the storage.
+   * @param {string} key
+   * @param {function} callbackId
+   */
+  removeItem: function(key,callbackId){
+    if(!supportLocalStorage){
+      logger.error('your browser is not support localStorage yet.')
+      return
+    }
+    const sender = this.sender
+    if(!key){
+      sender.performCallback(callbackId,{
+        result:FAILED,
+        data:INVALID_PARAM
+      })
+    }
+    localStorage.removeItem(key)
+    sender.performCallback(callbackId,{
+      result:SUCCESS,
+      data:UNDEFINED
+    })
+  },
+
+
+  /**
+   * Returns an integer representing the number of data items stored in the Storage object.
+   * @param {function} callbackId
+   */
+  length: function(callbackId){
+    if(!supportLocalStorage){
+      logger.error('your browser is not support localStorage yet.')
+      return
+    }
+    const sender = this.sender
+    let len = localStorage.length
+    sender.performCallback(callbackId,{
+      result:SUCCESS,
+      data:len
+    })
+  },
+
+
+  /**
+   * Returns a array that contains all k-v pairs stored in Storage object.
+   * @param {function} callbackId
+   */
+  getAllKeys: function(callbackId){
+    if(!supportLocalStorage){
+      logger.error('your browser is not support localStorage yet.')
+      return
+    }
+    const sender = this.sender
+    let _arr = []
+    for(let i = 0; i < localStorage.length; i++){
+      _arr.push(localStorage.key(i))
+    }
+    sender.performCallback(callbackId,{
+      result:SUCCESS,
+      data:_arr
+    })
+  }
+}
+
+storage._meta = {
+  storage: [{
+    name:'setItem',
+    args:['string','string','function']
+  }, {
+    name:'getItem',
+    args:['string','function']
+  }, {
+    name:'removeItem',
+    args:['string','function']
+  }, {
+    name:'length',
+    args:['function']
+  }, {
+    name:'getAllKeys',
+    args:['function']
+  }]
+}
+
+module.exports = storage
