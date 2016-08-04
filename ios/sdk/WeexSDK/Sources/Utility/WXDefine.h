ee698d798a8f1b89403804dafa9f188e0c3a2092
Yinfeng <cxfeng1@gmail.com>
2016-08-04 19:52:25 +0800

    * [ios] change jsfm version number

--- ios/sdk/WeexSDK/Sources/Utility/WXDefine.h
+++ ios/sdk/WeexSDK/Sources/Utility/WXDefine.h
@@ -10,7 +10,7 @@
 #define __WX_DEFINE_H__
 
 #define WX_SDK_VERSION @"0.7.0"
-#define WX_JS_FRAMEWORK_VERSION @"0.14.7"
+#define WX_JS_FRAMEWORK_VERSION @"0.15.2"
 
 #if defined(__cplusplus)
 #define WX_EXTERN extern "C" __attribute__((visibility("default")))
