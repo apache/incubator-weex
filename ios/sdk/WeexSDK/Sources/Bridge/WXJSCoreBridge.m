58aad24587b266989592fe088452279f35d419f5
acton393 <zhangxing610321@gmail.com>
2016-08-23 19:48:13 +0800

    * [ios] update appMonitor pageName

--- ios/sdk/WeexSDK/Sources/Bridge/WXJSCoreBridge.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXJSCoreBridge.m
@@ -71,7 +71,7 @@
                         if ([flag isEqualToNumber:[NSNumber numberWithInteger:WXLogFlagWarning]]) {
                             id<WXAppMonitorProtocol> appMonitorHandler = [WXHandlerFactory handlerForProtocol:@protocol(WXAppMonitorProtocol)];
                             if ([appMonitorHandler respondsToSelector:@selector(commitAppMonitorAlarm:monitorPoint:success:errorCode:errorMsg:arg:)]) {
-                                [appMonitorHandler commitAppMonitorAlarm:[WXSDKEngine topInstance].pageName monitorPoint:@"jswarning" success:FALSE errorCode:@"99999" errorMsg:string arg:@"weex"];
+                                [appMonitorHandler commitAppMonitorAlarm:@"weex" monitorPoint:@"jswarning" success:FALSE errorCode:@"99999" errorMsg:string arg:[WXSDKEngine topInstance].pageName];
                             }
                         }
                         WX_LOG([flag unsignedIntegerValue], @"%@", string);
