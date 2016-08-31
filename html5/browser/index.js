ff4cf67eb5754f0e337de516ac7e24229c2dd856
MrRaindrop <tekkahs@gmail.com>
2016-08-31 19:26:33 +0800

    * [html5] downgrade root support loadmore

--- html5/browser/index.js
+++ html5/browser/index.js
@@ -7,11 +7,13 @@ import Weex from './render'
  */
 import root from './base/root'
 import div from './base/div'
+import droot from './base/droot'
 import components from './extend/components'
 import api from './extend/api'
 
 Weex.install(root)
 Weex.install(div)
+Weex.install(droot)
 Weex.install(components)
 Weex.install(api)
 
