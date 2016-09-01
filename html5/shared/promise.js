02711c097e60666ed4ac2a90dd47e18af81413e0
Hanks10100 <zhanghan.me@gmail.com>
2016-09-01 12:13:26 +0800

    * [jsfm] always use Promise polyfill in iOS

--- html5/shared/promise.js
+++ html5/shared/promise.js
@@ -1,10 +1,13 @@
 // fix Promise Problem on JSContext of iOS7~8
 // @see https://bugs.webkit.org/show_bug.cgi?id=135866
-const { WXEnvironment } = global
-if (WXEnvironment && WXEnvironment.platform === 'iOS' &&
-  /^(7|8)\./.test(WXEnvironment.osVersion)) {
-  global.Promise = null
-}
+global.Promise = null
+
+// use iOS native Promise in some version (deprecated)
+// const { WXEnvironment } = global
+// if (WXEnvironment && WXEnvironment.platform === 'iOS' &&
+//   /^(7|8)\./.test(WXEnvironment.osVersion)) {
+//   global.Promise = null
+// }
 require('core-js/modules/es6.object.to-string')
 require('core-js/modules/es6.string.iterator')
 require('core-js/modules/web.dom.iterable')
