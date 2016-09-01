acbc252982e108fc05cdee144af3d994227ccd72
MrRaindrop <tekkahs@gmail.com>
2016-09-01 17:44:22 +0800

    * [html5] add globalEvent api

---
+++ html5/browser/extend/api/globalEvent.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// track varies kinds of events and listeners.
+const handlerTraker = {}
+
+const globalEvent = {
+
+  /**
+   * addEventListener
+   * @param {string} evt - the event name to add a listener on.
+   */
+  addEventListener (evt, callbackId) {
+    // const cb = e => this.sender.performCallback(callbackId, e)
+    const cb = function (e) {
+      debugger;
+      this.sender.performCallback(callbackId, e)
+    }.bind(this)
+    if (!handlerTraker[evt]) {
+      handlerTraker[evt] = [cb]
+    } else {
+      handlerTraker.push(cb)
+    }
+    document.addEventListener(evt, cb)
+  },
+
+  /**
+   * removeEventListener
+   * @param {string} evt - the event name to remove a listener from.
+   */
+  removeEventListener (evt) {
+    handlerTraker[evt].forEach(cb => document.removeEventListener(evt, cb))
+  }
+}
+
+const meta = {
+  globalEvent: [{
+    name: 'addEventListener',
+    args: ['string', 'function']
+  }, {
+    name: 'removeEventListener',
+    args: ['string']
+  }]
+}
+
+export default {
+  init (Weex) {
+    Weex.registerApiModule('globalEvent', globalEvent, meta)
+  }
+}
