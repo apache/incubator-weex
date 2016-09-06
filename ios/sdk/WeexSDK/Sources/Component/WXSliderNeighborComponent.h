295149131e1fbeb8f465a4ddbe82bec3bb849e8d
acton393 <zhangxing610321@gmail.com>
2016-09-06 22:28:07 +0800

    * [ios] replace file header

--- ios/sdk/WeexSDK/Sources/Component/WXSliderNeighborComponent.h
+++ ios/sdk/WeexSDK/Sources/Component/WXSliderNeighborComponent.h
@@ -1,10 +1,10 @@
-//
-//  WXSliderNeighbor.h
-//  WeexSDK
-//
-//  Created by zifan.zx on 8/30/16.
-//  Copyright © 2016 taobao. All rights reserved.
-//
+/**
+ * Created by Weex.
+ * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
+ */
 
 #import <Foundation/Foundation.h>
 #import "WXComponent.h"
