2b29988ac04611636f8b2c586611be411000667d
acton393 <zhangxing610321@gmail.com>
2016-09-09 12:11:26 +0800

    * [ios] replace file header

--- ios/sdk/WeexSDK/Sources/Component/WXCanvasComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXCanvasComponent.m
@@ -1,10 +1,10 @@
-//
-//  WXCanvasComponent.m
-//  WeexSDK
-//
-//  Created by yuankong on 16/6/22.
-//  Copyright © 2016年 taobao. All rights reserved.
-//
+/**
+ * Created by Weex.
+ * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
+ */
 
 #import "WXCanvasComponent.h"
 #import "WXComponent_internal.h"
