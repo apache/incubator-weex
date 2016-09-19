ec5e938b062ae8d562853d7cdc25183cbcc10839
MrRaindrop <tekkahs@gmail.com>
2016-09-19 15:09:34 +0800

    * [html5] support timeout and maximumAge

--- html5/browser/extend/api/geolocation.js
+++ html5/browser/extend/api/geolocation.js
@@ -6,7 +6,8 @@ const errorMsg = `[h5-render]: browser doesn't support geolocation.`
 const geolocation = {
   // options:
   //   - enableHighAccuracy optional, value is true or false, false by default.
-  //   - address optional, value is true or false, false by default
+  //   - timeout [none-native] optional, value is a number (milliseconds), default vaule is FINFINITY.
+  //   - maximumAge [none-native] optional, value is a number (milliseconds), default value is 0.
   getCurrentPosition (successCbId, errorCbId, options) {
     const successCb = pos => this.sender.performCallback(successCbId, pos)
     const errorCb = err => this.sender.performCallback(errorCbId, err)
@@ -19,6 +20,7 @@ const geolocation = {
     }
   },
 
+  // options: the same with `getCurrentPosition`.
   watchPosition (successCbId, errorCbId, options) {
     const successCb = pos => this.sender.performCallback(successCbId, pos, true)
     const errorCb = err => this.sender.performCallback(errorCbId, err)
