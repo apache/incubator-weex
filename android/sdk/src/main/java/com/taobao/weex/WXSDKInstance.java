7412899d3635c75338be9a55cea2630c28a93a29
sospartan <sospartan@gmail.com>
2016-09-21 11:59:15 +0800

    * [android] clear Instance fields when destory

--- android/sdk/src/main/java/com/taobao/weex/WXSDKInstance.java
+++ android/sdk/src/main/java/com/taobao/weex/WXSDKInstance.java
@@ -945,6 +945,9 @@ public class WXSDKInstance implements IWXActivityStateListener {
       mGlobalEvents.clear();
     }
 
+    mNestedInstanceInterceptor = null;
+    mUserTrackAdapter = null;
+    mWXHttpAdapter = null;
     rootView = null;
     mScrollView = null;
     mContext = null;
