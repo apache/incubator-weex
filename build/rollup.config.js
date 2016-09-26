4a1c208cf33fb3e5649f8116133f97b003e78127
Jinjiang <zhaojinjiang@me.com>
2016-09-27 02:32:39 +0800

    * [jsfm] fixed rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,9 +1,16 @@
 import { rollup } from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import json from 'rollup-plugin-json'
 import buble from 'rollup-plugin-buble'
- 
+
+const banner = `var global = this
+var process = { env: {}}
+`
+
 export default {
   entry: './html5/render/native/index.js',
   dest: './dist/native.js',
-  plugins: [ json(), buble() ]
+  banner,
+  plugins: [ nodeResolve({ jsnext: true, main: true }), commonjs(), json(), buble() ]
 }
