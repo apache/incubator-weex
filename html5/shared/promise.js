902d5e02ab741b36d07e5f4039ed1c4394103a9e
Jinjiang <zhaojinjiang@me.com>
2016-10-06 17:41:01 +0800

    * [jsfm] adapted Promise polyfill with Vue 2.0

--- html5/shared/promise.js
+++ html5/shared/promise.js
@@ -2,7 +2,7 @@
 // @see https://bugs.webkit.org/show_bug.cgi?id=135866
 const { WXEnvironment } = global
 if (WXEnvironment && WXEnvironment.platform === 'iOS') {
-  global.Promise = null
+  global.Promise = undefined
 }
 import 'core-js/modules/es6.object.to-string'
 import 'core-js/modules/es6.string.iterator'
