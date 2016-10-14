3d4d2d6b9e5ad9fc59f6b1ac2abe364f710442a7
acton393 <zhangxing610321@gmail.com>
2016-10-14 17:54:51 +0800

    Revert "* [ios] debug performance"
    This reverts commit ae3d30802681990e79ce06fb4229b74f3c1d99c2.

--- ios/sdk/WeexSDK/Sources/Component/WXTextComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXTextComponent.m
@@ -61,10 +61,6 @@
     }
     
     if (!layoutManager || !textContainer) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Tip" message:@"text 为nil" delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles:nil];
-            [alert show];
-        });
         return nil;
     }
     
