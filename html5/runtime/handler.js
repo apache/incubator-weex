0c2bac16fbc03c2859e6980534c0fac9c721cab5
Hanks10100 <zhanghan.me@gmail.com>
2016-10-19 21:53:08 +0800

    * [jsfm] separate the operation of the dispatch task into a function

--- html5/runtime/handler.js
+++ html5/runtime/handler.js
@@ -1,27 +1,61 @@
 
+const handlerMap = {
+  addElement: 'callAddElement'
+}
+
+/**
+ * Create task handler
+ * @param {string} id
+ * @param {function} handler
+ * @return {function} taskHandler
+ */
 export function createHandler (id, handler) {
-  // @todo: The `callAddElement` API should be re-design immediately
-  // because its public and global and without config opportunity.
   const defaultHandler = handler || callNative
-  const hasAddElementHandler = typeof callAddElement === 'function'
 
   return function taskHandler (tasks) {
     if (!Array.isArray(tasks)) {
       tasks = [tasks]
     }
     for (let i = 0; i < tasks.length; i++) {
-      const task = tasks[i]
-      let returnValue
-      if (hasAddElementHandler && task.module === 'dom' && task.method === 'addElement') {
-        const [ref, json, index] = task.args
-        returnValue = callAddElement(id, ref, json, index, '-1')
-      }
-      else {
-        returnValue = defaultHandler(id, [task], '-1')
-      }
+      const returnValue = dispatchTask(id, tasks[i], defaultHandler)
       if (returnValue === -1) {
         return returnValue
       }
     }
   }
+}
+
+/**
+ * Check if there is a corresponding available handler in the environment
+ * @param {string} module
+ * @param {string} method
+ * @return {boolean}
+ */
+function hasAvailableHandler (module, method) {
+  return module === 'dom'
+    && handlerMap[method]
+    && typeof global[handlerMap[method]] === 'function'
+}
+
+/**
+ * Dispatch the task to the specified handler
+ * @param {string} id
+ * @param {object} task
+ * @param {function} defaultHandler
+ * @return {number} signal returned from native
+ */
+function dispatchTask (id, task, defaultHandler) {
+  const { module, method, args } = task
+
+  if (hasAvailableHandler(module, method)) {
+    switch (method) {
+      case 'addElement':
+        return callAddElement(id, args[0], args[1], args[2], '-1')
+
+      default:
+        console.error(`[JS Runtime] "${method}" has no corresponding handler`)
+    }
+  }
+
+  return defaultHandler(id, [task], '-1')
 }
