a66bfdb5dbd4a5be155b5fb0899564c01c3aea5c
Hanks10100 <zhanghan.me@gmail.com>
2016-10-22 15:40:12 +0800

    * [jsfm] use extend() to add prototype methods

--- html5/runtime/listener.js
+++ html5/runtime/listener.js
@@ -1,3 +1,5 @@
+import { extend } from './vdom/operation'
+
 export default function Listener (id, handler) {
   this.id = id
   this.batched = false
@@ -10,97 +12,99 @@ export default function Listener (id, handler) {
   }
 }
 
-Listener.prototype.createFinish = function (callback) {
-  const handler = this.handler
-  return handler([createAction('createFinish')], callback)
-}
-
-Listener.prototype.updateFinish = function (callback) {
-  const handler = this.handler
-  return handler([createAction('updateFinish')], callback)
-}
-
-Listener.prototype.refreshFinish = function (callback) {
-  const handler = this.handler
-  return handler([createAction('refreshFinish')], callback)
-}
-
-Listener.prototype.createBody = function (element) {
-  const body = element.toJSON()
-  const children = body.children
-  delete body.children
-  const actions = [createAction('createBody', [body])]
-  if (children) {
-    actions.push.apply(actions, children.map(child => {
-      return createAction('addElement', [body.ref, child, -1])
-    }))
-  }
-  return this.addActions(actions)
-}
-
-Listener.prototype.addElement = function (element, ref, index) {
-  if (!(index >= 0)) {
-    index = -1
-  }
-  return this.addActions(createAction('addElement', [ref, element.toJSON(), index]))
-}
-
-Listener.prototype.removeElement = function (ref) {
-  if (Array.isArray(ref)) {
-    const actions = ref.map((r) => createAction('removeElement', [r]))
-    return this.addActions(actions)
-  }
-  return this.addActions(createAction('removeElement', [ref]))
-}
-
-Listener.prototype.moveElement = function (targetRef, parentRef, index) {
-  return this.addActions(createAction('moveElement', [targetRef, parentRef, index]))
-}
-
-Listener.prototype.setAttr = function (ref, key, value) {
-  const result = {}
-  result[key] = value
-  return this.addActions(createAction('updateAttrs', [ref, result]))
-}
-
-Listener.prototype.setStyle = function (ref, key, value) {
-  const result = {}
-  result[key] = value
-  return this.addActions(createAction('updateStyle', [ref, result]))
-}
-
-Listener.prototype.setStyles = function (ref, style) {
-  return this.addActions(createAction('updateStyle', [ref, style]))
-}
-
-Listener.prototype.addEvent = function (ref, type) {
-  return this.addActions(createAction('addEvent', [ref, type]))
-}
-
-Listener.prototype.removeEvent = function (ref, type) {
-  return this.addActions(createAction('removeEvent', [ref, type]))
-}
-
-Listener.prototype.handler = function (actions, cb) {
-  return cb && cb()
-}
-
-Listener.prototype.addActions = function (actions) {
-  const updates = this.updates
-  const handler = this.handler
-
-  if (!Array.isArray(actions)) {
-    actions = [actions]
-  }
-
-  if (this.batched) {
-    updates.push.apply(updates, actions)
-  }
-  else {
-    return handler(actions)
-  }
-}
-
 export function createAction (name, args = []) {
   return { module: 'dom', method: name, args: args }
 }
+
+extend(Listener.prototype, {
+  createFinish (callback) {
+    const handler = this.handler
+    return handler([createAction('createFinish')], callback)
+  },
+
+  updateFinish (callback) {
+    const handler = this.handler
+    return handler([createAction('updateFinish')], callback)
+  },
+
+  refreshFinish (callback) {
+    const handler = this.handler
+    return handler([createAction('refreshFinish')], callback)
+  },
+
+  createBody (element) {
+    const body = element.toJSON()
+    const children = body.children
+    delete body.children
+    const actions = [createAction('createBody', [body])]
+    if (children) {
+      actions.push.apply(actions, children.map(child => {
+        return createAction('addElement', [body.ref, child, -1])
+      }))
+    }
+    return this.addActions(actions)
+  },
+
+  addElement (element, ref, index) {
+    if (!(index >= 0)) {
+      index = -1
+    }
+    return this.addActions(createAction('addElement', [ref, element.toJSON(), index]))
+  },
+
+  removeElement (ref) {
+    if (Array.isArray(ref)) {
+      const actions = ref.map((r) => createAction('removeElement', [r]))
+      return this.addActions(actions)
+    }
+    return this.addActions(createAction('removeElement', [ref]))
+  },
+
+  moveElement (targetRef, parentRef, index) {
+    return this.addActions(createAction('moveElement', [targetRef, parentRef, index]))
+  },
+
+  setAttr (ref, key, value) {
+    const result = {}
+    result[key] = value
+    return this.addActions(createAction('updateAttrs', [ref, result]))
+  },
+
+  setStyle (ref, key, value) {
+    const result = {}
+    result[key] = value
+    return this.addActions(createAction('updateStyle', [ref, result]))
+  },
+
+  setStyles (ref, style) {
+    return this.addActions(createAction('updateStyle', [ref, style]))
+  },
+
+  addEvent (ref, type) {
+    return this.addActions(createAction('addEvent', [ref, type]))
+  },
+
+  removeEvent (ref, type) {
+    return this.addActions(createAction('removeEvent', [ref, type]))
+  },
+
+  handler (actions, cb) {
+    return cb && cb()
+  },
+
+  addActions (actions) {
+    const updates = this.updates
+    const handler = this.handler
+
+    if (!Array.isArray(actions)) {
+      actions = [actions]
+    }
+
+    if (this.batched) {
+      updates.push.apply(updates, actions)
+    }
+    else {
+      return handler(actions)
+    }
+  }
+})
