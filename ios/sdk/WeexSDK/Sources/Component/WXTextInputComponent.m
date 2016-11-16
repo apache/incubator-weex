5d94f846b4b0a5b8d14a54205832b7a6fafcd6dd
acton393 <zhangxing610321@gmail.com>
2016-11-16 16:19:39 +0800

    * [ios] add remove observer

--- ios/sdk/WeexSDK/Sources/Component/WXTextInputComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXTextInputComponent.m
@@ -232,6 +232,11 @@ WX_EXPORT_METHOD(@selector(blur))
                                                   object:nil];
 }
 
+- (void)dealloc {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 -(void)focus
 {
     if(self.view)
