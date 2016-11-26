87ae0d7df9379a5b7430eb170755a0887fc0dc32
Hanks <zhanghan.me@gmail.com>
2016-11-27 00:21:43 +0800

    * [html5] add npm build script for vue renderer

---
+++ build/webpack.vue.config.js
@@ -0,0 +1,50 @@
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+
+var bannerPlugin = new webpack.BannerPlugin('//banner\n', {
+  raw: true
+})
+
+var devtool = '#inline-source-map'
+
+var plugins = [
+  bannerPlugin,
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    }
+  }),
+  new webpack.optimize.OccurenceOrderPlugin()
+]
+
+if (process.env.NODE_ENV === 'production') {
+  devtool = null
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }))
+}
+
+module.exports = {
+  entry: path.resolve('./html5/render/vue/index.js'),
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'vue-renderer.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules|vender/
+      }, {
+        test: /\.vue$/,
+        loader: 'vue'
+      }
+    ]
+  },
+  devtool: devtool,
+  plugins: plugins
+}
