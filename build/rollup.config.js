f2a4394da7ded8286a04eab8d1a4649e4a5e6ee1
勾三股四 <zhaojinjiang@me.com>
2016-11-26 23:48:54 -0600

    [jsfm] Supported replacing process.env (#1739)

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,16 +1,19 @@
 import { rollup } from 'rollup'
 import json from 'rollup-plugin-json'
 import eslint from 'rollup-plugin-eslint'
+import replace from 'rollup-plugin-replace'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
+
+const env = (process.argv.indexOf('--dev') >= 0) ? '' : 'production'
 
 const pkg = require('../package.json')
 const version = pkg.subversion.framework
 const date = new Date().toISOString().split('T')[0].replace(/\-/g, '')
 const banner = `\
 (this.nativeLog || function(s) {console.log(s)})('START JS FRAMEWORK: ${version} Build ${date}');
-var global = this, process = { env: {}};var setTimeout = global.setTimeout;
+var global = this, process = { env: {}}; var setTimeout = global.setTimeout;
 `
 
 export default {
@@ -18,11 +21,14 @@ export default {
   dest: './dist/native.js',
   banner,
   format: 'umd',
-  sourceMap: 'inline',
+  sourceMap: (env === 'production') ? false : 'inline',
   plugins: [
     json(),
     eslint({
       exclude: './package.json'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
     }),
     nodeResolve({
       jsnext: true,
