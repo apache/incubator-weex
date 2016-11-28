f9dcaa5874d5d1ab5a875e4095b0026ef61f4991
孤剑 <hongfeng78999@126.com>
2016-11-28 15:10:35 +0800

    + [ios] fix feature datepicker. 1.Split datePick into pickDate and pickTime. 2. Format code style.

--- ios/sdk/WeexSDK/Sources/Module/WXPickerModule.m
+++ ios/sdk/WeexSDK/Sources/Module/WXPickerModule.m
@@ -30,7 +30,7 @@
 
 //date picker
 @property(nonatomic,strong)UIDatePicker *datePicker;
-@property(nonatomic)NSString *datePickerType;
+@property(nonatomic)UIDatePickerMode datePickerMode;
 
 @end
 
@@ -39,7 +39,8 @@
 
 
 WX_EXPORT_METHOD(@selector(pick:callback:))
-WX_EXPORT_METHOD(@selector(datepick:callback:))
+WX_EXPORT_METHOD(@selector(pickDate:callback:))
+WX_EXPORT_METHOD(@selector(pickTime:callback:))
 
 #pragma mark -
 #pragma mark Single Picker
@@ -47,16 +48,16 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 {
     NSArray *items = @[];
     NSInteger index = 0 ;
-    if(options[@"items"]) {
+    if (options[@"items"]) {
         items = options[@"items"];
     }
-    if(options[@"index"]) {
+    if (options[@"index"]) {
         index = [WXConvert NSInteger:options[@"index"]];
     }
-    if(items && [items count]>0) {
+    if (items && [items count]>0) {
         [self createPicker:items index:index];
         self.callback = callback;
-    }else {
+    } else {
         callback(@{ @"result": @"error" });
         self.callback = nil;
     }
@@ -67,9 +68,9 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
     [self configPickerView];
     self.items = [items copy];
     self.index = index;
-    if(items && index < [items count]) {
+    if (items && index < [items count]) {
         [self.picker selectRow:index inComponent:0 animated:NO];
-    }else if(items && [items count]>0) {
+    } else if(items && [items count]>0) {
         [self.picker selectRow:0 inComponent:0 animated:NO];
     }
     [self show];
@@ -80,7 +81,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
     [[[UIApplication sharedApplication] keyWindow] endEditing:YES];  //收起键盘
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
     [window addSubview:self.backgroudView];
-    if(self.isAnimating) {
+    if (self.isAnimating) {
         return;
     }
     self.isAnimating = YES;
@@ -95,7 +96,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 
 -(void)hide
 {
-    if(self.isAnimating) {
+    if (self.isAnimating) {
         return;
     }
     self.isAnimating = YES;
@@ -128,12 +129,12 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 
 -(void)configPickerView
 {
-    if(!self.backgroudView) {
+    if (!self.backgroudView) {
         self.backgroudView = [self createBackgroudView];
         UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(hide)];
         [self.backgroudView addGestureRecognizer:tapGesture];
     }
-    if(!self.pickerView) {
+    if (!self.pickerView) {
         self.pickerView = [self createPickerView];
         UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, WXPickerToolBarHeight)];
         [toolBar setBackgroundColor:[UIColor whiteColor]];
@@ -145,7 +146,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
         [toolBar setItems:[NSArray arrayWithObjects:noSpace,cancelBtn,flexSpace,doneBtn,noSpace, nil]];
         [self.pickerView addSubview:toolBar];
     }
-    if(!self.picker) {
+    if (!self.picker) {
         self.picker = [[UIPickerView alloc]init];
         self.picker.delegate = self;
         CGRect pickerFrame = CGRectMake(0, WXPickerToolBarHeight, [UIScreen mainScreen].bounds.size.width, WXPickerHeight-WXPickerToolBarHeight);
@@ -195,12 +196,24 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 }
     
 #pragma mark -
-#pragma Date Picker
+#pragma Date & Time Picker
+-(void)pickDate:(NSDictionary *)options callback:(WXModuleCallback)callback
+{
+    self.datePickerMode = UIDatePickerModeDate;
+    [self datepick:options callback:callback];
+}
+
+-(void)pickTime:(NSDictionary *)options callback:(WXModuleCallback)callback
+{
+    self.datePickerMode = UIDatePickerModeTime;
+    [self datepick:options callback:callback];
+}
+    
 -(void)datepick:(NSDictionary *)options callback:(WXModuleCallback)callback
 {
-    if([options[@"type"] isEqualToString:@"date"] || [options[@"type"] isEqualToString:@"time"]) {
+    if ((UIDatePickerModeTime == self.datePickerMode) || (UIDatePickerModeDate == self.datePickerMode)) {
         [self createDatePicker:options callback:callback];
-    }else {
+    } else {
         callback(@{ @"result": @"error" });
         self.callback = nil;
     }
@@ -209,48 +222,39 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 - (void)createDatePicker:(NSDictionary *)options callback:(WXModuleCallback)callback
 {
     self.callback = callback;
-    if(!self.datePicker) {
+    if (!self.datePicker) {
         self.datePicker = [[UIDatePicker alloc]init];
     }
-    self.datePickerType = options[@"type"];
-    if([self.datePickerType isEqualToString:@"date"]) {
+    if (UIDatePickerModeDate == self.datePickerMode) {
         self.datePicker.datePickerMode = UIDatePickerModeDate;
         NSString *value = [WXConvert NSString:options[@"value"]];
-        if(value)
-        {
+        if (value) {
             NSDate *date = [self inputDateStringToDate:value];
-            if(date)
-            {
+            if (date) {
                 self.datePicker.date =date;
             }
         }
         NSString *max = [WXConvert NSString:options[@"max"]];
-        if(max)
-        {
+        if (max) {
             NSDate *date = [self inputDateStringToDate:max];
-            if(date)
-            {
+            if (date) {
                 self.datePicker.maximumDate =date;
             }
         }
         NSString *min = [WXConvert NSString:options[@"min"]];
-        if(min)
-        {
+        if (min) {
             NSDate *date = [self inputDateStringToDate:min];
             if(date)
             {
                 self.datePicker.minimumDate =date;
             }
         }
-    }else if([self.datePickerType isEqualToString:@"time"])
-    {
+    } else if (UIDatePickerModeTime == self.datePickerMode) {
         self.datePicker.datePickerMode = UIDatePickerModeTime;
         NSString *value = [WXConvert NSString:options[@"value"]];
-        if(value)
-        {
+        if (value) {
             NSDate *date = [self inputTimeStringToDate:value];
-            if(date)
-            {
+            if (date) {
                 self.datePicker.date = date;
             }
         }
@@ -258,15 +262,16 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
     [self configDatePickerView];
     [self showDatePicker];
 }
-    
+ 
+       
 -(void)configDatePickerView
 {
-    if(!self.backgroudView) {
+    if (!self.backgroudView) {
         self.backgroudView = [self createBackgroudView];
         UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(hide)];
         [self.backgroudView addGestureRecognizer:tapGesture];
     }
-    if(!self.pickerView) {
+    if (!self.pickerView) {
         self.pickerView = [self createPickerView];
         UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, WXPickerToolBarHeight)];
         [toolBar setBackgroundColor:[UIColor whiteColor]];
@@ -288,7 +293,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 {
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
     [window addSubview:self.backgroudView];
-    if(self.isAnimating)
+    if (self.isAnimating)
     {
         return;
     }
@@ -304,8 +309,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
     
 -(void)hideDatePicker
 {
-    if(self.isAnimating)
-    {
+    if (self.isAnimating) {
         return;
     }
     self.isAnimating = YES;
@@ -322,6 +326,7 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 -(void)cancelDatePicker:(id)sender
 {
     [self hide];
+    self.callback(@{ @"result": @"cancel"});
     self.callback = nil;
 }
     
@@ -329,10 +334,9 @@ WX_EXPORT_METHOD(@selector(datepick:callback:))
 {
     [self hide];
     NSString *value = @"";
-    if([self.datePickerType isEqualToString:@"time"])
-    {
+    if (UIDatePickerModeTime == self.datePicker.datePickerMode) {
         value = [self timeToString:self.datePicker.date];
-    }else if([self.datePickerType isEqualToString:@"date"])
+    } else if(UIDatePickerModeDate == self.datePicker.datePickerMode)
     {
         value = [self dateToString:self.datePicker.date];
     }
