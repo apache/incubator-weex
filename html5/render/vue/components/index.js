8edd85c8d63c5d1237913c1cefbd973d6f224d8d
Hanks <zhanghan.me@gmail.com>
2016-12-09 17:12:04 +0800

    * [html5] add web component and test cases

--- html5/render/vue/components/index.js
+++ html5/render/vue/components/index.js
@@ -1,11 +1,13 @@
 import a from './a.vue'
 import text from './text'
 import image from './image.vue'
+import web from './web.vue'
 import _switch from './switch.vue'
 
 export default {
   a,
   text,
+  web,
   'switch': _switch,
   image
 }
---
+++ html5/render/vue/components/web.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe :src="src" class="weex-web"></iframe>
+</template>
+
+<script>
+  export default {
+    props: {
+      src: String
+    }
+  }
+</script>
