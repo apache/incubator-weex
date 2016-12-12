ae8174cf2d5fb25d71d57e3aab47a2169c387154
Hanks <zhanghan.me@gmail.com>
2016-12-12 23:26:41 +0800

    * [html5] enable to auto-play for slider component

--- html5/render/vue/components/slider/index.js
+++ html5/render/vue/components/slider/index.js
@@ -1,8 +1,11 @@
 import { validateStyles } from '../../validator'
 import { throttle, bind } from '../../utils'
 import indicator from './indicator'
+import slideMixin from './slideMixin'
 
 export default {
+  mixins: [slideMixin],
+  components: { indicator },
   props: {
     'auto-play': {
       type: [String, Boolean],
@@ -13,8 +16,6 @@ export default {
       default: 3000
     }
   },
-
-  components: { indicator },
 
   data () {
     return {
@@ -39,83 +40,6 @@ export default {
       if (inner) {
         inner.style.width = this.wrapperWidth * this.frameCount + 'px'
       }
-    },
-
-    slideTo (index) {
-      let newIndex = (index | 0) // % this.frameCount
-      // let newIndex = (index | 0) % this.frameCount // scroll to left
-      newIndex = Math.max(newIndex, 0)
-      newIndex = Math.min(newIndex, this.frameCount - 1)
-
-      const offset = -newIndex * this.wrapperWidth
-      const inner = this.$refs.inner
-
-      if (inner) {
-        inner.style.transform = `translate3d(${offset}px, 0, 0)`
-      }
-      this.currentIndex = newIndex
-    },
-
-    next () {
-      this.slide(this.currentIndex + 1)
-    },
-
-    prev () {
-      this.slide(this.currentIndex - 1)
-    },
-
-    handleTouchStart (event) {
-      event.preventDefault()
-      // console.log('touch start', event)
-      const touch = event.changedTouches[0]
-      // console.log('touch start', event.target, event.target.pageY)
-      // console.log('touches', touch)
-      this._touchParams = {
-        originalTransform: this.$refs.inner.style.transform,
-        startTouchEvent: touch,
-        startX: touch.pageX,
-        startY: touch.pageY,
-        timeStamp: event.timeStamp
-      }
-    },
-
-    handleTouchMove (event) {
-      event.preventDefault()
-      // console.log('touch move')
-      if (this._touchParams) {
-        const inner = this.$refs.inner
-        const { startX } = this._touchParams
-        const touch = event.changedTouches[0]
-        const offsetX = touch.pageX - startX
-        // console.log('offsetX', offsetX, 'startX', startX, 'pageX', touch.pageX)
-        this._touchParams.offsetX = offsetX
-
-        if (inner && offsetX) {
-          // console.log('transform', `${offsetX - this.currentIndex * this.wrapperWidth}`)
-          inner.style.transform = `translate3d(${offsetX - this.currentIndex * this.wrapperWidth}px, 0, 0)`
-        }
-      }
-    },
-
-    handleTouchEnd (event) {
-      event.preventDefault()
-      // console.log('touch end')
-      const inner = this.$refs.inner
-      if (this._touchParams) {
-        const { offsetX } = this._touchParams
-        // console.log('touch pageX:', touch.pageX, ', offsetX:', offsetX)
-        if (inner) {
-          inner.style.transition = `transform .2s ease-in-out`
-
-          const reset = Math.abs(offsetX / this.wrapperWidth) < 0.2
-          const direction = offsetX > 0 ? 1 : -1
-          const newIndex = reset ? this.currentIndex : (this.currentIndex - direction)
-
-          // console.log('reset:', reset, ', newIndex:', newIndex)
-          this.slideTo(newIndex)
-        }
-      }
-      delete this._touchParams
     }
   },
 
@@ -123,6 +47,26 @@ export default {
     this.$nextTick(() => {
       this.updateLayout()
     })
+  },
+
+  mounted () {
+    if (this.autoPlay) {
+      const interval = Number(this.interval)
+      this._lastSlideTime = Date.now()
+
+      const autoPlayFn = bind(function () {
+        clearTimeout(this._autoPlayTimer)
+        const now = Date.now()
+        let nextTick = interval - now + this._lastSlideTime
+        nextTick = nextTick > 100 ? nextTick : interval
+
+        this.next()
+        this._lastSlideTime = now
+        this._autoPlayTimer = setTimeout(autoPlayFn, nextTick)
+      }, this)
+
+      this._autoPlayTimer = setTimeout(autoPlayFn, interval)
+    }
   },
 
   render (createElement) {
@@ -137,23 +81,30 @@ export default {
     }, [vnode]))
     this.frameCount = innerElements.length
 
-    return createElement('nav', {
-      ref: 'wrapper',
-      attrs: { 'weex-type': 'slider' },
-      staticClass: 'weex-slider weex-slider-wrapper',
-      on: {
-        touchstart: this.handleTouchStart,
-        touchmove: throttle(bind(this.handleTouchMove, this), 25),
-        touchend: this.handleTouchEnd
-      }
-    }, [createElement('ul', {
-      ref: 'inner',
-      staticClass: 'weex-slider-inner'
-    }, innerElements), createElement(indicator, {
-      attrs: {
-        count: this.frameCount,
-        active: this.currentIndex
-      }
-    })])
+    return createElement(
+      'nav',
+      {
+        ref: 'wrapper',
+        attrs: { 'weex-type': 'slider' },
+        staticClass: 'weex-slider weex-slider-wrapper',
+        on: {
+          touchstart: this.handleTouchStart,
+          touchmove: throttle(bind(this.handleTouchMove, this), 25),
+          touchend: this.handleTouchEnd
+        }
+      },
+      [
+        createElement('ul', {
+          ref: 'inner',
+          staticClass: 'weex-slider-inner'
+        }, innerElements),
+        createElement(indicator, {
+          attrs: {
+            count: this.frameCount,
+            active: this.currentIndex
+          }
+        })
+      ]
+    )
   }
 }
---
+++ html5/render/vue/components/slider/slideMixin.js
@@ -0,0 +1,89 @@
+export default {
+  methods: {
+    slideTo (index) {
+      // let newIndex = (index | 0) // % this.frameCount
+      let newIndex = (index | 0) % this.frameCount // scroll to left
+      newIndex = Math.max(newIndex, 0)
+      newIndex = Math.min(newIndex, this.frameCount - 1)
+
+      const offset = -newIndex * this.wrapperWidth
+      const inner = this.$refs.inner
+
+      if (inner) {
+        // TODO: will-change | set styles together
+        inner.style.transition = `transform .2s ease-in-out`
+        inner.style.transform = `translate3d(${offset}px, 0, 0)`
+        setTimeout(() => {
+          inner.style.transition = ''
+        }, 200)
+      }
+      if (newIndex !== this.currentIndex) {
+        this.currentIndex = newIndex
+        this.$emit('change', { index: this.currentIndex })
+      }
+    },
+
+    next () {
+      this.slideTo(this.currentIndex + 1)
+    },
+
+    prev () {
+      this.slideTo(this.currentIndex - 1)
+    },
+
+    handleTouchStart (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      // console.log('touch start', event)
+      const touch = event.changedTouches[0]
+      // console.log('touch start', event.target, event.target.pageY)
+      // console.log('touches', touch)
+      this._touchParams = {
+        originalTransform: this.$refs.inner.style.transform,
+        startTouchEvent: touch,
+        startX: touch.pageX,
+        startY: touch.pageY,
+        timeStamp: event.timeStamp
+      }
+    },
+
+    handleTouchMove (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      // console.log('touch move')
+      if (this._touchParams) {
+        const inner = this.$refs.inner
+        const { startX } = this._touchParams
+        const touch = event.changedTouches[0]
+        const offsetX = touch.pageX - startX
+        // console.log('offsetX', offsetX, 'startX', startX, 'pageX', touch.pageX)
+        this._touchParams.offsetX = offsetX
+
+        if (inner && offsetX) {
+          // console.log('transform', `${offsetX - this.currentIndex * this.wrapperWidth}`)
+          inner.style.transform = `translate3d(${offsetX - this.currentIndex * this.wrapperWidth}px, 0, 0)`
+        }
+      }
+    },
+
+    handleTouchEnd (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      // console.log('touch end')
+      const inner = this.$refs.inner
+      if (this._touchParams) {
+        const { offsetX } = this._touchParams
+        // console.log('touch pageX:', touch.pageX, ', offsetX:', offsetX)
+        if (inner) {
+          const reset = Math.abs(offsetX / this.wrapperWidth) < 0.2
+          const direction = offsetX > 0 ? 1 : -1
+          const newIndex = reset ? this.currentIndex : (this.currentIndex - direction)
+
+          // console.log('reset:', reset, ', newIndex:', newIndex)
+          this.slideTo(newIndex)
+        }
+      }
+      delete this._touchParams
+    }
+  }
+}
