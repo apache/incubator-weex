dfbe888d81b379f1b0f6faa5af3eeac340a81643
卜道 <705040424@qq.com>
2016-12-12 17:55:31 +0800

    * [android] update syncTest demo

--- android/playground/app/src/main/java/com/alibaba/weex/extend/module/SyncTestModule.java
+++ android/playground/app/src/main/java/com/alibaba/weex/extend/module/SyncTestModule.java
@@ -3,6 +3,7 @@ package com.alibaba.weex.extend.module;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.common.WXModule;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,32 +13,39 @@ import java.util.Map;
 
 public class SyncTestModule extends WXModule {
 
-//    @JSMethod(uiThread = false)
-//    public Object getString(){
-//        ArrayList list = new ArrayList();
-//        list.add(1,"222");
-//        list.add("test");
-//        return list;
-//    }
+    @JSMethod(uiThread = false)
+    public ArrayList getArray(){
+        ArrayList list = new ArrayList();
+        list.add("ArrayList test 1");
+        list.add("ArrayList test 2");
+        return list;
+    }
     @JSMethod(uiThread = false)
     public String  getString(){
 
-        return "i am string ";
+        return "getString :i am string ";
     }
-
 
     @JSMethod(uiThread = false)
     public int getNumber(){
         return 1111;
     }
 
+    /**
+     * JSON NOT allow KeyValue  as  non-string value
+     * @return
+     */
     @JSMethod(uiThread = false)
     public Object getObject(){
-
+        ArrayList list = new ArrayList();
+        list.add("222");
+        list.add("test");
         Map map = new HashMap();
-        map.put(11,"test11");
-        map.put(22,"test22");
-        return map;
+        map.put("11","test11");
+        map.put("22","test22");
+        list.add(map);
+        return list;
+
     }
 
 
