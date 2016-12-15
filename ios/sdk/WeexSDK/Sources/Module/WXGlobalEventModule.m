b7187cf2bf2fd61563a49ca0814d44050ffa1966
acton393 <zhangxing610321@gmail.com>
2016-12-15 19:37:36 +0800

    * [ios] add annotation about fireglobalEvent

--- ios/sdk/WeexSDK/Sources/Module/WXGlobalEventModule.m
+++ ios/sdk/WeexSDK/Sources/Module/WXGlobalEventModule.m
@@ -56,9 +56,14 @@ WX_EXPORT_METHOD(@selector(removeEventListener:))
 {
     NSDictionary * userInfo = notification.userInfo;
     NSString * userWeexInstanceId = userInfo[@"weexInstance"];
+    /*
+     1. The userWeexInstanceId param will be passed by globalEvent module notification.
+     2. The notification is posted by native user using Notificationcenter, native user don't need care about what the userWeexInstanceId is. What you do is to addEventListener in weex file using globalEvent module, and then post notification anywhere.
+     */
     WXSDKInstance * userWeexInstance = [WXSDKManager instanceForID:userWeexInstanceId];
      // In case that userInstanceId exists but instance has been dealloced
     if (!userWeexInstanceId || userWeexInstance == weexInstance) {
+        
         for (WXModuleKeepAliveCallback callback in _eventCallback[notification.name]) {
             callback(userInfo[@"param"], true);
         }
