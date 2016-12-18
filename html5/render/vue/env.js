781c8e4e79beb57aa06c69ece014d023b44d9107
Hanks <zhanghan.me@gmail.com>
2016-12-18 20:45:50 +0800

    + [html5] add components styles

--- html5/render/vue/env.js
+++ html5/render/vue/env.js
@@ -1,7 +1,14 @@
+import './styles/reset.css'
+import './styles/components.css'
+
 import '../../shared/arrayFrom'
 import '../../shared/objectAssign'
 import '../../shared/objectSetPrototypeOf'
-// TODO: Promise polyfill
+
+import 'core-js/modules/es6.object.to-string'
+import 'core-js/modules/es6.string.iterator'
+import 'core-js/modules/web.dom.iterable'
+import 'core-js/modules/es6.promise'
 
 // TODO: parse UA
 const ua = navigator.userAgent
