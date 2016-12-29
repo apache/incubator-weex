0d51b76694f860ab27e421e866603fb11e47ac66
隐风 <cxfeng1@gmail.com>
2016-12-29 14:06:58 +0800

    * [ios] fix test running problem

--- ios/sdk/WeexSDKTests/WXBridgeMethodTests.m
+++ ios/sdk/WeexSDKTests/WXBridgeMethodTests.m
@@ -54,11 +54,4 @@
     XCTAssertTrue(args.count == 0);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
--- ios/sdk/WeexSDKTests/WXStorageTests.m
+++ ios/sdk/WeexSDKTests/WXStorageTests.m
@@ -75,11 +75,4 @@
     }];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
