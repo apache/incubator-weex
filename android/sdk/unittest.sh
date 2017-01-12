6a8f1ca7fe0ce0d5df6c31da7b5d268976fc6821
sospartan <sospartan@gmail.com>
2017-01-12 11:34:10 +0800

    * [android] add mem dump after ci run

--- android/sdk/unittest.sh
+++ android/sdk/unittest.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-./gradlew clean testDebugUnitTest -PtargetSDK=19
-./gradlew jacocoTestReportDebug -PtargetSDK=19
+./gradlew clean testDebugUnitTest jacocoTestReportDebug -PtargetSDK=19  -Dorg.gradle.daemon=true -Dorg.gradle.parallel=true -Dorg.gradle.jvmargs="-Xmx512m -XX:+HeapDumpOnOutOfMemoryError" -Dfile.encoding=UTF-8
 osascript -e 'display notification "Test Done!" with title "Weex Unit Test"'
