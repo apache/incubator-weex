e3c044e21d262f85bdbe8c57345878240060193a
隐风 <cxfeng1@gmail.com>
2017-01-22 19:40:38 +0800

    * [ios] Add copyright header check

--- ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.m
+++ ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.m
@@ -3,8 +3,6 @@
  * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
  *
  * This source code is licensed under the Apache Licence 2.0.
- * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
- */
 
 #import "WXTestCopyRight.h"
 
