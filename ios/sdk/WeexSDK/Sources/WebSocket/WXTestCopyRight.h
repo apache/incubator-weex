a1e5ab41dbf43790fafb4b6fd713256f4b435f07
隐风 <cxfeng1@gmail.com>
2017-01-22 18:38:29 +0800

    * [ios] test copy right

---
+++ ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.h
@@ -0,0 +1,13 @@
+//
+//  WXTestCopyRight.h
+//  WeexSDK
+//
+//  Created by yinfeng on 2017/1/22.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXTestCopyRight : NSObject
+
+@end
---
+++ ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.m
@@ -0,0 +1,13 @@
+//
+//  WXTestCopyRight.m
+//  WeexSDK
+//
+//  Created by yinfeng on 2017/1/22.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import "WXTestCopyRight.h"
+
+@implementation WXTestCopyRight
+
+@end
