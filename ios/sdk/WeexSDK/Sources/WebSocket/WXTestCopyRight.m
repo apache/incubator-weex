a549c374e2f3c5c793a8917b47760b663b414f93
隐风 <cxfeng1@gmail.com>
2017-01-22 18:58:43 +0800

    * [ios] test copy right

--- ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.m
+++ ios/sdk/WeexSDK/Sources/WebSocket/WXTestCopyRight.m
@@ -1,10 +1,10 @@
-//
-//  WXTestCopyRight.m
-//  WeexSDK
-//
-//  Created by yinfeng on 2017/1/22.
-//  Copyright © 2017年 taobao. All rights reserved.
-//
+/**
+ * Created by Weex.
+ * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
+ */
 
 #import "WXTestCopyRight.h"
 
