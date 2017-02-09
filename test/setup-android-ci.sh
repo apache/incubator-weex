1270dbf60105925aff4f0383bc172c5a58b08b1d
sospartan <sospartan@gmail.com>
2017-02-09 15:24:16 +0800

    * [test] test ci 3

--- test/setup-android-ci.sh
+++ test/setup-android-ci.sh
@@ -1,1 +1,3 @@
-echo no | android create avd --force -n weexavd -t android-19 --abi armeabi-v7a &
+echo y | android update sdk --no-ui --all --filter extra-android-m2repository
+echo no | android create avd --force -n weexavd -t android-19 --abi armeabi-v7a
+$ANDROID_HOME/tools/emulator avd -list-avds
