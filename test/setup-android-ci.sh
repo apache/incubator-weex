b0494b0f5b8af30c61e4dc4a772b7fd819ee83d8
sospartan <sospartan@gmail.com>
2017-02-09 12:26:54 +0800

    * [test] test ci

--- test/setup-android-ci.sh
+++ test/setup-android-ci.sh
@@ -1,2 +1,2 @@
 echo no | android create avd --force -n weexavd -t android-19 --abi armeabi-v7a
-emulator -avd weexavd -no-audio -no-window &
+/usr/local/android-sdk-linux/tools/emulator -avd weexavd -no-audio -no-window &
