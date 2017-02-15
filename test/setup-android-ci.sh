aa0ffdea28832500f0a65856e2ab887a041db18e
sospartan <sospartan@gmail.com>
2017-02-15 12:21:22 +0800

    * [test] update travis

--- test/setup-android-ci.sh
+++ test/setup-android-ci.sh
@@ -1,3 +1,22 @@
-echo y | android update sdk --no-ui --all --filter extra-android-m2repository
-echo no | android create avd --force -n weexavd -t android-19 --abi armeabi-v7a
-$ANDROID_HOME/tools/emulator avd -list-avds
+function installAndroidSDK {
+    brew install android-sdk
+    export ANDROID_HOME=/usr/local/opt/android-sdk
+    export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+    echo yes | android update sdk --all --no-ui --force -t 2 #platform tools
+    echo yes | android update sdk --all --no-ui --force -t 1 #tools
+    echo yes | android update sdk --all --no-ui --force -t 11 #build-tool
+    echo yes | android update sdk --all --no-ui --force -t 39 #sdk android-19
+    echo yes | android update sdk --all --no-ui --force -t 97 #sys-img
+    echo yes | android update sdk --all --no-ui --force -t 160 #support
+}
+
+function waitForEmulator {
+  local bootanim=""
+  export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
+  until [[ "$bootanim" =~ "stopped" ]]; do
+    sleep 5
+    bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
+    echo "emulator status=$bootanim"
+  done
+  adb shell input keyevent 82 &
+}
