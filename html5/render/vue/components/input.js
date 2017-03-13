8172c0fc8e7324dafe35ae784cf6a8efb7bc1008
MrRaindrop <tekkahs@gmail.com>
2017-03-14 00:13:44 +0800

    * [html5] fix input's methods, focus and blur.

--- html5/render/vue/components/input.js
+++ html5/render/vue/components/input.js
@@ -28,6 +28,15 @@ export default {
     maxlength: [String, Number]
   },
 
+  methods: {
+    focus () {
+      this.$el && this.$el.focus()
+    },
+    blur () {
+      this.$el && this.$el.blur()
+    }
+  },
+
   render (createElement) {
     /* istanbul ignore next */
     // if (process.env.NODE_ENV === 'development') {
