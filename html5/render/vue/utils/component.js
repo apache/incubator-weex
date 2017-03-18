e8a1b904f4a6089cc1b2a2a28c3b9a81bba60b1a
MrRaindrop <tekkahs@gmail.com>
2017-03-18 17:00:36 +0800

    * [html5] fix error of appear.

--- html5/render/vue/utils/component.js
+++ html5/render/vue/utils/component.js
@@ -94,10 +94,10 @@ export function watchAppear (context) {
               listener = listener.fn
             }
             const scrollTop = container.scrollTop || window.pageYOffset
-            listener(createEvent(context.$el, type, {
+            lastScrollTop = scrollTop
+            listener && listener(createEvent(context.$el, type, {
               direction: scrollTop > lastScrollTop ? 'down' : 'up'
             }))
-            lastScrollTop = scrollTop
           }
         }, 25, true)
 
