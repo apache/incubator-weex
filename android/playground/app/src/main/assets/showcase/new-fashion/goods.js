93063f1425289e2b501a0bd821f5e1153e52849a
xkli <569664668@qq.com>
2017-03-27 14:58:56 +0800

    Merge branch 'v0.10.0-playground' into 0.11-dev
    * v0.10.0-playground:
      *[android] update app version 0.10.0
      *[android] update app version
      *[android] update example
      更新example
      *[android] update ic_launcher
      * [android] update app version
      * [android] update weex_inspector version
      * [android] add mask component
      * [android] update verison
      * [android] add linear-gradient.we
    
    # Conflicts:
    #	android/playground/app/build.gradle
    #	android/sdk/assets/main.js
    #	android/sdk/build.gradle
    #	examples/module/websocket-demo.we
    #	examples/vue/components/input.vue
    #	examples/vue/include/marquee.vue
    #	examples/vue/index.vue
    #	examples/vue/showcase/calculator.vue
    #	examples/vue/template.vue

--- android/playground/app/src/main/assets/showcase/new-fashion/goods.js
+++ android/playground/app/src/main/assets/showcase/new-fashion/goods.js
@@ -45,10 +45,10 @@
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(237)
-	var __weex_template__ = __webpack_require__(253)
-	var __weex_style__ = __webpack_require__(254)
-	var __weex_script__ = __webpack_require__(255)
+	__webpack_require__(246)
+	var __weex_template__ = __webpack_require__(262)
+	var __weex_style__ = __webpack_require__(263)
+	var __weex_script__ = __webpack_require__(264)
 
 	__weex_define__('@weex-component/4ee2a41c614b4058c24278d8a41a24eb', [], function(__weex_require__, __weex_exports__, __weex_module__) {
 
@@ -67,7 +67,7 @@
 
 /***/ },
 
-/***/ 235:
+/***/ 244:
 /***/ function(module, exports) {
 
 	module.exports = {
@@ -87,7 +87,7 @@
 
 /***/ },
 
-/***/ 236:
+/***/ 245:
 /***/ function(module, exports) {
 
 	module.exports = function(module, exports, __weex_require__){'use strict';
@@ -117,11 +117,11 @@
 
 /***/ },
 
-/***/ 237:
+/***/ 246:
 /***/ function(module, exports, __webpack_require__) {
 
-	var __weex_template__ = __webpack_require__(235)
-	var __weex_script__ = __webpack_require__(236)
+	var __weex_template__ = __webpack_require__(244)
+	var __weex_script__ = __webpack_require__(245)
 
 	__weex_define__('@weex-component/banner', [], function(__weex_require__, __weex_exports__, __weex_module__) {
 
@@ -137,7 +137,7 @@
 
 /***/ },
 
-/***/ 253:
+/***/ 262:
 /***/ function(module, exports) {
 
 	module.exports = {
@@ -263,7 +263,7 @@
 
 /***/ },
 
-/***/ 254:
+/***/ 263:
 /***/ function(module, exports) {
 
 	module.exports = {
@@ -289,7 +289,7 @@
 
 /***/ },
 
-/***/ 255:
+/***/ 264:
 /***/ function(module, exports) {
 
 	module.exports = function(module, exports, __weex_require__){"use strict";
