86bacfb3399ecdad72002a630ad719e76a0bbf7c
kaifeng.ly <kaifeng.ly@alibaba-inc.com>
2017-04-06 17:29:23 +0800

    eslint warning

--- html5/frameworks/legacy/app/ctrl/init.js
+++ html5/frameworks/legacy/app/ctrl/init.js
@@ -89,49 +89,49 @@ export function init (app, code, data, services) {
           args[0](...args.slice(2))
         }
 
-        var timerId, callbackId;
+        let timerId, callbackId
 
-        if( global.setTimeoutWeex && (typeof global.setTimeoutWeex === 'function')){
-            callbackId = app.doc.taskCenter.normalize(handler);
-            timerId = global.setTimeoutWeex(app.id, callbackId, args[1]);
-            return timerId;
-        }else{
-          timer.setTimeout(handler, args[1]);
-          return app.doc.taskCenter.callbackManager.lastCallbackId.toString();
+        if (global.setTimeoutWeex && (typeof global.setTimeoutWeex === 'function')) {
+          callbackId = app.doc.taskCenter.normalize(handler)
+          timerId = global.setTimeoutWeex(app.id, callbackId, args[1])
+          return timerId
         }
-
+        else {
+          timer.setTimeout(handler, args[1])
+          return app.doc.taskCenter.callbackManager.lastCallbackId.toString()
+        }
       },
       setInterval: (...args) => {
         const handler = function () {
           args[0](...args.slice(2))
         }
 
-        var timerId, callbackId;
-        
-        if( global.setIntervalWeex && (typeof global.setIntervalWeex === 'function')){
-          callbackId = app.doc.taskCenter.normalize(handler);
-          timerId = global.setIntervalWeex(app.id, callbackId, args[1]);
-          return timerId;
-        }else{
-          timer.setInterval(handler, args[1]);
-          return app.doc.taskCenter.callbackManager.lastCallbackId.toString();
+        let timerId, callbackId
+
+        if (global.setIntervalWeex && (typeof global.setIntervalWeex === 'function')) {
+          callbackId = app.doc.taskCenter.normalize(handler)
+          timerId = global.setIntervalWeex(app.id, callbackId, args[1])
+          return timerId
         }
-        
+        else {
+          timer.setInterval(handler, args[1])
+          return app.doc.taskCenter.callbackManager.lastCallbackId.toString()
+        }
       },
       clearTimeout: (n) => {
-        
-        if( global.clearTimeoutWeex && (typeof global.clearTimeoutWeex === 'function')){
-          global.clearTimeoutWeex(n);
-        }else{
+        if (global.clearTimeoutWeex && (typeof global.clearTimeoutWeex === 'function')) {
+          global.clearTimeoutWeex(n)
+        }
+        else {
           timer.clearTimeout(n)
         }
       },
       clearInterval: (n) => {
-        
-        if( global.clearIntervalWeex && (typeof global.clearIntervalWeex === 'function')){
-          global.clearIntervalWeex(n);
-        }else{
-          timer.clearInterval(n);
+        if (global.clearIntervalWeex && (typeof global.clearIntervalWeex === 'function')) {
+          global.clearIntervalWeex(n)
+        }
+        else {
+          timer.clearInterval(n)
         }
       }
     })
