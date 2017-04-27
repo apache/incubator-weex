8d6b37e6a482b7cb253eac4983a24aa0706485d4
acton393 <zhangxing610321@gmail.com>
2017-04-27 11:44:15 +0800

    * [ios] remove unused

--- ios/sdk/WeexSDK/Sources/Component/WXImageComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXImageComponent.m
@@ -295,7 +295,7 @@ static dispatch_queue_t WXImageUpdateQueue;
         WXLogDebug(@"Updating image:%@, component:%@", self.imageSrc, self.ref);
         NSDictionary *userInfo = @{@"imageQuality":@(self.imageQuality), @"imageSharp":@(self.imageSharp), @"blurRadius":@(self.blurRadius)};
         NSMutableString * newURL = [imageSrc mutableCopy];
-        WX_REWRITE_URL(imageSrc, WXResourceTypeImage, self.weexInstance);
+        WX_REWRITE_URL(imageSrc, WXResourceTypeImage, self.weexInstance)
         __weak typeof(self) weakSelf = self;
         dispatch_async(dispatch_get_main_queue(), ^{
             weakSelf.imageOperation = [[weakSelf imageLoader] downloadImageWithURL:newURL imageFrame:weakSelf.calculatedFrame userInfo:userInfo completed:^(UIImage *image, NSError *error, BOOL finished) {
