8b2f3e20cc6adf025a59518ce4f3ce295ebdf5fa
lammmmmmm <lamenfactory@gmail.com>
2017-04-30 13:20:08 +0800

    Update iconfont.vue
    Make it work for android
    change to Camel-Case can make it work in android

--- examples/vue/iconfont.vue
+++ examples/vue/iconfont.vue
@@ -1,59 +1,59 @@
 <template>
-  <div>
-    <text class="title1">Assets: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
-    <text class="title2">http ttf: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
-    <text class="title3">http woff: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
-    <text class="title4">file: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
-  </div>
+    <div>
+        <text class="title1">Assets: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
+        <text class="title2">http ttf: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
+        <text class="title3">http woff: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
+        <text class="title4">file: &#xe606;&#xe605;&#xe604;&#xe603;&#xe602;&#xe601;&#xe600;</text>
+    </div>
 
 </template>
 <style scoped>
-  .title1 {
-    color: red;
-    font-size: 36;
-    font-family: iconfont1;
-  }
+    .title1 {
+        color: red;
+        font-size: 36;
+        fontFamily: iconfont1;
+    }
 
-  .title2 {
-    color: red;
-    font-size: 36;
-    font-family: iconfont2;
-  }
+    .title2 {
+        color: red;
+        font-size: 36;
+        fontFamily: iconfont2;
+    }
 
-  .title3 {
-    color: red;
-    font-size: 36;
-    font-family: iconfont3;
-  }
+    .title3 {
+        color: red;
+        font-size: 36;
+        fontFamily: iconfont3;
+    }
 
-  .title4 {
-    color: red;
-    font-size: 36;
-    font-family: iconfont4;
-  }
+    .title4 {
+        color: red;
+        font-size: 36;
+        fontFamily: iconfont4;
+    }
 </style>
 
 <script>
-  var domModule = weex.requireModule('dom');
-  module.exports = {
-    created: function() {
-      //目前支持ttf、woff文件，不支持svg、eot类型
-      domModule.addRule('font-face', {
-        'font-family': "iconfont1",
-        'src' : "url('iconfont.ttf')"
-      });
-      domModule.addRule('font-face', {
-        'font-family': "iconfont2",
-        'src': "url('http://at.alicdn.com/t/font_1469606063_76593.ttf')"
-      });
-      domModule.addRule('font-face', {
-        'font-family': "iconfont3",
-        'src': "url('http://at.alicdn.com/t/font_1469606522_9417143.woff')"
-      });
-      domModule.addRule('font-face', {
-        'font-family': "iconfont4",
-        'src': "url('file:///storage/emulated/0/Android/data/com.alibaba.weex/cache/http:__at.alicdn.com_t_font_1469606063_76593.ttf')"
-      });
+    var domModule = weex.requireModule('dom');
+    module.exports = {
+        created: function() {
+            //目前支持ttf、woff文件，不支持svg、eot类型
+            domModule.addRule('fontFace', {
+                'fontFamily': "iconfont1",
+                'src' : "url('iconfont.ttf')"
+            });
+            domModule.addRule('fontFace', {
+                'fontFamily': "iconfont2",
+                'src': "url('http://at.alicdn.com/t/font_1469606063_76593.ttf')"
+            });
+            domModule.addRule('fontFace', {
+                'fontFamily': "iconfont3",
+                'src': "url('http://at.alicdn.com/t/font_1469606522_9417143.woff')"
+            });
+            domModule.addRule('fontFace', {
+                'fontFamily': "iconfont4",
+                'src': "url('file:///storage/emulated/0/Android/data/com.alibaba.weex/cache/http:__at.alicdn.com_t_font_1469606063_76593.ttf')"
+            });
+        }
     }
-  }
 </script>
