b8d8dca89aa60c006dcc3144822c1b2331eea2d5
tancy <ronntx@gmail.com>
2017-05-18 15:35:10 +0800

    + [doc] add article anchor

--- doc/themes/weex/scripts/helper.js
+++ doc/themes/weex/scripts/helper.js
@@ -1,4 +1,5 @@
 'use strict'
+var cheerio = require('cheerio');
 
 hexo.extend.helper.register('url_for_lang', function(path){
   var root = this.config.root || ''
@@ -12,6 +13,23 @@ hexo.extend.helper.register('url_for_lang', function(path){
   }
   return url
 })
+
+hexo.extend.helper.register('page_anchor', function(str){
+  var $ = cheerio.load(str, {decodeEntities: false});
+  var headings = $('h1, h2, h3, h4, h5, h6');
+
+  if (!headings.length) return str;
+
+  headings.each(function(){
+    var id = $(this).attr('id');
+
+    $(this)
+      .addClass('article-heading')
+      .append('<a class="article-anchor" href="#' + id + '" aria-hidden="true"></a>');
+  });
+
+  return $.html();
+});
 
 hexo.extend.helper.register('pick_lang', function(lang){
   var root = this.config.root || ''
