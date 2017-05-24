119cc7006a1ee39250d4ba6f0180ac7608c51b6d
sospartan <sospartan@apache.org>
2017-05-24 16:39:15 +0800

    * [all] add repackage to release script

--- scripts/generate_apache_release.sh
+++ scripts/generate_apache_release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
+set -e
 if [ ! -f scripts/build_from_source.sh ];then
     echo "This script must execute in project root"
     exit 1
@@ -9,6 +10,21 @@ dest='apache_release_temp'
 rm -rf $dest
 mkdir $dest
 rsync -rvv  --include-from=scripts/release_files.rules ./ $dest
+
+#repackage
+find $dest/android/sdk/src -type f -name '*.java' -exec sed -i '' 's/com\.taobao\.weex/org\.apache\.weex/g' {} \;
+
+mkdir -p $dest/android/sdk/src/main/java/org
+mkdir -p $dest/android/sdk/src/main/java/org/apache
+mv $dest/android/sdk/src/main/java/com/taobao $dest/android/sdk/src/main/java/org/apache
+rm -rf $dest/android/sdk/src/main/java/com
+
+mkdir -p $dest/android/sdk/src/test/java/org
+mkdir -p $dest/android/sdk/src/test/java/org/apache
+mv $dest/android/sdk/src/test/java/com/taobao $dest/android/sdk/src/test/java/org/apache
+rm -rf $dest/android/sdk/src/test/java/com
+
+sed -i '' 's/com\/taobao\/weex/org\/apache\/weex/g' $dest/android/sdk/build.gradle
 
 mv $dest/ios/sdk $dest/ios_sdk
 rm -rf $dest/ios
--- scripts/replace-header.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eu
-  #find ios/playground/WeexDemo -name "*.[m|h]" -exec ./scripts/rh/replace_header.sh {} \;
-  # find html5 -name "*.js" -exec ./scripts/rh/replace_header.sh {} \;
-  # find android -name "*.java" -exec ./scripts/rh/replace_header.sh {} \;
