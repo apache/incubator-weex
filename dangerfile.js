48d7aded1e68f907933ac6a0a078974445fa9d1d
yinfeng <cxfeng@apache.org>
2017-05-31 16:27:07 +0800

    * [test] reject if blame info request is failed

--- dangerfile.js
+++ dangerfile.js
@@ -219,6 +219,9 @@ function findReviewer(resolve, reject) {
     number: danger.github.pr.number,
     headers: {Accept: 'application/vnd.github.diff'}
   }, function (err, result) {
+    if (!result || !result.data || err) {
+      reject()
+    }
     parseDeleteAndNormalLines(result.data, fileToDeletedLinesMap, fileToNormalLinesMap)
     var promises = danger.git.modified_files.map(function(file) {
       let repoURL = danger.github.pr.base.repo.html_url
