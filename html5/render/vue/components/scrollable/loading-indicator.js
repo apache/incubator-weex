baffd79a12119c4cc517c9f6218d06f30305b13f
MrRaindrop <tekkahs@gmail.com>
2017-06-20 11:48:41 +0800

    * [html5] fix refresh & loading & loading-indicator @notdanger.
    * [html5] update

--- html5/render/vue/components/scrollable/loading-indicator.js
+++ html5/render/vue/components/scrollable/loading-indicator.js
@@ -21,8 +21,8 @@ import { extractComponentStyle } from '../../core'
 const _css = `
 .weex-refresh-indicator,
 .weex-loading-indicator {
-  width: 1.0rem;
-  height: 1.0rem;
+  width: 1rem !important;
+  height: 1rem !important;
   -webkit-box-align: center;
   -webkit-align-items: center;
       -ms-flex-align: center;
@@ -39,17 +39,15 @@ const _css = `
   display: block;
   content: '';
   font-size: 0.16rem;
-  width: 1em;
-  height: 1em;
-  left: -60%;
-  top: 40%;
+  width: 0.5em;
+  height: 0.5em;
+  left: 0;
+  top: 0;
   border-radius: 50%;
   position: relative;
   text-indent: -9999em;
   -webkit-animation: weex-spinner 1.1s infinite ease;
           animation: weex-spinner 1.1s infinite ease;
-  -webkit-transform: translate3d(1.0rem, 0, 0);
-          transform: translate3d(1.0rem, 0, 0);
 }
 `
 
