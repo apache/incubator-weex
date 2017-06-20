3184a46eead7e3122f55d0dea8b7d9dec8a6bd5d
lybeen <ilybeen@gmail.com>
2017-06-20 14:11:54 +0800

    add array protect to prevent out of index

--- ios/sdk/WeexSDK/Sources/Component/WXListComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXListComponent.m
@@ -598,15 +598,19 @@
         NSIndexPath *topCellPath = [[_tableView indexPathsForVisibleRows] objectAtIndex:0];
         if (self.currentTopVisibleSection != topCellPath.section) {
             if (self.currentTopVisibleSection) {
-                WXSection *removeSection = [_sections objectAtIndex:self.currentTopVisibleSection];
-                if (removeSection.header && [removeSection.header.events containsObject:@"unsticky"]) {
-                    [removeSection.header fireEvent:@"unsticky" params:nil];
+                if (self.currentTopVisibleSection < [_sections count]) {
+                    WXSection *removeSection = [_sections objectAtIndex:self.currentTopVisibleSection];
+                    if (removeSection.header && [removeSection.header.events containsObject:@"unsticky"]) {
+                        [removeSection.header fireEvent:@"unsticky" params:nil];
+                    }
                 }
             }
             self.currentTopVisibleSection = topCellPath.section;
-            WXSection *showSection = [_sections objectAtIndex:topCellPath.section];
-            if (showSection.header && [showSection.header.events containsObject:@"sticky"]) {
-                [showSection.header fireEvent:@"sticky" params:nil];
+            if (topCellPath.section < [_sections count]) {
+                WXSection *showSection = [_sections objectAtIndex:topCellPath.section];
+                if (showSection.header && [showSection.header.events containsObject:@"sticky"]) {
+                    [showSection.header fireEvent:@"sticky" params:nil];
+                }
             }
         }
     }
