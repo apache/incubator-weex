323b836731f4d66a0b5836aa4ab82bc3cfa43b0a
erha19 <faterrole@gmail.com>
2017-06-23 16:23:24 +0800

    + [html5] add unit test for webSocket module

---
+++ html5/test/render/vue/modules/websocket.js
@@ -0,0 +1,11 @@
+import websocket from '../../../../render/vue/modules/websocket/websocket';
+describe('webSocket module', function() {
+  afterEach(function(done) {
+    // Cleanup
+    websocket.close();
+    done();
+  });
+  it('should have connection lifecycle constants defined on the instance', () => {
+    expect(websocket.WebSocket('wss://echo.websocket.org').CONNECTING).to.be.equal(0);
+  });
+});
