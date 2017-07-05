e34099147a2eb64b8caaecb5d6c1f72c0bfb9041
齐山 <sunjjbobo@163.com>
2017-07-05 22:32:39 +0800

    + [ios] update code

--- ios/playground/WeexDemo/WXDemoViewController.m
+++ ios/playground/WeexDemo/WXDemoViewController.m
@@ -147,7 +147,7 @@
     _instance.renderFinish = ^(UIView *view) {
          WXLogDebug(@"%@", @"Render Finish...");
         dispatch_async(dispatch_get_main_queue(), ^{
-            [WXTracingManager startTracing:weakSelf.instance.instanceId ref:nil parentRef:nil className:nil name:nil ph:WXTracingInstant fName:WXTracingRenderFinish parentId:nil];
+            [WXTracingManager startTracing:weakSelf.instance.instanceId ref:nil parentRef:nil className:nil name:nil ph:WXTracingInstant fName:WXTRenderFinish parentId:nil];
             [WXTracingManager getTracingData:weakSelf.instance.instanceId];
         });
         [weakSelf updateInstanceState:WeexInstanceAppear];
