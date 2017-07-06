6616f4b4ceb29fdd2762967dcd785f313a854c97
erha19 <faterrole@gmail.com>
2017-07-06 16:36:49 +0800

    * [html5] update unit test

--- html5/test/render/vue/utils/func.js
+++ html5/test/render/vue/utils/func.js
@@ -4,7 +4,7 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * "License") you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
@@ -17,51 +17,143 @@
  * under the License.
  */
 import * as utils from '../../../../render/vue/utils'
-
 describe('utils', function () {
   describe('function', function () {
-    it('cached', function () {
-      expect(utils.cached).to.be.a('function')
-    })
-
-    it('camelize', function () {
-      const { camelize } = utils
-      expect(camelize).to.be.a('function')
-      expect(camelize('')).to.be.equal('')
-      expect(camelize('dispaly')).to.be.equal('dispaly')
-      expect(camelize('-webkit-transform')).to.be.equal('WebkitTransform')
-      expect(camelize('text-overflow')).to.be.equal('textOverflow')
-      expect(camelize('a-b-c-d')).to.be.equal('aBCD')
-    })
-
-    it('capitalize', function () {
-      const { capitalize } = utils
-      expect(capitalize).to.be.a('function')
-      expect(capitalize('')).to.be.equal('')
-      expect(capitalize('string')).to.be.equal('String')
-      expect(capitalize('string object')).to.be.equal('String object')
-      expect(capitalize('[string object]')).to.be.equal('[string object]')
-      expect(capitalize('I have an apple')).to.be.equal('I have an apple')
-    })
-
-    it('hyphenate', function () {
-      const { hyphenate } = utils
-      expect(hyphenate).to.be.a('function')
-      expect(hyphenate('')).to.be.equal('')
-      expect(hyphenate('dispaly')).to.be.equal('dispaly')
-      expect(hyphenate('WebkitTransform')).to.be.equal('webkit-transform')
-      expect(hyphenate('textOverflow')).to.be.equal('text-overflow')
-      expect(hyphenate('aBCD')).to.be.equal('a-b-c-d')
-    })
-
     it('extend', function () {
-      const { extend } = utils
+      const {
+        extend
+      } = utils
       expect(extend).to.be.a('function')
-
-      const abc = { name: 'abc' }
+      const abc = {
+        name: 'abc'
+      }
       expect(extend(abc)).to.deep.equal(abc)
-      expect(extend(abc, { name: 'x' }) === abc).to.be.true
-      expect(abc).to.deep.equal({ name: 'x' })
+      expect(extend(abc, {
+        name: 'x'
+      }) === abc).to.be.true
+      expect(abc).to.deep.equal({
+        name: 'x'
+      })
+      expect(abc).to.deep.equal({
+        name: 'x'
+      })
+      expect(extend(abc, {})).to.be.deep.equal(abc)
+    })
+    it('extendTruthy', function () {
+      const {
+        extendTruthy
+      } = utils
+      expect(extendTruthy).to.be.a('function')
+      const from = {
+        '': '',
+        'undefined': 'undefined',
+        0: '0',
+        test: 'test'
+      }
+      expect(extendTruthy({}, [from])[0]).to.be.deep.equal(from)
+      expect(extendTruthy({}, [])).to.be.deep.equal({})
+    })
+    it('extendKeys', function () {
+      const {
+        extendKeys
+      } = utils
+      expect(extendKeys).to.be.a('function')
+      const from = {
+        test1: 'test1',
+        test2: 'test2'
+      }
+      const expected = {
+        test1: 'test1'
+      }
+      expect(extendKeys({}, from, ['test1'])).to.be.deep.equal(expected)
+    })
+    it('extractKeys', function () {
+      const {
+        extractKeys
+      } = utils
+      expect(extractKeys).to.be.a('function')
+      const from = {
+        test1: 'test1',
+        test2: 'test2'
+      }
+      const toExpected = {
+        test1: 'test1'
+      }
+      const fromExpected = {
+        test2: 'test2'
+      }
+      expect(extractKeys({}, from, ['test1'])).to.be.deep.equal(toExpected)
+      expect(from).to.be.deep.equal(fromExpected)
+      expect(extractKeys({}, {}, [])).to.be.deep.equal({})
+    })
+    it('bind', function () {
+      const {
+        bind
+      } = utils
+      const testfn = function (name) {
+        if (name) {
+          this.name = name
+        }
+        return this.name ? this.name : ''
+      }
+      const TEST = function () {
+        this.name = 'test'
+      }
+      const shouldBe = bind(testfn, new TEST())()
+      expect(bind).to.be.a('function')
+      expect(shouldBe).to.be.equal('test')
+    })
+    it('debounce', function () {
+      const {
+        debounce
+      } = utils
+      const shouldBe = 'test'
+      const expected = debounce(function () {
+        return shouldBe
+      }, 100)
+      setTimeout(function () {
+        expect(shouldBe).to.be.equal(expected)
+      }, 100)
+      expect(debounce).to.be.a('function')
+    })
+    it('depress', function () {
+      const {
+        depress
+      } = utils
+      const shouldBe = 'test'
+      const expected = depress(function () {
+        return shouldBe
+      }, 100)
+      setTimeout(function () {
+        expect(shouldBe).to.be.equal(expected)
+      }, 100)
+      expect(depress).to.be.a('function')
+    })
+    it('throttle', function () {
+      const {
+        throttle
+      } = utils
+      let expected
+      throttle(function () {
+        expected = 'test1'
+      }, 100)
+      throttle(function () {
+        expected = 'test2'
+      }, 50)
+      setTimeout(function () {
+        expect(expected).to.be.equal('test1')
+      }, 100)
+      expect(throttle).to.be.a('function')
+    })
+    it('loopArray', function () {
+      const {
+        loopArray
+      } = utils
+      expect(loopArray).to.be.a('function')
+      expect(loopArray()).to.be.equal(undefined)
+      expect(loopArray([1, 2, 3], 1)).to.be.deep.equal([3, 1, 2])
+      expect(loopArray([1, 2, 3], -1)).to.be.deep.equal([2, 3, 1])
+      expect(loopArray([1, 2, 3], 0)).to.be.deep.equal([1, 2, 3])
     })
   })
 })
