91a30cab167661bc4f1e237bb3bfd673d83ddfdf
atomtong <tong_huabing@qq.com>
2017-07-06 14:36:57 +0800

    * [test] add test case for downgrade/viewport /api-support for vue

---
+++ testVue/Weex-Api-Support.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper">
+    <div v-for="(key, i) in conditions" class="line">
+      <text class="key">{{key}}</text>
+      <text :class="['value', results[i] ? 'green' : 'red']">{{results[i]}}</text>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                results: [],
+                conditions: [
+                    '@component/div',
+                    '@component/slider',
+                    '@component/countdown',
+
+                    '@module/stream',
+                    '@module/mtop',
+                    '@module/dom',
+                    '@module/abcd',
+                    '@module/abcd.efgh',
+                    '@module/picker.pick',
+                    '@module/stream.fetch',
+                    '@module/stream.abcde'
+                ]
+            }
+        },
+        created() {
+            this.results = this.conditions.map(key => {
+                try {
+                    return weex.supports(key)
+                } catch (e) {
+                    return 'unknown'
+                }
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper {
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+    .line {
+        flex-direction: row;
+        margin-bottom: 16px;
+    }
+    
+    .key {
+        width: 480px;
+        font-size: 40px;
+        margin-left: 60px;
+        color: #888888;
+    }
+    
+    .value {
+        font-size: 40px;
+        color: #555555;
+    }
+    
+    .green {
+        color: green;
+    }
+    
+    .red {
+        color: red;
+    }
+</style>
