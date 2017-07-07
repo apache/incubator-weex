02f9ff5d6b17d56faf66b7b4be93c8cb51d45436
erha19 <faterrole@gmail.com>
2017-07-07 18:06:55 +0800

    + [html5] add utils/lazyload unit test

---
+++ html5/test/render/vue/utils/lazyload.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import * as lazyload from '../../../../render/vue/utils/lazyload'
+describe('utils', function () {
+  describe('lazyload', function () {
+    before(function () {
+      this.clock = sinon.useFakeTimers()
+    })
+    after(function () {
+      this.clock.restore()
+    })
+    it('applySrc', function () {})
+    it('fireLazyload', function () {
+      const {
+        fireLazyload
+      } = lazyload
+      const node = document.createElement('figure')
+      node.setAttribute('img-src', 'img-src.jpg')
+      node.setAttribute('img-placeholder', 'img-placeholder.jpg')
+      document.body.appendChild(node)
+      fireLazyload(node, true)
+      expect(node.style.backgroundImage).to.be.equal('url(http://localhost:9876/img-src.jpg)')
+    })
+    it('getThrottleLazyload', function () {
+      const {
+        getThrottleLazyload
+      } = lazyload
+      const node = document.createElement('figure')
+      node.setAttribute('img-src', 'img-src.jpg')
+      node.setAttribute('img-placeholder', 'img-placeholder.jpg')
+      document.body.appendChild(node)
+      const throttle = getThrottleLazyload(100, node)
+      expect(throttle).to.be.a('function')
+    //   throttle()
+    //   this.clock.tick(100)
+    //   console.log(node.style.backgroundImage)
+    //   expect(node.style.backgroundImage).to.be.equal('url(http://localhost:9876/img-src.jpg)')
+    })
+  })
+})
