784c9f398bf102c107374a9210c0d84bf082d416
acton393 <zhangxing610321@gmail.com>
2017-07-17 15:33:18 +0800

    * [ios] add more note about get component method @nodanger

--- ios/sdk/WeexSDK/Sources/Module/WXDomModule.m
+++ ios/sdk/WeexSDK/Sources/Module/WXDomModule.m
@@ -203,9 +203,10 @@ WX_EXPORT_METHOD(@selector(getComponentRect:callback:))
                     [callbackRsp setObject:[NSString stringWithFormat:@"Illegal parameter, no ref about \"%@\" can be found", ref] forKey:@"errMsg"];
                 } else {
                     CGRect componentRect = CGRectZero;
+                    // if current component view is not loaded or it hasn't been inserted to its superview, so the position cannot be obtained correct except width and height
                     if ([component isViewLoaded] && component.view.superview) {
                         componentRect = [component.view.superview convertRect:component.view.frame toView:rootView];
-                    }else {
+                    } else {
                         componentRect = component.calculatedFrame;
                     }
                     callbackRsp = [strongSelf _componentRectInfoWithViewFrame:componentRect];
