b3e404937bafa77cee4544c97598c1c4ec6ef2ed
齐山 <sunjjbobo@163.com>
2017-07-23 21:30:45 +0800

    + [ios] support extend call native for third part

--- ios/sdk/WeexSDK/Sources/Bridge/WXJSCoreBridge.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXJSCoreBridge.m
@@ -34,6 +34,7 @@
 #import "JSValue+Weex.h"
 #import "WXJSExceptionProtocol.h"
 #import "WXSDKManager.h"
+#import "WXExtendCallNativeProtocol.h"
 
 #import <dlfcn.h>
 
@@ -102,6 +103,9 @@
             NSString *base64Decoded = [[NSString alloc]
                                        initWithData:nsdataFromBase64String encoding:NSUTF8StringEncoding];
             return base64Decoded;
+        };
+        _jsContext[@"extendCallNative"] = ^(JSValue *value ) {
+            return [weakSelf extendCallNative:[value toDictionary]];
         };
         
         _jsContext[@"nativeLog"] = ^() {
@@ -496,4 +500,14 @@
         [_timers removeObject:ret];
     }
 }
+
+-(id)extendCallNative:(NSDictionary *)dict
+{
+    id extendCallNative = [WXSDKEngine handlerForProtocol:@protocol(WXExtendCallNativeProtocol)];
+    if(extendCallNative){
+        return [extendCallNative excuteCallNative:dict];
+    }
+    return @(-1);
+}
+
 @end
