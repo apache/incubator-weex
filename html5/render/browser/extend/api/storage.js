c01f87d130770f08b48361317a73b885fdfb030b
行久 <yiyuan.zhangyy@alibaba-inc.com>
2017-08-14 14:20:48 +0800

    [html5] commit update. and fire CI run again. @notdanger

--- html5/render/browser/extend/api/storage.js
+++ html5/render/browser/extend/api/storage.js
@@ -32,7 +32,7 @@ const storage = {
    * When passed a key name and value, will add that key to the storage,
    * or update that key's value if it already exists.
    * @param {string} key
-   * @param {string} value
+   * @param {string} value not null nor undifined，but 0 works
    * @param {function} callbackId
    */
   setItem: function (key, value, callbackId) {
