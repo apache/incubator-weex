995bece0c0060376a9a85dde5604350a4dc398bc
gurisxie <279483595@qq.com>
2017-08-25 14:01:21 +0800

    * [test] update text tc

--- test/scripts/components/text.test.js
+++ test/scripts/components/text.test.js
@@ -18,7 +18,6 @@ describe('weex text @ignore-ios', function () {
     return util.init(driver)
       .get(util.getPage('/components/text-layout.js'))
       .sleep(2000)
-      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .getWindowSize()
       .then(size => {
         screenWidth = size.width
@@ -32,6 +31,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#2 Text Content', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('text-only')
       .text()
       .then((text) => {
@@ -41,6 +41,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#3 Line Height', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('lines')
       .getRect()
       .then(rect => {
@@ -52,6 +53,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#4 Font Size', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('font')
       .getRect()
       .then(rect => {
@@ -63,6 +65,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#5 Fixed-Size', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('fixed-size')
       .getRect()
       .then(rect => {
@@ -74,6 +77,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#6 flex:1; align-Items: stretch; flex-direction:row', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('flexgrow-alignitems')
       .getRect()
       .then(rect => {
@@ -85,6 +89,7 @@ describe('weex text @ignore-ios', function () {
 
   it('#7 flex:1; align-Items: center; flex-direction:row', () => {
     return driver
+      .waitForElementById("lines", util.getGETActionWaitTimeMills(), 1000)
       .elementById('flexgrow')
       .getRect()
       .then(rect => {
