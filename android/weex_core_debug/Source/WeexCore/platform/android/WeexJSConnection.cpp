10c1eea7a266128958d380b30e4be7cf0bdba14d
御魂 <wentao.swt@alibaba-inc.com>
2017-09-27 17:42:21 +0800

    Fix runtime bugs caused by environ miss.

--- android/weex_core_debug/Source/WeexCore/platform/android/WeexJSConnection.cpp
+++ android/weex_core_debug/Source/WeexCore/platform/android/WeexJSConnection.cpp
@@ -1,16 +1,26 @@
 #include "WeexJSConnection.h"
 
+#include "ashmem.h"
 #include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <string>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/mman.h>
 #include <IPC/IPCFutexPageQueue.h>
 #include <IPC/IPCException.h>
 #include <IPC/IPCSender.h>
 #include <base/LogUtils.h>
 #include <IPC/ashmem.h>
 
+#include <unistd.h>
+
+#include <sys/mman.h>
+#include <vector>
 
 static void doExec(int fd, bool traceEnable);
 
@@ -19,12 +29,13 @@ static void closeAllButThis(int fd);
 extern const char *s_cacheDir;
 
 struct WeexJSConnection::WeexJSConnectionImpl {
-  std::unique_ptr<IPCSender> serverSender;
-  std::unique_ptr<IPCFutexPageQueue> futexPageQueue;
-  pid_t child{0};
+    std::unique_ptr<IPCSender> serverSender;
+    std::unique_ptr<IPCFutexPageQueue> futexPageQueue;
+    pid_t child{0};
 };
 
-WeexJSConnection::WeexJSConnection() : m_impl(new WeexJSConnectionImpl) {
+WeexJSConnection::WeexJSConnection()
+        : m_impl(new WeexJSConnectionImpl) {
 }
 
 WeexJSConnection::~WeexJSConnection() {
@@ -44,7 +55,7 @@ IPCSender *WeexJSConnection::start(IPCHandler *handler) {
     throw IPCException("failed to map ashmem region: %s", strerror(_errno));
   }
   std::unique_ptr<IPCFutexPageQueue> futexPageQueue(
-      new IPCFutexPageQueue(base, IPCFutexPageQueue::ipc_size, 0));
+          new IPCFutexPageQueue(base, IPCFutexPageQueue::ipc_size, 0));
   std::unique_ptr<IPCSender> sender(createIPCSender(futexPageQueue.get(), handler));
   m_impl->serverSender = std::move(sender);
   m_impl->futexPageQueue = std::move(futexPageQueue);
@@ -119,7 +130,10 @@ static void findPath(std::string &executablePath, std::string &icuDataPath) {
       continue;
     if (val > target) {
       executablePath.assign(strstr(end, "/"));
-      executablePath = executablePath.substr(0, executablePath.rfind('/'));
+      std::size_t found = executablePath.rfind('/');
+      if (found != std::string::npos) {
+        executablePath = executablePath.substr(0, found);
+      }
     }
     if (!executablePath.empty()
         && !icuDataPath.empty()) {
@@ -128,6 +142,48 @@ static void findPath(std::string &executablePath, std::string &icuDataPath) {
   }
   fclose(f);
   return;
+}
+
+class EnvPBuilder {
+public:
+    EnvPBuilder();
+
+    ~EnvPBuilder() = default;
+
+    void addNew(const char *n);
+
+    std::unique_ptr<const char *[]> build();
+
+private:
+    std::vector<const char *> m_vec;
+};
+
+EnvPBuilder::EnvPBuilder() {
+  for (char **env = environ; *env; env++) {
+    // fixme:add for ANDROID_ROOT envp
+    // if cannot find some env, can use such as
+    // PATH/ANDROID_BOOTLOGO/ANDROID_ASSETS/ANDROID_DATA/ASEC_MOUNTPOINT
+    // LOOP_MOUNTPOINT/BOOTCLASSPATH and etc
+    // but don't use LD_LIBRARY_PATH env may cause so cannot be found
+    const char *android_root_env = "ANDROID_ROOT=";
+    if (strstr(*env, android_root_env) != nullptr) {
+      addNew(*env);
+      break;
+    }
+  }
+}
+
+void EnvPBuilder::addNew(const char *n) {
+  m_vec.emplace_back(n);
+}
+
+std::unique_ptr<const char *[]> EnvPBuilder::build() {
+  std::unique_ptr<const char *[]> ptr(new const char *[m_vec.size() + 1]);
+  for (size_t i = 0; i < m_vec.size(); ++i) {
+    ptr.get()[i] = m_vec[i];
+  }
+  ptr.get()[m_vec.size()] = nullptr;
+  return ptr;
 }
 
 void doExec(int fd, bool traceEnable) {
@@ -142,30 +198,33 @@ void doExec(int fd, bool traceEnable) {
     LOGE("icuDataPath is empty");
     return;
   }
+
   std::string ldLibraryPathEnv("LD_LIBRARY_PATH=");
   std::string icuDataPathEnv("ICU_DATA_PATH=");
   std::string crashFilePathEnv("CRASH_FILE_PATH=");
-
+  ldLibraryPathEnv.append(executablePath);
+  icuDataPathEnv.append(icuDataPath);
   if (!s_cacheDir) {
-    crashFilePathEnv.append("/data/data/com.taobao.tabao/cache");
+    LOGE("crash log file path s_cacheDir is empty");
+    crashFilePathEnv.append("/data/data/com.taobao.taobao/cache");
   } else {
     crashFilePathEnv.append(s_cacheDir);
   }
-  ldLibraryPathEnv.append(executablePath);
-  icuDataPathEnv.append(icuDataPath);
+
   crashFilePathEnv.append("/jsserver_crash");
   char fdStr[16];
   snprintf(fdStr, 16, "%d", fd);
-  const char *envp[] = {ldLibraryPathEnv.c_str(),
-                        icuDataPathEnv.c_str(),
-                        crashFilePathEnv.c_str(),
-                        nullptr};
+  EnvPBuilder envpBuilder;
+  envpBuilder.addNew(ldLibraryPathEnv.c_str());
+  envpBuilder.addNew(icuDataPathEnv.c_str());
+  envpBuilder.addNew(crashFilePathEnv.c_str());
+  auto envp = envpBuilder.build();
   {
     std::string executableName = executablePath + '/' + "libweexjsb64.so";
     chmod(executableName.c_str(), 0755);
     const char *argv[] = {executableName.c_str(), fdStr, traceEnable ? "1" : "0", nullptr};
-    if (-1 ==
-        execve(argv[0], const_cast<char *const *>(&argv[0]), const_cast<char *const *>(envp))) {
+    if (-1 == execve(argv[0], const_cast<char *const *>(&argv[0]),
+                     const_cast<char *const *>(envp.get()))) {
       LOGE("execve failed: %s", strerror(errno));
     }
   }
@@ -173,8 +232,8 @@ void doExec(int fd, bool traceEnable) {
     std::string executableName = executablePath + '/' + "libweexjsb.so";
     chmod(executableName.c_str(), 0755);
     const char *argv[] = {executableName.c_str(), fdStr, traceEnable ? "1" : "0", nullptr};
-    if (-1 ==
-        execve(argv[0], const_cast<char *const *>(&argv[0]), const_cast<char *const *>(envp))) {
+    if (-1 == execve(argv[0], const_cast<char *const *>(&argv[0]),
+                     const_cast<char *const *>(envp.get()))) {
       LOGE("execve failed: %s", strerror(errno));
     }
   }
