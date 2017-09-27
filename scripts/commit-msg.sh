634c6413c1e715cb3174b1b039198f339df25736
Jonathan Dong <dong.yan@aliyun.com>
2017-09-27 23:18:51 +0800

    [WEEX-74] Added Jira issue number as commit log title
    As Apache Jira provides the function that can automatically connect
    commit and Jira issue according to the issue number with specific
    format, so added a new way in commit message title check regex to
    allow this.
    
    Removed the Merge title because we are not going to allow merge
    and start to follow the rebase way.
    
    Also updated error message to point to the correct URL.

--- scripts/commit-msg.sh
+++ scripts/commit-msg.sh
@@ -17,11 +17,11 @@
 # under the License.
 
 # Validate commit log
-commit_regex='^Merge.+|[+*-] \[(android|ios|jsfm|html5|component|doc|build|website|example|test|all)\] .{1,50}'
+commit_regex='[+*-] \[(android|ios|jsfm|html5|component|doc|build|website|example|test|all)\] .{1,50}|\[WEEX-[0-9]*\] .{1,50}'
 
 if ! grep -iqE "$commit_regex" "$1"; then
-    echo "ERROR: commit log format is not correct!"
-    echo "See https://github.com/alibaba/weex/blob/dev/CONTRIBUTING.md#commit-log"
+    echo "ERROR: Incorrect commit log format, please refer to:"
+    echo "https://github.com/apache/incubator-weex/blob/master/CONTRIBUTING.md#commit-log"
     exit 1
 fi
 # FIXME no effect after editor (like vim) exits
