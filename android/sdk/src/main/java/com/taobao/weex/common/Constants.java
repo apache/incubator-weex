067bf140570b34b6e3f303ca3f5efb1f5e028f4c
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2017-10-23 15:15:43 +0800

    image recycle enable  auto release image reduce memory usage

--- android/sdk/src/main/java/com/taobao/weex/common/Constants.java
+++ android/sdk/src/main/java/com/taobao/weex/common/Constants.java
@@ -109,6 +109,7 @@ public class Constants {
     String SRC = "src";
     String PLACE_HOLDER = "placeHolder";
     String RESIZE_MODE = "resizeMode";
+    String AUTO_RECYCLE = "autoRecycle";
     String SHOW_INDICATORS = "showIndicators";
     String AUTO_PLAY = "autoPlay";
     String SCROLL_DIRECTION = "scrollDirection";
