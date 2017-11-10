815fa337e454b7782971024a5dad3dfb202cab1f
xusw <xusw21@gmail.com>
2017-11-10 16:15:31 +0800

    * [ios] fix #WEEX-88, support BOOL type argument bridging when in 32-bit iOS and add related unit test.

---
+++ ios/sdk/WeexSDKTests/WXTestBridgeMethodDummy.h
@@ -0,0 +1,13 @@
+//
+//  WXTestBridgeMethodDummy.h
+//  WeexSDK
+//
+//  Created by 远怀 on 2017/10/27.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXTestBridgeMethodDummy : NSObject
+
+@end
---
+++ ios/sdk/WeexSDKTests/WXTestBridgeMethodDummy.m
@@ -0,0 +1,17 @@
+//
+//  WXTestBridgeMethodDummy.m
+//  WeexSDK
+//
+//  Created by 远怀 on 2017/10/27.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import "WXTestBridgeMethodDummy.h"
+
+@implementation WXTestBridgeMethodDummy
+
+- (void)methodWithBOOLArg:(BOOL)arg {
+    // No implementaion here, just for testing.
+}
+
+@end
