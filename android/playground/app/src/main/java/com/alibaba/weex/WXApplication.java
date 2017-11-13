280752bef945068a060f55b07a75840e935a269d
misakuo <misakuo@apache.org>
2017-11-13 11:37:08 +0800

    * [android] support role property to accessibility * [android] add license header close #912 * [android] remove cn strings * [android] fix en role

--- android/playground/app/src/main/java/com/alibaba/weex/WXApplication.java
+++ android/playground/app/src/main/java/com/alibaba/weex/WXApplication.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 import com.alibaba.weex.commons.adapter.DefaultWebSocketAdapterFactory;
 import com.alibaba.weex.commons.adapter.ImageAdapter;
 import com.alibaba.weex.commons.adapter.JSExceptionAdapter;
+import com.alibaba.weex.extend.adapter.DefaultAccessibilityRoleAdapter;
 import com.alibaba.weex.extend.adapter.InterceptWXHttpAdapter;
 import com.alibaba.weex.extend.component.RichText;
 import com.alibaba.weex.extend.component.WXComponentSyncTest;
@@ -70,6 +71,8 @@ public class WXApplication extends Application {
                                .setHttpAdapter(new InterceptWXHttpAdapter())
                                .build()
                           );
+
+    WXSDKManager.getInstance().setAccessibilityRoleAdapter(new DefaultAccessibilityRoleAdapter());
 
     try {
       Fresco.initialize(this);
