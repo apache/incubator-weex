47e8f2ce0f87ccc16315ead6d157a466b13c3770
YorkShen <shenyuancs@gmail.com>
2017-11-29 18:02:24 +0800

    * [test] Update FlexWrapTest.

--- test/lib-cplus/case/FlexWrapTest.cpp
+++ test/lib-cplus/case/FlexWrapTest.cpp
@@ -7,34 +7,293 @@
 
 using namespace WXCoreFlexLayout;
 using namespace testing;
+using namespace std;
 
-class FlexWrap : public TestWithParam<tuple<WXCoreFlexDirection, WXCoreFlexWrap>> {
+constexpr float margin = 10;
+constexpr float childWidth = 150;
+constexpr float childHeight = 100;
+constexpr float targetWidth = 700;
+constexpr float targetHeight = 1000;
+constexpr int childCount = 10;
+
+class FlexWrap : public Test {
 protected:
     FlexWrap() {
-        WXCoreFlexDirection direction = get<0>(GetParam());
-        WXCoreFlexWrap wrap = get<1>(GetParam());
         root = WXCoreLayoutNode::newWXCoreNode();
-        root->setStyleWidth(700);
-        root->setStyleHeight(1000);
-        root->setFlexDirection(direction);
-        root->setFlexWrap(wrap);
-        for (auto i = 0; i < 10; i++) {
-            WXCoreLayoutNode *child = WXCoreLayoutNode::newWXCoreNode();
-            child->setStyleWidth(200);
-            child->setStyleHeight(300);
-            root->addChildAt(child, i);
-        }
+        target = WXCoreLayoutNode::newWXCoreNode();
+        firstChildInTarget = nullptr;
 
+        root->setStyleWidth(750);
+        root->setStyleHeight(1200);
+        target->setStyleWidth(targetWidth);
+        target->setStyleHeight(targetHeight);
+
+        for (auto i = 0; i < childCount; i++) {
+            WXCoreLayoutNode *child = WXCoreLayoutNode::newWXCoreNode();
+            if (firstChildInTarget == nullptr) {
+                firstChildInTarget = child;
+            }
+            child->setMargin(WXCore_Margin_ALL, margin);
+            child->setPadding(WXCore_Padding_ALL, 10);
+            child->setBorderWidth(WXCore_Border_Width_ALL, 10);
+            child->setStyleWidth(childWidth);
+            child->setStyleHeight(childHeight);
+            lastChildInTarget = child;
+            target->appendChild(child);
+        }
+        root->appendChild(target);
     }
 
     ~FlexWrap() override {
         root->freeWXCoreNode();
+        target->freeWXCoreNode();
         FormatingContext bfc = NON_BFC;
         for (auto i = 0; i < root->getChildCount(bfc); i++) {
             root->getChildAt(bfc, i)->freeWXCoreNode();
         }
     }
 
-    WXCoreLayoutNode *root;
+    WXCoreLayoutNode *root, *target, *firstChildInTarget, *lastChildInTarget;
 };
 
+TEST_F(FlexWrap, NoWrapRow) {
+    target->setFlexWrap(WXCore_Wrap_NoWrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Row);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), (childCount - 1) * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(), (childCount) * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+}
+
+TEST_F(FlexWrap, NoWrapRowReverse) {
+    target->setFlexWrap(WXCore_Wrap_NoWrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Row_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), targetWidth - margin - childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), targetWidth - margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(),
+                    targetWidth - (childCount) * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    targetWidth - (childCount - 1) * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+}
+
+TEST_F(FlexWrap, NoWrapColumn) {
+    target->setFlexWrap(WXCore_Wrap_NoWrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Column);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), (childCount - 1) * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(), (childCount) * (childHeight + 2 * margin) - margin);
+}
+
+TEST_F(FlexWrap, NoWrapColumnReverse) {
+    target->setFlexWrap(WXCore_Wrap_NoWrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Column_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), targetHeight - margin - childHeight);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), targetHeight - margin);
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(),
+                    targetHeight - (childCount) * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    targetHeight - (childCount - 1) * (childHeight + 2 * margin) - margin);
+}
+
+
+TEST_F(FlexWrap, WrapRow) {
+    target->setFlexWrap(WXCore_Wrap_Wrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Row);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    auto flexItemCap = static_cast<int>(targetWidth / (childWidth + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), itemsBeforeLast * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    (itemsBeforeLast + 1) * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), linesBeforeLast * (childHeight + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    (linesBeforeLast + 1) * (childHeight + margin * 2) - margin);
+}
+
+TEST_F(FlexWrap, WrapRowReverse) {
+    target->setFlexWrap(WXCore_Wrap_Wrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Row_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), targetWidth - margin - childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), targetWidth - margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    auto flexItemCap = static_cast<int>(targetWidth / (childWidth + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(),
+                    targetWidth - (itemsBeforeLast + 1) * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    targetWidth - itemsBeforeLast * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), linesBeforeLast * (childHeight + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    (linesBeforeLast + 1) * (childHeight + margin * 2) - margin);
+}
+
+TEST_F(FlexWrap, WrapColumn) {
+    target->setFlexWrap(WXCore_Wrap_Wrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Column);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    auto flexItemCap = static_cast<int>(targetHeight / (childHeight + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), linesBeforeLast * (childWidth + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    (linesBeforeLast + 1) * (childWidth + margin * 2) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), itemsBeforeLast * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    (itemsBeforeLast + 1) * (childHeight + 2 * margin) - margin);
+}
+
+TEST_F(FlexWrap, WrapColumnReverse) {
+    target->setFlexWrap(WXCore_Wrap_Wrap);
+    target->setFlexDirection(WXCore_Flex_Direction_Column_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), targetHeight - margin - childHeight);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), targetHeight - margin);
+
+    auto flexItemCap = static_cast<int>(targetHeight / (childHeight + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), linesBeforeLast * (childWidth + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    (linesBeforeLast + 1) * (childWidth + margin * 2) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(),
+                    targetHeight - (itemsBeforeLast + 1) * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    targetHeight - (itemsBeforeLast) * (childHeight + 2 * margin) - margin);
+}
+
+
+TEST_F(FlexWrap, WrapReverseRow) {
+    target->setFlexWrap(WXCore_Wrap_WrapReverse);
+    target->setFlexDirection(WXCore_Flex_Direction_Row);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), margin + childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), targetHeight - margin - childHeight);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), targetHeight - margin);
+
+    auto flexItemCap = static_cast<int>(targetWidth / (childWidth + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(), itemsBeforeLast * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    (itemsBeforeLast + 1) * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(),
+                    targetHeight - (linesBeforeLast + 1) * (childHeight + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    targetHeight - linesBeforeLast * (childHeight + margin * 2) - margin);
+}
+
+TEST_F(FlexWrap, WrapReverseRowReverse) {
+    target->setFlexWrap(WXCore_Wrap_WrapReverse);
+    target->setFlexDirection(WXCore_Flex_Direction_Row_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), targetWidth - margin - childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), targetWidth - margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), targetHeight - margin - childHeight);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), targetHeight - margin);
+
+    auto flexItemCap = static_cast<int>(targetWidth / (childWidth + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(),
+                    targetWidth - (itemsBeforeLast + 1) * (childWidth + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    targetWidth - itemsBeforeLast * (childWidth + 2 * margin) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(),
+                    targetHeight - (linesBeforeLast + 1) * (childHeight + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    targetHeight - linesBeforeLast * (childHeight + margin * 2) - margin);
+}
+
+TEST_F(FlexWrap, WrapReverseColumn) {
+    target->setFlexWrap(WXCore_Wrap_WrapReverse);
+    target->setFlexDirection(WXCore_Flex_Direction_Column);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), targetWidth - margin - childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), targetWidth - margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), margin + childHeight);
+
+    auto flexItemCap = static_cast<int>(targetHeight / (childHeight + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(),
+                    targetWidth - (linesBeforeLast + 1) * (childWidth + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    targetWidth - linesBeforeLast * (childWidth + margin * 2) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(), itemsBeforeLast * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    (itemsBeforeLast + 1) * (childHeight + 2 * margin) - margin);
+}
+
+TEST_F(FlexWrap, WrapReverseColumnReverse) {
+    target->setFlexWrap(WXCore_Wrap_WrapReverse);
+    target->setFlexDirection(WXCore_Flex_Direction_Column_Reverse);
+    root->calculateLayout();
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionLeft(), targetWidth - margin - childWidth);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionRight(), targetWidth - margin);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionTop(), targetHeight - margin - childHeight);
+    EXPECT_FLOAT_EQ(firstChildInTarget->getLayoutPositionBottom(), targetHeight - margin);
+
+    auto flexItemCap = static_cast<int>(targetHeight / (childHeight + 2 * margin));
+    int linesBeforeLast = childCount / flexItemCap;
+    int itemsBeforeLast = childCount % flexItemCap - 1;
+
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionLeft(),
+                    targetWidth - (linesBeforeLast + 1) * (childWidth + margin * 2) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionRight(),
+                    targetWidth - linesBeforeLast * (childWidth + margin * 2) - margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionTop(),
+                    targetHeight - (itemsBeforeLast + 1) * (childHeight + 2 * margin) + margin);
+    EXPECT_FLOAT_EQ(lastChildInTarget->getLayoutPositionBottom(),
+                    targetHeight - (itemsBeforeLast) * (childHeight + 2 * margin) - margin);
+}
