ed0920180eaed2f57a6ce5f4c822d024c7444799
YorkShen <shenyuancs@gmail.com>
2017-12-11 22:59:50 +0800

    * [test] Update Alignment Test.

--- test/lib-cplus/case/AlignItemsAlignSelfTest.cpp
+++ test/lib-cplus/case/AlignItemsAlignSelfTest.cpp
@@ -73,27 +73,74 @@ TEST_P(AlignmentItemsAlignSelf, row) {
     string expectedCase("Row/AlignmentItemsAlignSelf");
     string actualCase(UnitTest::GetInstance()->current_test_info()->test_case_name());
     if (expectedCase == actualCase) {
+        WXCoreAlignItems alignItems = get<2>(GetParam());
+        WXCoreAlignSelf alignSelf = get<3>(GetParam());
+        float height = 0;
+        float alignSelfWithoutCrossSizeBottom = -1;
         if (get<1>(GetParam()) == WXCore_Wrap_Wrap) {
-            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), childWidth);
-            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutHeight(), 0);
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutWidth(), childWidth);
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutHeight(), childHeight);
-
             auto flexCap = static_cast<int>(targetWidth / childWidth);
             auto linesBeforeLast = childCount / flexCap - (childCount % flexCap == 0 ? 1 : 0);
             auto itemsBeforeLast = childCount % flexCap == 0 ? flexCap - 1 : childCount % flexCap - 1;
+            float alignItemsWithCrossSizeTop, alignItemsWithCrossSizeBottom;
+            if (flexCap >= childCount) {
+                if (alignSelf == WXCore_AlignSelf_Stretch ||
+                    (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
+                    height = targetHeight;
+                    alignSelfWithoutCrossSizeBottom = targetHeight;
+                }
 
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionLeft(), itemsBeforeLast * childWidth);
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionRight(), (itemsBeforeLast + 1) * childWidth);
+                switch (alignItems) {
+                    case WXCore_AlignItems_Stretch:
+                    case WXCore_AlignItems_Flex_Start:
+                        alignItemsWithCrossSizeTop = 0;
+                        alignItemsWithCrossSizeBottom = childHeight;
+                        break;
+                    case WXCore_AlignItems_Center:
+                        alignItemsWithCrossSizeTop = targetHeight / 2 - childHeight / 2;
+                        alignItemsWithCrossSizeBottom = targetHeight / 2 + childHeight / 2;
+                        break;
+                    case WXCore_AlignItems_Flex_End:
+                        alignItemsWithCrossSizeTop = targetHeight - childHeight;
+                        alignItemsWithCrossSizeBottom = targetHeight;
+                        break;
+                    default:
+                        alignItemsWithCrossSizeTop = -1;
+                        alignItemsWithCrossSizeBottom = -1;
+                        break;
+                }
+            } else {
+                if (alignSelf == WXCore_AlignSelf_Stretch ||
+                    (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
+                    height = childHeight;
+                    alignSelfWithoutCrossSizeBottom = childHeight;
+                }
+                alignItemsWithCrossSizeTop = linesBeforeLast * childHeight;
+                alignItemsWithCrossSizeBottom = (linesBeforeLast + 1) * childHeight;
+            }
+
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), childWidth);
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutHeight(), height);
+            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutWidth(), childWidth);
+            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutHeight(), childHeight);
+
+            if (alignItemsWithCrossSizeTop != -1 && alignItemsWithCrossSizeBottom != -1) {
+                EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionLeft(), itemsBeforeLast * childWidth);
+                EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionRight(), (itemsBeforeLast + 1) * childWidth);
+            }
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionTop(), linesBeforeLast * childHeight);
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionBottom(), (linesBeforeLast + 1) * childHeight);
+
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionLeft(), 0);
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionRight(), childWidth);
+            if (alignSelfWithoutCrossSizeBottom != -1) {
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionTop(), 0);
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionBottom(), alignSelfWithoutCrossSizeBottom);
+            }
         } else if (get<1>(GetParam()) == WXCore_Wrap_NoWrap) {
-            WXCoreAlignItems alignItems = get<2>(GetParam());
-            WXCoreAlignSelf alignSelf = get<3>(GetParam());
-            float height = 0;
             if (alignSelf == WXCore_AlignSelf_Stretch ||
                 (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
                 height = targetHeight;
+                alignSelfWithoutCrossSizeBottom = targetHeight;
             }
 
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), childWidth);
@@ -114,19 +161,11 @@ TEST_P(AlignmentItemsAlignSelf, row) {
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionBottom(),
                             alignItemsWithCrossSizeTop + childHeight);
 
-            float alignSelfWithoutCrossSizeTop = -1;
-            if (alignSelf == WXCore_AlignSelf_Auto) {
-                alignSelfWithoutCrossSizeTop = alignItemsWithCrossSizeTop;
-            } else if (alignSelf == WXCore_AlignSelf_Stretch) {
-                alignSelfWithoutCrossSizeTop = targetHeight;
-            }
-
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionLeft(), 0);
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionRight(), childWidth);
-            if (alignSelfWithoutCrossSizeTop != -1) {
-                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionTop(), alignSelfWithoutCrossSizeTop);
-                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionBottom(),
-                                alignSelfWithoutCrossSizeTop + height);
+            if (alignSelfWithoutCrossSizeBottom != -1) {
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionTop(), 0);
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionBottom(), alignSelfWithoutCrossSizeBottom);
             }
         }
     }
@@ -136,27 +175,74 @@ TEST_P(AlignmentItemsAlignSelf, column) {
     string expectedCase("Column/AlignmentItemsAlignSelf");
     string actualCase(UnitTest::GetInstance()->current_test_info()->test_case_name());
     if (expectedCase == actualCase) {
+        WXCoreAlignItems alignItems = get<2>(GetParam());
+        WXCoreAlignSelf alignSelf = get<3>(GetParam());
+        float width = 0;
+        float alignSelfWithoutCrossSizeRight = -1;
         if (get<1>(GetParam()) == WXCore_Wrap_Wrap) {
-            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), 0);
+            auto flexCap = static_cast<int>(targetHeight / childHeight);
+            auto linesBeforeLast = childCount / flexCap - (childCount % flexCap == 0 ? 1 : 0);
+            auto itemsBeforeLast = childCount % flexCap == 0 ? flexCap - 1 : childCount % flexCap - 1;
+            float alignItemsWithCrossSizeLeft, alignItemsWithCrossSizeRight;
+            if (flexCap >= childCount) {
+                if (alignSelf == WXCore_AlignSelf_Stretch ||
+                    (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
+                    width = targetWidth;
+                    alignSelfWithoutCrossSizeRight = targetWidth;
+                }
+
+                switch (alignItems) {
+                    case WXCore_AlignItems_Stretch:
+                    case WXCore_AlignItems_Flex_Start:
+                        alignItemsWithCrossSizeLeft = 0;
+                        alignItemsWithCrossSizeRight = childWidth;
+                        break;
+                    case WXCore_AlignItems_Center:
+                        alignItemsWithCrossSizeLeft = targetWidth / 2 - childWidth / 2;
+                        alignItemsWithCrossSizeRight = targetWidth / 2 + childWidth / 2;
+                        break;
+                    case WXCore_AlignItems_Flex_End:
+                        alignItemsWithCrossSizeLeft = targetWidth - childWidth;
+                        alignItemsWithCrossSizeRight = targetWidth;
+                        break;
+                    default:
+                        alignItemsWithCrossSizeLeft = -1;
+                        alignItemsWithCrossSizeRight = -1;
+                        break;
+                }
+            } else {
+                if (alignSelf == WXCore_AlignSelf_Stretch ||
+                    (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
+                    width = childWidth;
+                    alignSelfWithoutCrossSizeRight = childWidth;
+                }
+                alignItemsWithCrossSizeLeft = linesBeforeLast * childWidth;
+                alignItemsWithCrossSizeRight = (linesBeforeLast + 1) * childWidth;
+            }
+
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), width);
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutHeight(), childHeight);
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutWidth(), childWidth);
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutHeight(), childHeight);
 
-            auto flexCap = static_cast<int>(targetHeight / childHeight);
-            auto linesBeforeLast = childCount / flexCap - (childCount % flexCap == 0 ? 1 : 0);
-            auto itemsBeforeLast = childCount % flexCap == 0 ? flexCap - 1 : childCount % flexCap - 1;
-
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionLeft(), linesBeforeLast * childWidth);
-            EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionRight(), (linesBeforeLast + 1) * childWidth);
+            if (alignItemsWithCrossSizeLeft != -1 && alignItemsWithCrossSizeRight != -1) {
+                EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionLeft(), alignItemsWithCrossSizeLeft);
+                EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionRight(), alignItemsWithCrossSizeRight);
+            }
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionTop(), itemsBeforeLast * childHeight);
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionBottom(), (itemsBeforeLast + 1) * childHeight);
+
+            if (alignSelfWithoutCrossSizeRight != -1) {
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionLeft(), 0);
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionRight(), alignSelfWithoutCrossSizeRight);
+            }
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionTop(), 0);
+            EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionBottom(), childHeight);
         } else if (get<1>(GetParam()) == WXCore_Wrap_NoWrap) {
-            WXCoreAlignItems alignItems = get<2>(GetParam());
-            WXCoreAlignSelf alignSelf = get<3>(GetParam());
-            float width = 0;
             if (alignSelf == WXCore_AlignSelf_Stretch ||
                 (alignItems == WXCore_AlignItems_Stretch && alignSelf == WXCore_AlignSelf_Auto)) {
                 width = targetWidth;
+                alignSelfWithoutCrossSizeRight = targetWidth;
             }
 
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutWidth(), width);
@@ -177,19 +263,9 @@ TEST_P(AlignmentItemsAlignSelf, column) {
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionTop(), (childCount - 1) * childHeight);
             EXPECT_FLOAT_EQ(alignItemsWithCrossSize->getLayoutPositionBottom(), childCount * childHeight);
 
-            float alignSelfWithoutCrossSizeLeft = -1;
-            if (alignSelf == WXCore_AlignSelf_Auto) {
-                alignSelfWithoutCrossSizeLeft = alignItemsWithCrossSizeLeft;
-            } else if (alignSelf == WXCore_AlignSelf_Stretch) {
-                alignSelfWithoutCrossSizeLeft = targetWidth;
-            }
-
-
-            if (alignSelfWithoutCrossSizeLeft != -1) {
-                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionLeft(), alignSelfWithoutCrossSizeLeft);
-                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionRight(),
-                                alignSelfWithoutCrossSizeLeft + childWidth);
-
+            if (alignSelfWithoutCrossSizeRight != -1) {
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionLeft(), 0);
+                EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionRight(), alignSelfWithoutCrossSizeRight);
             }
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionTop(), 0);
             EXPECT_FLOAT_EQ(alignSelfWithoutCrossSize->getLayoutPositionBottom(), childHeight);
