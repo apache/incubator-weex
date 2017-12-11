c5357041cd6aa23b79072219014d3117fff5ae4c
YorkShen <shenyuancs@gmail.com>
2017-12-11 23:47:09 +0800

    * [test] Update Margin Test

--- test/lib-cplus/case/MarginTest.cpp
+++ test/lib-cplus/case/MarginTest.cpp
@@ -243,10 +243,10 @@ TEST_F(Margin, MarginWidthEdgeOverrideShorthand) {
 TEST_F(Margin, MarginDimension) {
     root->setMargin(marginAll, 200);
     root->calculateLayout();
-    EXPECT_FLOAT_EQ(root->getLayoutWidth(), 300);
-    EXPECT_FLOAT_EQ(root->getLayoutHeight(), 500);
+    EXPECT_FLOAT_EQ(root->getLayoutWidth(), 100);
+    EXPECT_FLOAT_EQ(root->getLayoutHeight(), 300);
     EXPECT_FLOAT_EQ(root->getLayoutPositionLeft(), 200);
     EXPECT_FLOAT_EQ(root->getLayoutPositionRight(), 300);
-    EXPECT_FLOAT_EQ(root->getLayoutPositionTop(), 500);
+    EXPECT_FLOAT_EQ(root->getLayoutPositionTop(), 200);
     EXPECT_FLOAT_EQ(root->getLayoutPositionBottom(), 500);
 }
