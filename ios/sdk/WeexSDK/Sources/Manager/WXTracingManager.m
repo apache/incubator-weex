080807289c22affee5afa918d7b7cced178b9c6e
acton393 <zhangxing610321@gmail.com>
2017-12-12 14:56:44 +0800

    [WEEX-240][iOS] support sandbox on iOS * [iOS] bugfix bridgeManger mutli instance
    * [iOS] support multi-context
    
    * [iOS] handle  common javaScript context exposed method
    
    * [iOS] add miss environment information
    
    * [iOS] fix dead lock about dealloc object
    
    * [iOS] try to protect crash when set nil object in dictionary
    
    * [iOS] release jsContext object
    
    * [iOS] support multi-context
    
    * [iOS] release jsContext object
    
    * [iOS] pare bundleType dynamic
    
    * [iOS] mark js vm garbage collect when instance destroyed
    
    * [iOS] support multi-context
    
    * [iOS] release jsContext object
    
    * [iOS] pare bundleType dynamic
    
    * [iOS] mark js vm garbage collect when instance destroyed
    
    * [iOS] fix compier warnings
    
    * [iOS] set the protoType of Vue to the protoType of globalObject
    
    * [iOS] preserve nativeLog function
    
    * [iOS] fix compiler complain
    
    * [jsfm] update jsfm version
    
    * [iOS] use boolValue about the config
    
    * [iOS] bugfix bridgeManger mutli instance
    
    * [iOS] support multi-context
    
    * [iOS] fix dead lock about dealloc object
    
    * [iOS] try to protect crash when set nil object in dictionary
    
    * [iOS] release jsContext object
    
    * [iOS] pare bundleType dynamic
    
    * [iOS] mark js vm garbage collect when instance destroyed
    
    * [iOS] bugfix bridgeManger mutli instance
    
    * [iOS] support multi-context
    
    * [iOS] handle  common javaScript context exposed method
    
    * [iOS] add miss environment information
    
    * [iOS] fix dead lock about dealloc object
    
    * [iOS] try to protect crash when set nil object in dictionary
    
    * [iOS] release jsContext object
    
    * [iOS] bugfix bridgeManger mutli instance
    
    * [iOS] support multi-context
    
    * [iOS] handle  common javaScript context exposed method
    
    * [iOS] fix dead lock about dealloc object
    
    * [iOS] try to protect crash when set nil object in dictionary
    
    * [iOS] release jsContext object
    
    * [iOS] pare bundleType dynamic
    
    * [iOS] mark js vm garbage collect when instance destroyed
    
    * [iOS] fix compier warnings
    
    * [iOS] set the protoType of Vue to the protoType of globalObject
    
    * [iOS] set pageName is empty string if it is null
    
    * [iOS] update config
    
    * [iOS] callJS on instanceContext
    
    * [iOS] update Rax logic
    
    *[iOS] remove useless code
    
    * [iOS] update jsfm about rax
    
    * [iOS] update jsfm
    
    * [iOS] bugfix
    
    * [iOS] remove unused line
    
    * [iOS] trim jsbundle string, so that we can parse the bundleType
    
    * [iOS] downgrade while jsfm restart
    
    * [iOS] add local storage for configuration
    
    * [iOS] add resources for jsfm
    
    * [iOS] downgrade to original process when iOS is less than 9.0
    
    * [iOS] downgrade when iOS version less than 9.0
    
    * [iOS] enable multicontext default
    
    * [iOS] downgrade to single context when iOS lower than 10.0
    
    * [iOS] add globalContext for bridge protocol
    
    * [iOS] fix set env
    
    * [iOS] bugfix about atob & btoa
    
    * [iOS] compatible for rax framework
    
    *[iOS] fix out of range
    
    * [iOS] downgrade to share context as the promise of iOS 10.0
    
    * [iOS] convert to BOOL value from id value
    
    * [iOS] parse javaScript bundleType
    
    + [jsfm] add standalone Promise polyfill for iOS
    
    * [iOS] add poly-fill for iOS so that it can apply to iOS 9 and iOS 8
    
    * [iOS] update project setting for new resources
    
    * [iOS] refactor multi-context logic for devtool adapter
    
    * [iOS] add header
    
    * [iOS] bugfix about evaluate javaScript with sourceURL
    
    * [iOS] clean unused code
    
    * [iOS] callJSBridgeMethod
    
    * [iOS] fix devtool adapter
    
    * [iOS] refactor multi-context logic for devtool adapter (#3)
    
    * [iOS] refactor multi-context logic for devtool adapter
    
    * [iOS] update project settings
    
    * [iOS] fix compiler error
    
    [WEEX-240][iOS] clean up useless code
    
    [WEEX-240] add weexInstanceId for bridgeProtocol
    
    [WEEX-240][iOS] trim newline and whiteSpace character
    
    [WEEX-240][iOS] update instance id when jscore context updated
    
    close #1101

--- ios/sdk/WeexSDK/Sources/Manager/WXTracingManager.m
+++ ios/sdk/WeexSDK/Sources/Manager/WXTracingManager.m
@@ -92,7 +92,9 @@
         if(![[WXTracingManager sharedInstance].tracingTasks objectForKey:tracing.iid]){
             WXTracingTask *task = [WXTracingTask new];
             task.iid = tracing.iid;
-            [[WXTracingManager sharedInstance].tracingTasks setObject:task forKey:tracing.iid];
+            if (tracing.iid) {
+                [[WXTracingManager sharedInstance].tracingTasks setObject:task forKey:tracing.iid];
+            }
         }
         WXTracingTask *task = [[WXTracingManager sharedInstance].tracingTasks objectForKey:tracing.iid];
         if(task.bundleUrl.length == 0){
