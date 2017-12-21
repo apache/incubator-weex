bc96b6525215fe7dde55ad3b11d27e0d98020a47
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2017-12-21 10:41:39 +0800

    [WEEX-179][android]List View Component supported show scrollbar options

--- android/sdk/src/main/java/com/taobao/weex/ui/component/list/BasicListComponent.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/list/BasicListComponent.java
@@ -310,6 +310,8 @@ public abstract class BasicListComponent<T extends ViewGroup & ListComponentView
         }
       }
     });
+
+
     bounceRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
       @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
       @Override
@@ -370,6 +372,11 @@ public abstract class BasicListComponent<T extends ViewGroup & ListComponentView
         boolean draggable = WXUtils.getBoolean(param,false);
         setDraggable(draggable);
         return true;
+      case Constants.Name.SHOW_SCROLLBAR:
+        Boolean result = WXUtils.getBoolean(param,null);
+        if (result != null)
+          setShowScrollbar(result);
+        return true;
     }
     return super.setProperty(key, param);
   }
@@ -398,6 +405,19 @@ public abstract class BasicListComponent<T extends ViewGroup & ListComponentView
       WXLogUtils.d("set draggable : " + isDraggable);
     }
   }
+
+  @WXComponentProp(name = Constants.Name.SHOW_SCROLLBAR)
+  public void setShowScrollbar(boolean show) {
+    if(getHostView() == null || getHostView().getInnerView() == null){
+      return;
+    }
+    if (getOrientation() == Constants.Orientation.VERTICAL) {
+      getHostView().getInnerView().setVerticalScrollBarEnabled(show);
+    } else {
+      getHostView().getInnerView().setHorizontalScrollBarEnabled(show);
+    }
+  }
+
 
   @Override
   public boolean isScrollable() {
