d724832ff8a35f83d6a8de8049eedb446cac0d62
acton393 <zhangxing610321@gmail.com>
2018-01-02 15:50:52 +0800

    * [iOS] revert cancel round pixel

--- ios/sdk/WeexSDK/Sources/Utility/WXUtility.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXUtility.m
@@ -41,8 +41,6 @@
 #define KEY_PASSWORD  @"com.taobao.Weex.123456"
 #define KEY_USERNAME_PASSWORD  @"com.taobao.Weex.weex123456"
 
-static BOOL utilityShouldRoundPixel = NO;
-
 void WXPerformBlockOnMainThread(void (^ _Nonnull block)(void))
 {
     if (!block) return;
@@ -119,29 +117,20 @@ CGFloat WXPixelScale(CGFloat value, CGFloat scaleFactor)
 
 CGFloat WXRoundPixelValue(CGFloat value)
 {
-    if (utilityShouldRoundPixel) {
-        CGFloat scale = WXScreenScale();
-        return round(value * scale) / scale;
-    }
-    return value;
+    CGFloat scale = WXScreenScale();
+    return round(value * scale) / scale;
 }
 
 CGFloat WXCeilPixelValue(CGFloat value)
 {
-    if (utilityShouldRoundPixel) {
-        CGFloat scale = WXScreenScale();
-        return ceil(value * scale) / scale;
-    }
-    return value;
+    CGFloat scale = WXScreenScale();
+    return ceil(value * scale) / scale;
 }
 
 CGFloat WXFloorPixelValue(CGFloat value)
 {
-    if (utilityShouldRoundPixel) {
-        CGFloat scale = WXScreenScale();
-        return floor(value * scale) / scale;
-    }
-    return value;
+    CGFloat scale = WXScreenScale();
+    return floor(value * scale) / scale;
 }
 
 @implementation WXUtility
@@ -163,15 +152,6 @@ CGFloat WXFloorPixelValue(CGFloat value)
 + (void)_performBlock:(void (^)(void))block
 {
     block();
-}
-
-+ (void)setShouldRoudPixel:(BOOL)shouldRoundPixel
-{
-    utilityShouldRoundPixel = shouldRoundPixel;
-}
-+ (BOOL)shouldRoudPixel
-{
-    return utilityShouldRoundPixel;
 }
 
 + (NSDictionary *)getEnvironment
