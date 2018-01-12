3f3c809100c0048b929899c4a1457fcc26bb550b
acton393 <zhangxing610321@gmail.com>
2018-01-12 18:05:27 +0800

    * [iOS] support openURL for scan viewController
    * [iOS] add title bar modules
    
    * [iOS] register titleBar
    
    * [iOS] add titlebar file to project
    
    * [iOS] remove devtool in release mode
    
    * [example] update examples of playground app on ios
    
    * [iOS] fix bug
    
    * [example] update examples of playground app on ios
    
    * [iOS] rewrite navigation handler
    
    * [iOS] bugfix
    
    * [iOS] bugfix about sanner ViewController
    close #985

--- ios/playground/WeexDemo/WXNavigationHandlerImpl.h
+++ ios/playground/WeexDemo/WXNavigationHandlerImpl.h
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,28 +17,9 @@
  * under the License.
  */
 
-#import "WXLocaleModule.h"
+#import <Foundation/Foundation.h>
+#import "WXNavigationProtocol.h"
 
-@implementation WXLocaleModule
-
-WX_EXPORT_METHOD_SYNC(@selector(getLanguage))
-WX_EXPORT_METHOD_SYNC(@selector(getLanguages))
-
-/**
- Get preferred language of the user
- */
-- (NSString *)getLanguage:(WXCallback)callback {
-    if (callback) {
-        callback([NSLocale preferredLanguages][0]);
-    }
-	return [NSLocale preferredLanguages][0];
-}
-
-/**
- Get an array of strings representing the user's preferred languages
- */
-- (NSArray<NSString *> *)getLanguages {
-	return [NSLocale preferredLanguages];
-}
+@interface WXNavigationHandlerImpl : NSObject <WXNavigationProtocol>
 
 @end
