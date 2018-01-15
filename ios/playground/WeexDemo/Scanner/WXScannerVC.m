ba2eb96e46c8bd45ae9db85a7dbdedafd19d4050
acton393 <zhangxing610321@gmail.com>
2018-01-15 14:46:08 +0800

    * [iOS] remove devtool in release mode

--- ios/playground/WeexDemo/Scanner/WXScannerVC.m
+++ ios/playground/WeexDemo/Scanner/WXScannerVC.m
@@ -23,7 +23,9 @@
 #import "UIViewController+WXDemoNaviBar.h"
 #import "WXDemoViewController.h"
 #import "WXDebugTool.h"
+#if DEBUG
 #import <TBWXDevTool/WXDevTool.h>
+#endif
 #import <AudioToolbox/AudioToolbox.h>
 
 @interface WXScannerVC ()
@@ -208,12 +210,14 @@
             }
             return YES;
         } else if ([[elts firstObject] isEqualToString:@"_wx_devtool"]) {
+#ifdef DEBUG
             NSString *devToolURL = [[elts lastObject]  stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
             [WXDevTool launchDevToolDebugWithUrl:devToolURL];
             if ([[[self.navigationController viewControllers] objectAtIndex:0] isKindOfClass:NSClassFromString(@"WXDemoViewController")]) {
                 WXDemoViewController * vc = (WXDemoViewController*)[[self.navigationController viewControllers] objectAtIndex:0];
                 [self.navigationController popToViewController:vc animated:NO];
             }
+#endif
             
             return YES;
         }
