21340d1aec0adb7bea62741c21da4db96b1416e8
miomin <691292118@qq.com>
2018-02-01 15:36:40 +0800

    Revert "[Core] Fix some crash due to refrence null exception."
    This reverts commit 7e221b9fd90e33431ee453b95c8667de2151944e.

--- android/weex_core_debug/Source/WeexCore/render/node/factory/RenderCreator.h
+++ android/weex_core_debug/Source/WeexCore/render/node/factory/RenderCreator.h
@@ -35,7 +35,7 @@ namespace WeexCore {
       return m_pInstance;
     }
 
-    IRenderObject *CreateRender(std::string type, std::string ref);
+    IRenderObject *CreateRender(const std::string &type, const std::string &ref);
 
   private:
     static RenderCreator *m_pInstance;
