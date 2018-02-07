d75f605be487f3fb0c2b12211da3778bb4ccfdc2
YorkShen <shenyuancs@gmail.com>
2018-02-07 20:55:21 +0800

    * [core] Fix embed not shown problem. http://dotwe.org/vue/e993b9ad71e03afbe85480ac6c99ff84

--- android/sdk/src/main/java/com/taobao/weex/ui/component/WXEmbed.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/WXEmbed.java
@@ -280,11 +280,9 @@ public class WXEmbed extends WXDiv implements WXSDKInstance.OnInstanceVisibleLis
       return sdkInstance;
     }
 
-    ViewGroup.LayoutParams layoutParams = getHostView().getLayoutParams();
     sdkInstance.renderByUrl(WXPerformance.DEFAULT,
             url,
-            null, null, layoutParams.width,
-            layoutParams.height,
+            null, null,
             WXRenderStrategy.APPEND_ASYNC);
     return sdkInstance;
   }
