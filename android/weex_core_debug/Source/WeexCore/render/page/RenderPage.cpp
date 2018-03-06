5dbe76fdfdcaf2ed04a62a57250ae55a72395bc9
miomin <691292118@qq.com>
2018-03-06 16:40:00 +0800

    * [Core] Update libweexjsc.so

--- android/weex_core_debug/Source/WeexCore/render/page/RenderPage.cpp
+++ android/weex_core_debug/Source/WeexCore/render/page/RenderPage.cpp
@@ -81,14 +81,14 @@ namespace WeexCore {
         switch (render_root->getFlexDirection()) {
           case kFlexDirectionColumn:
           case kFlexDirectionColumnReverse:
-            if (render_root->getLargestMainSize() * radio > deviceHeight / 2) {
+            if (render_root->getLargestMainSize() * radio > deviceHeight / 3) {
               TraverseTree(render_root);
             }
             break;
           case kFlexDirectionRow:
           case kFlexDirectionRowReverse:
           default:
-            if (render_root->getLargestMainSize() * radio > deviceWidth / 2) {
+            if (render_root->getLargestMainSize() * radio > deviceWidth / 3) {
               TraverseTree(render_root);
             }
             break;
