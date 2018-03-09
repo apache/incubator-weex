a790beddfe60f65935963f693c9f931c86db608d
fjzznjsh <fjzznjsh@163.com>
2018-03-09 17:27:27 +0800

    *[ios]fix CTFont crash on iOS10

--- ios/sdk/WeexSDK/Sources/Component/WXTextComponent.m
+++ ios/sdk/WeexSDK/Sources/Component/WXTextComponent.m
@@ -464,12 +464,8 @@ do {\
     
     // set font
     UIFont *font = [WXUtility fontWithSize:_fontSize textWeight:_fontWeight textStyle:_fontStyle fontFamily:_fontFamily scaleFactor:self.weexInstance.pixelScaleFactor useCoreText:[self useCoreText]];
-    CTFontRef ctFont = CTFontCreateWithName((__bridge CFStringRef)font.fontName,
-                                           font.pointSize,
-                                           NULL);
-    if (ctFont) {
-        [attributedString addAttribute:(id)kCTFontAttributeName value:(__bridge id)(ctFont) range:NSMakeRange(0, string.length)];
-        CFRelease(ctFont);
+    if (font) {
+        [attributedString addAttribute:NSFontAttributeName value:font range:NSMakeRange(0, string.length)];
     }
     
     if(_textDecoration == WXTextDecorationUnderline){
