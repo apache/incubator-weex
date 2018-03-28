9b5f3bea83921245a4bf05bbd6fbf3a21b4eb477
CYJB <f8cyjb@126.com>
2018-03-28 19:43:32 +0800

    [WEEX-267][ios]monitor the frequency of module invoke
    Add new optional method for module invoke monitor, do not use alarm anymore.
    
    [WEEX-267][ios]change method signature
    close #1090

--- ios/sdk/WeexSDK/Sources/Bridge/WXModuleMethod.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXModuleMethod.m
@@ -113,7 +113,13 @@
 - (void)commitModuleInvoke
 {
     id<WXAppMonitorProtocol> appMonitorHandler = [WXHandlerFactory handlerForProtocol:@protocol(WXAppMonitorProtocol)];
-    if ([appMonitorHandler respondsToSelector:@selector(commitAppMonitorAlarm:monitorPoint:success:errorCode:errorMsg:arg:)]) {
+	if ([appMonitorHandler respondsToSelector:@selector(commitMonitorWithPage:monitorPoint:args:)]) {
+		NSDictionary * args = @{
+								@"url": self.instance.pageName ?: @"",
+								@"name": [NSString stringWithFormat:@"%@.%@", self.moduleName, self.methodName],
+								};
+		[appMonitorHandler commitMonitorWithPage:@"weex" monitorPoint:@"invokeModule" args:args];
+	} else if ([appMonitorHandler respondsToSelector:@selector(commitAppMonitorAlarm:monitorPoint:success:errorCode:errorMsg:arg:)]) {
         NSString * arg = [NSString stringWithFormat:@"%@.%@", self.moduleName, self.methodName];
         [appMonitorHandler commitAppMonitorAlarm:@"weex" monitorPoint:@"invokeModule" success:NO errorCode:@"101" errorMsg:self.instance.pageName arg:arg];
     }
--- ios/sdk/WeexSDK/Sources/Protocol/WXAppMonitorProtocol.h
+++ ios/sdk/WeexSDK/Sources/Protocol/WXAppMonitorProtocol.h
@@ -54,4 +54,8 @@
 
 - (void)commitAppMonitorAlarm:(NSString *)pageName monitorPoint:(NSString *)monitorPoint success:(BOOL)success errorCode:(NSString *)errorCode errorMsg:(NSString *)errorMsg arg:(NSString *)arg;
 
+@optional
+
+- (void)commitMonitorWithPage:(NSString *)pageName monitorPoint:(NSString *)monitorPoint args:(NSDictionary *)args;
+
 @end
