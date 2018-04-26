f8c02bab4a9a427eaf5f05a3994b8f4f0d0e533a
zhongcang <qh438406812@gmail.com>
2018-04-26 20:33:40 +0800

    * [iOS] rm useless code

--- ios/sdk/WeexSDK/Sources/Component/WXListComponent.mm
+++ ios/sdk/WeexSDK/Sources/Component/WXListComponent.mm
@@ -164,11 +164,6 @@
     return [[WXTableView alloc] init];
 }
 
-//- (void)_insertChildCssNode:(WXComponent *)subcomponent atIndex:(NSInteger)index
-//{
-////    [super _insertChildCssNode:subcomponent atIndex:(uint32_t)index];
-//}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
