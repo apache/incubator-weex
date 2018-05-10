d5e710d3410503050c1ffcf9a7164a9ee8305232
maike(麦客) <maike.cxm@taobao.com>
2018-05-10 20:08:21 +0800

    [WEEX-352][iOS] Allow developer customize Refresh control by implement new refresh protocol WXPullRefreshProtocol.  Refined

--- ios/playground/WeexDemo/extend/handler/WXPullRefreshDefaultImpl.m
+++ ios/playground/WeexDemo/extend/handler/WXPullRefreshDefaultImpl.m
@@ -19,12 +19,23 @@
 
 #import "WXPullRefreshDefaultImpl.h"
 #import <UIKit/UIKit.h>
-#import <Masonry/Masonry.h>
 #import <WeexSDK/WXUtility.h>
 
-#import "QNWaveLayer.h"
-#import "QNRotateImageView.h"
+#pragma mark WXRotateImageView
+@interface WXRotateImageView : UIImageView
+@property (assign) CGFloat currentAngle;
+- (void)startRotate;
+@end
 
+#pragma mark WXWaveLayer
+typedef void (^WXWaveLayerOverBlock)();
+@interface WXWaveLayer : CALayer
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) NSTimeInterval animationDuration;
+- (void)startAnimation:(WXWaveLayerOverBlock)block;
+@end
+
+#pragma mark implementation WXPullRefreshDefaultImpl
 @interface WXPullRefreshDefaultImpl ()
 @property (nonatomic, weak) UIView *pullContentView;
 @property (nonatomic, weak) UIView *refreshContentView;
@@ -33,8 +44,8 @@
 @property (nonatomic, weak) NSTimer *finalTimer;
 
 @property (nonatomic, strong) UILabel *timeLabel;
-@property (nonatomic, strong) QNWaveLayer *waveLayer;
-@property (nonatomic, strong) QNRotateImageView *imageView;
+@property (nonatomic, strong) WXWaveLayer *waveLayer;
+@property (nonatomic, strong) WXRotateImageView *imageView;
 @property (nonatomic, strong) UIImageView *pullingIV;
 
 @property (nonatomic, assign) CGRect oldFrame;
@@ -51,21 +62,19 @@
     UIView *aView = [[UIView alloc] init];
     aView.backgroundColor = [UIColor greenColor];
     [inSuperView addSubview:aView];
-    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.mas_equalTo(inSuperView);
-        make.bottom.right.mas_equalTo(inSuperView);
-    }];
+    aView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight;
+    [aView setFrame:inSuperView.bounds];
     _pullContentView = aView;
 
     UIImageView *imageView = [[UIImageView alloc] init];
     imageView.contentMode = UIViewContentModeScaleToFill;
     imageView.image = [UIImage imageNamed:@"weex"];
     [aView addSubview:imageView];
-    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.mas_equalTo(aView);
-        make.width.mas_equalTo(@80);
-        make.height.mas_equalTo(aView);
-    }];
+    imageView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight;
+    CGRect bounds = inSuperView.bounds;
+    bounds.origin.x = (bounds.size.width - 80)/2;
+    bounds.size.width = 80;
+    imageView.frame = bounds;
     _pullingIV = imageView;
 }
 
@@ -100,22 +109,18 @@
     UIView *aView = [[UIView alloc] init];
     aView.backgroundColor = [UIColor grayColor];
     [inSuperView addSubview:aView];
-    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.mas_equalTo(inSuperView);
-        make.bottom.right.mas_equalTo(inSuperView);
-    }];
+    aView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight;
+    [aView setFrame:inSuperView.bounds];
     _refreshContentView = aView;
 
-    _imageView = [[QNRotateImageView alloc] init];
+    _imageView = [[WXRotateImageView alloc] init];
     _imageView.contentMode = UIViewContentModeScaleAspectFit;
     _imageView.image = [UIImage imageNamed:@"weex-icon"];
     [aView addSubview:_imageView];
-
-    [_imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.mas_equalTo(aView);
-        make.width.mas_equalTo(aView.mas_width).multipliedBy(0.7);
-        make.height.mas_equalTo(aView.mas_height).multipliedBy(0.7);
-    }];
+    CGRect bounds = inSuperView.bounds;
+    bounds.origin.x = (bounds.size.width - 80)/2;
+    bounds.size.width = 80;
+    _imageView.frame = bounds;
 
     [_imageView startRotate];
 }
@@ -142,10 +147,8 @@
     aView.backgroundColor = [UIColor blueColor];
     aView.clipsToBounds = YES;
     [inSuperView addSubview:aView];
-    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.mas_equalTo(inSuperView);
-        make.bottom.right.mas_equalTo(inSuperView);
-    }];
+    aView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight;
+    [aView setFrame:inSuperView.bounds];
     _finalContentView = aView;
     UIView *bView = (UIView *)inSuperView;
     CGFloat duration = inDuration/1000;
@@ -196,10 +199,10 @@
     return _timeLabel;
 }
 
-- (QNWaveLayer *)waveLayer {
+- (WXWaveLayer *)waveLayer {
 
     if (nil == _waveLayer) {
-        _waveLayer = [[QNWaveLayer alloc] init];
+        _waveLayer = [[WXWaveLayer alloc] init];
         [_finalContentView.layer addSublayer:_waveLayer];
     }
 
@@ -218,6 +221,167 @@
         NSString *time = [formatter stringFromDate:lastUpdatedTime];
         NSString *updateFormat = @"Last updated: %@";
         self.timeLabel.text = [NSString stringWithFormat:updateFormat, time];
+    }
+}
+
+@end
+
+#pragma mark implementation WXRotateImageView
+
+@interface WXRotateImageView()
+
+@property (assign, nonatomic) CATransform3D identityTransform;
+
+@property (strong) NSTimer *timer;
+
+@end
+
+@implementation WXRotateImageView
+
+- (id)init {
+
+    self = [super init];
+    if (nil == self) {
+
+        return self;
+    }
+
+    [self _init];
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+
+    self = [super initWithFrame:frame];
+    if (nil == self) {
+
+        return self;
+    }
+
+    [self _init];
+    return self;
+}
+
+- (void)awakeFromNib {
+
+    [super awakeFromNib];
+    [self _init];
+}
+
+- (void)_init {
+
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m34 = 1.0 / -500;
+    self.identityTransform = CATransform3DRotate(transform, 0, 0, 0, 1.0);
+}
+
+- (void)dealloc {
+
+    [self finishRotate];
+}
+
+- (void)startRotate {
+
+    [self finishRotate];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(onTimer:) userInfo:nil repeats:YES];
+}
+
+- (void)finishRotate {
+
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+- (void)onTimer:(NSTimer *)timer {
+
+    self.currentAngle += 0.04 * M_PI;
+    [self applyEffect];
+}
+
+- (void)applyEffect {
+
+    CATransform3D transform = CATransform3DRotate(self.identityTransform, self.currentAngle, 0, 0, 1.0);
+    self.layer.transform = transform;
+}
+
+
+@end
+
+#pragma mark implementation WXWaveLayer
+@interface WXWaveLayer()<CAAnimationDelegate>
+
+@property (nonatomic, strong) CAAnimationGroup *animationGroup;
+
+@property (strong) CAKeyframeAnimation *keyFrameAnimation;
+
+@property (copy) WXWaveLayerOverBlock block;
+
+@end
+
+@implementation WXWaveLayer
+
+- (id)init {
+    self = [super init];
+    if (self) {
+
+        self.contentsScale = [UIScreen mainScreen].scale;
+
+        self.animationDuration = 0.25f;
+        self.backgroundColor = [UIColor clearColor].CGColor;
+    }
+    return self;
+}
+
+- (void)startAnimation:(WXWaveLayerOverBlock)block {
+
+    self.block = block;
+    self.backgroundColor = [UIColor colorWithRed:0xFF/255.0f green:0x00/255.0f blue:0x00/255.0f alpha:1].CGColor;
+    [self setupAnimationGroup];
+    [self addAnimation:self.keyFrameAnimation forKey:@"pulse"];
+}
+
+- (void)stopAnimation {
+
+    [self removeAllAnimations];
+    self.animationGroup = nil;
+}
+
+- (void)reset {
+
+    self.backgroundColor = [UIColor clearColor].CGColor;
+}
+
+- (void)setRadius:(CGFloat)radius {
+
+    _radius = radius;
+
+    CGPoint tempPos = self.position;
+
+    CGFloat diameter = self.radius * 2;
+
+    self.bounds = CGRectMake(0, 0, diameter, diameter);
+    self.cornerRadius = self.radius;
+    self.position = tempPos;
+}
+
+- (void)setupAnimationGroup {
+
+    CAKeyframeAnimation *scaleAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale.xy"];
+    scaleAnimation.values = @[@0, @0.3, @0.8, @1];
+    scaleAnimation.keyTimes = @[@0, @0.5, @0.9, @1];
+    scaleAnimation.duration = self.animationDuration;
+    scaleAnimation.delegate = self;
+
+    self.keyFrameAnimation = scaleAnimation;
+}
+
+#pragma mark CAAnimationDelegate
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+
+    [self stopAnimation];
+    if (self.block) {
+
+        self.block();
     }
 }
 
