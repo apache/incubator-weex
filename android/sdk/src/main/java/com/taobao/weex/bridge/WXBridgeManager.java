eda4614c924d15ef461fc0ac970bcbafd98c4d70
Jonathan Dong <jondong.community@outlook.com>
2018-05-22 21:34:48 +0800

    * [Core] Reorganized the CMake build files.
    This patch fixed some of the issues in CMake build config and related
    files, which includes:
    
    1. Renamed the project as "WeexCore", also renamed the corresponding
      libraries as libweexcore and libweexipc, which better described
      the project current status.
    2. Splitted the source files into COMMON_SRCS which are platform
      irrelevant, as well as ANDROID_SRCS which are platform relevant.
    3. Removed some unnecessary includes and links.

--- android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
+++ android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -753,7 +753,7 @@ public class WXBridgeManager implements Callback, BactchExecutor {
   }
 
   public void callReportCrash(String crashFile, final String instanceId, final String url) {
-    // statistic weexjsc process crash
+    // statistic weex core process crash
     Date date = new Date();
     DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
     String time = format.format(date);
@@ -1176,7 +1176,7 @@ public class WXBridgeManager implements Callback, BactchExecutor {
     }
 
     String method = "callReportCrash";
-    String exception = "weexjsc process crash and restart exception";
+    String exception = "weex core process crash and restart exception";
     Map<String, String> extParams = new HashMap<String, String>();
     extParams.put("jscCrashStack", errMsg);
     IWXJSExceptionAdapter adapter = WXSDKManager.getInstance().getIWXJSExceptionAdapter();
