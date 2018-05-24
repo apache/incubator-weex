5566e2e40aa018ade1e4fd6c7e933b22d32c45fa
miomin <miomin_93@foxmail.com>
2018-05-24 15:47:51 +0800

    [WEEX-403] Update libweexcore.so for x86 and armeabi-v7a

--- weex_core/release.sh
+++ weex_core/release.sh
@@ -1,6 +1,6 @@
 cd ../android
 rm -rf sdk/build/intermediates/bundles/release/jni/*
 ./gradlew clean assemble
-cp sdk/build/intermediates/bundles/release/jni/armeabi/libweexjsc.so sdk/libs/armeabi/libweexjsc.so
-cp sdk/build/intermediates/bundles/release/jni/armeabi-v7a/libweexjsc.so sdk/libs/armeabi-v7a/libweexjsc.so
-cp sdk/build/intermediates/bundles/release/jni/x86/libweexjsc.so sdk/libs/x86/libweexjsc.so
+cp sdk/build/intermediates/bundles/release/jni/armeabi/libweexcore.so sdk/libs/armeabi/libweexcore.so
+cp sdk/build/intermediates/bundles/release/jni/armeabi-v7a/libweexcore.so sdk/libs/armeabi-v7a/libweexcore.so
+cp sdk/build/intermediates/bundles/release/jni/x86/libweexcore.so sdk/libs/x86/libweexcore.so
