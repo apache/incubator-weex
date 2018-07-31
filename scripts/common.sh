2864fdf1569317d5664242393acdba9e887aface
Jonathan Dong <jondong.community@outlook.com>
2018-07-31 17:08:58 +0800

    [WEEX-525] Created the automate beta branch-off scripts. (#1353)
    The script beta-branchoff.sh is to automate the weekly beta release
    branch-off procedure. It helps to identify which is the latest
    non-RC version number and release a new beta branch with the number.
    The branch will be branched-off from origin/master and will be pushed
    onto apache after its creation.
    
    A valid beta version number looks like: 0.18.0-beta1
    A Valid beta branch name looks like: 0.18.0-beta1-180720
    
    common.sh provides some useful gadgets for the scripts and can be
    shared across scripts.

---
+++ scripts/common.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+RESTORE=$(echo -en '\033[0m')
+RED=$(echo -en '\033[00;31m')
+GREEN=$(echo -en '\033[00;32m')
+YELLOW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MAGENTA=$(echo -en '\033[00;35m')
+PURPLE=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LIGHTGRAY=$(echo -en '\033[00;37m')
+LRED=$(echo -en '\033[01;31m')
+LGREEN=$(echo -en '\033[01;32m')
+LYELLOW=$(echo -en '\033[01;33m')
+LBLUE=$(echo -en '\033[01;34m')
+LMAGENTA=$(echo -en '\033[01;35m')
+LPURPLE=$(echo -en '\033[01;35m')
+LCYAN=$(echo -en '\033[01;36m')
+WHITE=$(echo -en '\033[01;37m')
+
+function info () {
+  echo -e $*
+}
+
+function notice () {
+  echo -e "${CYAN}$*${RESTORE}"
+}
+
+function warning () {
+  echo -e "${YELLOW}$*${RESTORE}"
+}
+
+function error () {
+  echo -e "${RED}$*${RESTORE}"
+}
