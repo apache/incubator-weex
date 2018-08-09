40fa04c611b8bd287cf9006d8bfa673d196fad1e
yxping <yxping@users.noreply.github.com>
2018-08-09 13:55:24 +0800

    Revert "* [android] Use rint on getFloatByViewport." (#1404)
    This reverts commit 4bffbd284817f61f95a367a8a66d8b7b7e3f7f6d.

--- weex_core/Source/base/ViewUtils.h
+++ weex_core/Source/base/ViewUtils.h
@@ -64,7 +64,7 @@ namespace WeexCore {
 
     float realPx = (src * WXCoreEnvironment::getInstance()->DeviceWidth() /
                     viewport);
-    float result = realPx > 0.005 && realPx < 1 ? 1.0f : rint(realPx);
+    float result = realPx > 0.005 && realPx < 1 ? 1.0f : realPx;
     return result;
   }
 
