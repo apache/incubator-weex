f69382b614f8d83e6da676c8a03dfe40fde3f81c
miomin <miomin_93@foxmail.com>
2018-09-16 20:49:02 +0800

    * [android] Remove IWXDebugJsBridge

--- android/sdk/src/main/java/com/taobao/weex/bridge/WXDebugJsBridge.java
+++ android/sdk/src/main/java/com/taobao/weex/bridge/WXDebugJsBridge.java
@@ -19,7 +19,6 @@
 package com.taobao.weex.bridge;
 
 import com.alibaba.fastjson.JSON;
-import com.taobao.weex.common.IWXDebugJsBridge;
 import com.taobao.weex.utils.WXWsonJSONSwitch;
 import com.taobao.weex.wson.WsonUtils;
 
@@ -27,21 +26,16 @@ import com.taobao.weex.wson.WsonUtils;
  * Created by darin on 27/03/2018.
  */
 
-public class WXDebugJsBridge implements IWXDebugJsBridge {
+public class WXDebugJsBridge {
 
-    @Override
     public native void resetWXBridge(Object bridge, String className);
 
-    @Override
     public native void jsHandleSetJSVersion(String jsVersion);
 
-    @Override
     public native void jsHandleReportException(String instanceId, String func, String exceptionString);
 
-    @Override
     public native void jsHandleCallNative(String instanceId, byte[] tasks, String callback);
 
-    @Override
     public void jsHandleCallNativeModule(String instanceId, String module, String method, byte[] arguments, byte[] options) {
         jsHandleCallNativeModule(instanceId, module, method,
                 WXWsonJSONSwitch.convertJSONToWsonIfUseWson(arguments), WXWsonJSONSwitch.convertJSONToWsonIfUseWson(options), true);
@@ -49,8 +43,6 @@ public class WXDebugJsBridge implements IWXDebugJsBridge {
 
     public native void jsHandleCallNativeModule(String instanceId, String module, String method, byte[] arguments, byte[] options, boolean h5);
 
-
-    @Override
     public void jsHandleCallNativeComponent(String instanceId, String componentRef, String method, byte[] arguments, byte[] options){
         jsHandleCallNativeComponent(instanceId, componentRef, method, WXWsonJSONSwitch.convertJSONToWsonIfUseWson(arguments), WXWsonJSONSwitch.convertJSONToWsonIfUseWson(options), true);
     }
@@ -58,52 +50,34 @@ public class WXDebugJsBridge implements IWXDebugJsBridge {
 
     public native void jsHandleCallNativeComponent(String instanceId, String componentRef, String method, byte[] arguments, byte[] options, boolean from);
 
-
-    @Override
     public void jsHandleCallAddElement(String instanceId, String ref, String dom, String index){
         jsHandleCallAddElement(instanceId, ref, WsonUtils.toWson(JSON.parse(dom)), index, true);
     }
 
     public native void jsHandleCallAddElement(String instanceId, String ref, byte[] dom, String index, boolean h5);
 
-
-    @Override
     public native void jsHandleSetTimeout(String callbackId, String time);
 
-
-    @Override
     public native void jsHandleCallNativeLog(byte[] str_array);
 
-
-    @Override
     public void jsHandleCallCreateBody(String pageId, String domStr){
         jsHandleCallCreateBody(pageId, WsonUtils.toWson(JSON.parse(domStr)), true);
     }
 
     public native void jsHandleCallCreateBody(String pageId, byte[] domStr, boolean h5);
 
-
-    @Override
     public native void jsHandleCallUpdateFinish(String instanceId, byte[] tasks, String callback);
 
-
-    @Override
     public native void jsHandleCallCreateFinish(String pageId);
 
-
-    @Override
     public native void jsHandleCallRefreshFinish(String instanceId, byte[] tasks, String callback);
 
-
-    @Override
     public void jsHandleCallUpdateAttrs(String pageId, String ref, String data){
         jsHandleCallUpdateAttrs(pageId, ref, WsonUtils.toWson(JSON.parseObject(data)), true);
     }
 
     public native void jsHandleCallUpdateAttrs(String pageId, String ref, byte[] data, boolean h5);
 
-
-    @Override
     public void jsHandleCallUpdateStyle(String pageId, String ref, String data){
         byte[] data1 = WsonUtils.toWson(JSON.parseObject(data));
         jsHandleCallUpdateStyleNative(pageId, ref, data1, true);
@@ -111,31 +85,17 @@ public class WXDebugJsBridge implements IWXDebugJsBridge {
 
     public native void jsHandleCallUpdateStyleNative(String pageId, String ref, byte[] data, boolean h5);
 
-
-    @Override
     public native void jsHandleCallRemoveElement(String pageId, String ref);
 
-
-    @Override
     public native void jsHandleCallMoveElement(String pageId, String ref, String parentRef, String index_str);
 
-
-    @Override
     public native void jsHandleCallAddEvent(String pageId, String ref, String event);
 
-
-    @Override
     public native void jsHandleCallRemoveEvent(String pageId, String ref, String event);
 
-
-    @Override
     public native void jsHandleSetInterval(String instanceId, String callbackId, String time);
 
-
-    @Override
     public native void jsHandleClearInterval(String instanceId, String callbackId);
 
-
-    @Override
     public native void jsHandleCallGCanvasLinkNative(String contextId, int type, String val);
 }
