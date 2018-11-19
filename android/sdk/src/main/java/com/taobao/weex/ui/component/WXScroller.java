4557136293a1a67ef09bd769f166317b161f9d74
yxping <yxpflyman@gmail.com>
2018-11-19 11:35:00 +0800

    [Android] fix scroller memory leack because of OnAttachedListener

--- android/sdk/src/main/java/com/taobao/weex/ui/component/WXScroller.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/WXScroller.java
@@ -100,6 +100,7 @@ public class WXScroller extends WXVContainer<ViewGroup> implements WXScrollViewL
   private int pageSize = 0;
   private boolean pageEnable = false;
   private boolean mIsHostAttachedToWindow = false;
+  private View.OnAttachStateChangeListener mOnAttachStateChangeListener;
 
   public static class Creator implements ComponentCreator {
     @Override
@@ -376,6 +377,9 @@ public class WXScroller extends WXVContainer<ViewGroup> implements WXScrollViewL
     if (mStickyMap != null) {
       mStickyMap.clear();
     }
+    if (mOnAttachStateChangeListener != null && getInnerView() != null) {
+      getInnerView().removeOnAttachStateChangeListener(mOnAttachStateChangeListener);
+    }
     if (getInnerView() != null && getInnerView() instanceof IWXScroller) {
       ((IWXScroller) getInnerView()).destroy();
     }
@@ -608,7 +612,7 @@ public class WXScroller extends WXVContainer<ViewGroup> implements WXScrollViewL
         }
       }
     });
-    host.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+    mOnAttachStateChangeListener = new View.OnAttachStateChangeListener() {
       @Override
       public void onViewAttachedToWindow(View v) {
         mIsHostAttachedToWindow = true;
@@ -620,7 +624,8 @@ public class WXScroller extends WXVContainer<ViewGroup> implements WXScrollViewL
         mIsHostAttachedToWindow = false;
         dispatchDisappearEvent();
       }
-    });
+    };
+    host.addOnAttachStateChangeListener(mOnAttachStateChangeListener);
     return host;
   }
 
