2683f9322bd4ba31513047f5349fbeea2f86a1de
YorkShen <shenyuancs@gmail.com>
2018-12-13 20:44:32 +0800

    * [Android] Add libweexcore.so due to the malfunction of integration machine

--- weex_core/release.sh
+++ weex_core/release.sh
@@ -1,6 +1,6 @@
 cd ../android
-rm -rf sdk/build/intermediates/bundles/release/jni/*
-./gradlew clean assemble
-cp sdk/build/intermediates/bundles/release/jni/armeabi/libweexcore.so sdk/libs/armeabi/libweexcore.so
-cp sdk/build/intermediates/bundles/release/jni/armeabi-v7a/libweexcore.so sdk/libs/armeabi-v7a/libweexcore.so
-cp sdk/build/intermediates/bundles/release/jni/x86/libweexcore.so sdk/libs/x86/libweexcore.so
+rm -rf sdk/build/intermediates/bundles/default/jni/*
+./gradlew clean assembleRelease
+cp sdk/build/intermediates/bundles/default/jni/armeabi/libweexcore.so sdk/libs/armeabi/libweexcore.so
+cp sdk/build/intermediates/bundles/default/jni/armeabi-v7a/libweexcore.so sdk/libs/armeabi-v7a/libweexcore.so
+cp sdk/build/intermediates/bundles/default/jni/x86/libweexcore.so sdk/libs/x86/libweexcore.so
