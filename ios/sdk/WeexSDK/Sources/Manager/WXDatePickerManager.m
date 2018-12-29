2af6bd69a8bb44565998e8fb9cca37ee543d2945
zhengweibin <zhengweibin@zhuojianchina.com>
2018-12-29 15:39:34 +0800

    input type为date的时候选择时间问题 #1484

--- ios/sdk/WeexSDK/Sources/Manager/WXDatePickerManager.m
+++ ios/sdk/WeexSDK/Sources/Manager/WXDatePickerManager.m
@@ -66,22 +66,21 @@
         CGRect pickerFrame = CGRectMake(0, 44, [UIScreen mainScreen].bounds.size.width, WXPickerHeight-44);
         datePicker.backgroundColor = [UIColor whiteColor];
         datePicker.frame = pickerFrame;
-        // UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 44)];
-        // [toolBar setBackgroundColor:[UIColor whiteColor]];
-        // UIBarButtonItem* noSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
-        // noSpace.width=10;
-        // UIBarButtonItem* doneBtn = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done:)];
-        // UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-        // UIBarButtonItem* cancelBtn = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel:)];
-        // [toolBar setItems:[NSArray arrayWithObjects:noSpace,cancelBtn,flexSpace,doneBtn,noSpace, nil]];
+        UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 44)];
+        [toolBar setBackgroundColor:[UIColor whiteColor]];
+        UIBarButtonItem* noSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+        noSpace.width=10;
+        UIBarButtonItem* doneBtn = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:nil];
+        UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+        UIBarButtonItem* cancelBtn = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:nil];
+        [toolBar setItems:[NSArray arrayWithObjects:noSpace,cancelBtn,flexSpace,doneBtn,noSpace, nil]];
 
-        UIView *toolBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 44)];
-        UIButton *doneBtn = [self createButton:@"确定" action:@selector(done:) andFrame: CGRectMake([UIScreen mainScreen].bounds.size.width-115, 2, 100, 40)];
-        doneBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
-        UIButton *cancelBtn = [self createButton:@"取消" action:@selector(cancel:) andFrame:CGRectMake(15, 2, 100, 40)];
-        cancelBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-        [toolBar addSubview:doneBtn];
-        [toolBar addSubview:cancelBtn];
+        UIView *toolView = [[UIView alloc] initWithFrame:toolBar.bounds];
+        UIButton *done = [self createButton:@selector(done:) andFrame: CGRectMake([UIScreen mainScreen].bounds.size.width-110, 2, 100, 40)];
+        UIButton *cancel = [self createButton:@selector(cancel:) andFrame:CGRectMake(10, 2, 100, 40)];
+        [toolView addSubview:done];
+        [toolView addSubview:cancel];
+        [toolBar addSubview:toolView];
 
         [self.datePickerView addSubview:datePicker];
         [self.datePickerView addSubview:toolBar];
@@ -142,10 +141,9 @@
     }
 }
 
-- (UIButton *)createButton:(NSString *)title action: (SEL)selector andFrame:(CGRect)rect  {
+- (UIButton *)createButton:(SEL)selector andFrame:(CGRect)rect  {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setTitle:title forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     button.titleLabel.font = [UIFont systemFontOfSize:16.f];
     button.frame = rect;
     [button addTarget:self action:selector forControlEvents:UIControlEventTouchUpInside];
