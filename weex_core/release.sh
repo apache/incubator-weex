dc45daae4d3152a3bf027c6f93d4d64fe7915df0
zhongcang <qh438406812@gmail.com>
2019-01-04 01:03:00 +0800

    * [Android] edit build release.sh for cp jss so

--- weex_core/release.sh
+++ weex_core/release.sh
@@ -4,3 +4,16 @@ rm -rf sdk/build/intermediates/bundles/default/jni/*
 cp sdk/build/intermediates/bundles/default/jni/armeabi/libweexcore.so sdk/libs/armeabi/libweexcore.so
 cp sdk/build/intermediates/bundles/default/jni/armeabi-v7a/libweexcore.so sdk/libs/armeabi-v7a/libweexcore.so
 cp sdk/build/intermediates/bundles/default/jni/x86/libweexcore.so sdk/libs/x86/libweexcore.so
+
+# cp jss so
+
+cp sdk/build/intermediates/bundles/default/jni/armeabi/libweexjss.so sdk/libs/armeabi/libweexjss.so
+cp sdk/build/intermediates/bundles/default/jni/armeabi-v7a/libweexjss.so sdk/libs/armeabi-v7a/libweexjss.so
+cp sdk/build/intermediates/bundles/default/jni/x86/libweexjss.so sdk/libs/x86/libweexjss.so
+
+# back up obj
+
+#rm -rf ~/Desktop/weex_so_armeabi
+#mkdir ~/Desktop/weex_so_armeabi
+#cp sdk/build/intermediates/cmake/release/obj/armeabi/libweexjss.so ~/Desktop/weex_so_armeabi/
+#cp sdk/build/intermediates/cmake/release/obj/armeabi/libweexcore.so ~/Desktop/weex_so_armeabi/
