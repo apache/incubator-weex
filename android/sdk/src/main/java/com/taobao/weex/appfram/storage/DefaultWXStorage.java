44cff24887a62ed67c554d885e6526eaa1725880
lvfen <284437334@qq.com>
2019-01-10 17:07:40 +0800

    * [Android] Fix storage problem due to multiple thread (#2010)

--- android/sdk/src/main/java/com/taobao/weex/appfram/storage/DefaultWXStorage.java
+++ android/sdk/src/main/java/com/taobao/weex/appfram/storage/DefaultWXStorage.java
@@ -143,20 +143,21 @@ public class DefaultWXStorage implements IWXStorageAdapter {
 
     @Override
     public void close() {
+        final ExecutorService needCloseService = mExecutorService;
         execute(new Runnable() {
             @Override
             public void run() {
                 try {
                     mDatabaseSupplier.closeDatabase();
-                    if (mExecutorService != null) {
-                        mExecutorService.shutdown();
-                        mExecutorService = null;
+                    if (needCloseService != null) {
+                        needCloseService.shutdown();
                     }
                 } catch (Exception e) {
                     WXLogUtils.e(WXSQLiteOpenHelper.TAG_STORAGE, e.getMessage());
                 }
             }
         });
+        mExecutorService = null;
     }
 
     private boolean performSetItem(String key, String value, boolean isPersistent, boolean allowRetryWhenFull) {
