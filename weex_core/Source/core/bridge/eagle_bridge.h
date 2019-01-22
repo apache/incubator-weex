81dda94d809a1044c845cfd82c39bc44abb5ff0e
linghe.lh <linghe.lh@alibaba-inc.com>
2019-01-22 14:36:34 +0800

    [core]add interface in DataRenderHandler

--- weex_core/Source/core/bridge/eagle_bridge.h
+++ weex_core/Source/core/bridge/eagle_bridge.h
@@ -91,6 +91,8 @@ namespace WeexCore {
             virtual void CreatePage(const char *contents, size_t length, const std::string& page_id, const std::string& options, const std::string& init_data, std::function<void(const char*)> exec_js) {}
             virtual bool RefreshPage(const std::string &page_id, const std::string &init_data) {return false;}
             virtual void UpdateComponentData(const std::string& page_id, const char* cid, const std::string& json_data) {}
+            virtual void FireEvent(const std::string &page_id, const std::string &ref, const std::string &event,const std::string &args,const std::string &dom_changes) {}
+            virtual void RegisterModules(const std::string &modules) {}
         };
         
         static EagleBridge* GetInstance() {
