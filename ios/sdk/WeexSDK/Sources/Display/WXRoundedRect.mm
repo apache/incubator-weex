a27027c9437c067b9c15d2b4f6100ffefbe73691
wqyfavor <wqyfavor88@gmail.com>
2019-02-12 16:18:17 +0800

    Revert "[iOS] Don't rescale corner-radius because some animation may animate width/height from zero to some value, but scaling generates zero factor if width/height is zero. (#2112)" (#2117)
    This reverts commit c30032d0fbc88d50768d472d3361d52032ea1808.

--- ios/sdk/WeexSDK/Sources/Display/WXRoundedRect.mm
+++ ios/sdk/WeexSDK/Sources/Display/WXRoundedRect.mm
@@ -76,17 +76,12 @@
     if (self = [super init]) {
         _rect = rect;
         _radii = [[WXRadii alloc] initWithTopLeft:topLeft topRight:topRight bottomLeft:bottomLeft bottomRight:bottomRight];
-        //[_radii scale:[self radiiConstraintScaleFactor]];
+        [_radii scale:[self radiiConstraintScaleFactor]];
     }
     
     return self;
 }
 
-/*
- We don't do this scaling because of this demo: http://dotwe.org/vue/594d43b10d8fb9847d8122bbd429f48b
- The width of the animating object is zero and radiiConstraintScaleFactor generated 0 factor
- which causes that no corner-radius is showing.
- 
 - (float)radiiConstraintScaleFactor
 {
     // Constrain corner radii using CSS3 rules:
@@ -121,6 +116,5 @@
     WXAssert(factor <= 1, @"Wrong factor for radii constraint scale:%f", factor);
     return factor;
 }
-*/
 
 @end
