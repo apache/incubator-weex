a753f25175736eab31542455787985299807cc67
gubaojian <gubaojian@163.com>
2019-03-06 19:34:19 +0800

    [WEEX][Weex-Heron] Add Weex Core So  And Build For Taobao App

--- weex_core/debug.sh
+++ weex_core/debug.sh
@@ -4,3 +4,7 @@ cd ..
 cp sdk/build/intermediates/cmake/debug/obj/armeabi/libweexcore.so sdk/libs/armeabi/libweexcore.so
 cp sdk/build/intermediates/cmake/debug/obj/armeabi-v7a/libweexcore.so sdk/libs/armeabi-v7a/libweexcore.so
 cp sdk/build/intermediates/cmake/debug/obj/x86/libweexcore.so sdk/libs/x86/libweexcore.so
+
+cp sdk/build/intermediates/cmake/debug/obj/armeabi/libweexjss.so sdk/libs/armeabi/libweexjss.so
+cp sdk/build/intermediates/cmake/debug/obj/armeabi-v7a/libweexjss.so sdk/libs/armeabi-v7a/libweexjss.so
+cp sdk/build/intermediates/cmake/debug/obj/x86/libweexjss.so sdk/libs/x86/libweexjss.so
