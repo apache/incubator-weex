a8a246b95fd28c93fa719f27f26ab9ad568f0d13
xujc <hpop1994@163.com>
2019-03-21 16:35:05 +0800

    add env in createpage for eagle

--- weex_core/Source/core/bridge/eagle_bridge.h
+++ weex_core/Source/core/bridge/eagle_bridge.h
@@ -94,7 +94,7 @@ namespace WeexCore {
             virtual int DestroyInstance(const char *instanceId);
             virtual void CreatePage(const std::string &input, const std::string &page_id, const std::string &options, const std::string &init_data, std::function<void(const char*)> exec_js) {}
             
-            virtual void CreatePage(const char *contents, size_t length, const std::string& page_id, const std::string& options, const std::string& init_data, std::function<void(const char*)> exec_js) {}
+            virtual void CreatePage(const char *contents, size_t length, const std::string& page_id, const std::string& options, const std::string& env, const std::string& init_data, std::function<void(const char*)> exec_js) {}
             virtual bool RefreshPage(const std::string &page_id, const std::string &init_data) {return false;}
             virtual void UpdateComponentData(const std::string& page_id, const char* cid, const std::string& json_data) {}
             virtual void FireEvent(const std::string &page_id, const std::string &ref, const std::string &event,const std::string &args,const std::string &dom_changes) {}
