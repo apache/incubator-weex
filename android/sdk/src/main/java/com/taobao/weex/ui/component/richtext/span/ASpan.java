40ae49e40c3dd725d6c546451075bd6e90001c9d
huanglei.hl <huanglei.hl@alibaba-inc.com>
2019-03-27 20:09:02 +0800

    [Android] Fire event when span is clicked which is in an a-label of a richtext component. * feature: If a span is clicked which is in an a-label of a richtext component, an itemclick event will be fired. * demo: http://editor.weex.io/p/sunshl/Contribute/commit/b21e1133830b48767c6d00d712e415b2 * doc pr: https://github.com/apache/incubator-weex-site/pull/362

--- android/sdk/src/main/java/com/taobao/weex/ui/component/richtext/span/ASpan.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/richtext/span/ASpan.java
@@ -21,20 +21,46 @@ package com.taobao.weex.ui.component.richtext.span;
 import android.text.TextPaint;
 import android.text.style.ClickableSpan;
 import android.view.View;
+
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.WXSDKManager;
+import com.taobao.weex.ui.component.richtext.node.RichTextNode;
 import com.taobao.weex.utils.ATagUtil;
+import com.taobao.weex.utils.WXDataStructureUtil;
+
+import java.util.Map;
 
 public class ASpan extends ClickableSpan {
 
   private String mInstanceId, mURL;
+  private final String mComponentRef;
+  private final String mPseudoRef;
 
-  public ASpan(String instanceId, String url) {
+  public ASpan(String instanceId, String url,String componentRef, String pseudoRef) {
     mInstanceId = instanceId;
     mURL = url;
+    mComponentRef = componentRef;
+    mPseudoRef = pseudoRef;
+  }
+  public ASpan(String instanceId, String url) {
+    this.mInstanceId = instanceId;
+    this.mURL = url;
+    this.mComponentRef = "";
+    this.mPseudoRef = "";
   }
 
   @Override
   public void onClick(View widget) {
-    ATagUtil.onClick(widget, mInstanceId, mURL);
+    if (mURL.equals(RichTextNode.CLICK_HREF) && !(mPseudoRef.isEmpty()) &&!(mComponentRef.isEmpty())) {
+      WXSDKInstance instance = WXSDKManager.getInstance().getSDKInstance(mInstanceId);
+      if (instance != null && !instance.isDestroy()) {
+        Map<String, Object> param = WXDataStructureUtil.newHashMapWithExpectedSize(1);
+        param.put(RichTextNode.PSEUDO_REF, mPseudoRef);
+        instance.fireEvent(mComponentRef, RichTextNode.ITEM_CLICK, param);
+      }
+    } else {
+      ATagUtil.onClick(widget, mInstanceId, mURL);
+    }
   }
 
   /**
