6f339a5941784ceec02dda093b5a71653248148e
zhiqiang.zhuzq <zhiqiang.zhuzq@alibaba-inc.com>
2019-04-02 15:45:52 +0800

    Fire event when span within an a-label is clicked.

--- android/sdk/src/main/java/com/taobao/weex/ui/component/richtext/node/ANode.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/richtext/node/ANode.java
@@ -54,14 +54,14 @@ class ANode extends RichTextNode {
   @Override
   protected void updateSpans(SpannableStringBuilder spannableStringBuilder, int level) {
     super.updateSpans(spannableStringBuilder, level);
-    if (attr != null && attr.containsKey(RichTextNode.PSEUDO_REF)) {
-      ItemClickSpan itemClickSpan = new ItemClickSpan(mInstanceId, mComponentRef, attr.get(RichTextNode.PSEUDO_REF).toString());
-      spannableStringBuilder.setSpan(itemClickSpan, 0, spannableStringBuilder.length(),
-              createSpanFlag(level));
-    }else if (attr != null && attr.containsKey(HREF)) {
-      ASpan aSpan = new ASpan(mInstanceId, attr.get(HREF).toString());
-      spannableStringBuilder.setSpan(aSpan, 0, spannableStringBuilder.length(),
-          createSpanFlag(level));
-    }
+      if (attr != null && attr.containsKey(RichTextNode.PSEUDO_REF)) {
+          ItemClickSpan itemClickSpan = new ItemClickSpan(mInstanceId, mComponentRef, attr.get(RichTextNode.PSEUDO_REF).toString());
+          spannableStringBuilder.setSpan(itemClickSpan, 0, spannableStringBuilder.length(),
+                  createSpanFlag(level));
+      } else if (attr != null && attr.containsKey(HREF)) {
+          ASpan aSpan = new ASpan(mInstanceId, attr.get(HREF).toString());
+          spannableStringBuilder.setSpan(aSpan, 0, spannableStringBuilder.length(),
+                  createSpanFlag(level));
+      }
   }
 }
