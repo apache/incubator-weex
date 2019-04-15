6341f7562b8a669d4968fa22711b95a10755a234
YorkShen <shenyuancs@gmail.com>
2019-04-15 17:32:23 +0800

    Update danger file

--- dangerfile-android.js
+++ dangerfile-android.js
@@ -92,7 +92,7 @@ console.log('-----------------------------hasAndroidFile------------------------
 if(hasAndroidFile){
   var runTryBuildCmd='source ~/.bash_profile; '
     +'cd android; '
-    +'./gradlew clean assembleDebug --info -PdisableCov=true '
+    +'./gradlew weex_sdk:clean weex_sdk:assembleRelease --info -PdisableCov=true '
     +'-Dorg.gradle.daemon=true -Dorg.gradle.parallel=true -Dorg.gradle.jvmargs="-Xmx512m '
     +'-XX:+HeapDumpOnOutOfMemoryError" -Dfile.encoding=UTF-8 '
   var runSuccess = shell.exec(runTryBuildCmd,{ async: false, timeout: 8 * 60 * 1000, maxBuffer: 200 * 1024 * 1024 }).code == 0;
