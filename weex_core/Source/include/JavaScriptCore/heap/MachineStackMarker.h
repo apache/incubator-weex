90b3d9df338c9c9b5ab69b15c0d94b7a1aaa3e0e
东煜 <yayun.dyy@alibaba-inc.com>
2019-05-30 10:22:57 +0800

    Fix JavaScriptCore copyright

--- weex_core/Source/include/JavaScriptCore/heap/MachineStackMarker.h
+++ weex_core/Source/include/JavaScriptCore/heap/MachineStackMarker.h
@@ -1,21 +1,24 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/*
+ *  Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
+ *  Copyright (C) 2001 Peter Kelly (pmk@post.com)
+ *  Copyright (C) 2003-2017 Apple Inc. All rights reserved.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  */
+
 #pragma once
 
 #include "MachineContext.h"
@@ -69,7 +72,6 @@ public:
         ~Thread();
 
         static Thread* createForCurrentThread();
-
         struct Registers {
             void* stackPointer() const;
 #if ENABLE(SAMPLING_PROFILER)
@@ -119,7 +121,7 @@ public:
     Lock& getLock() { return m_registeredThreadsMutex; }
     Thread* threadsListHead(const AbstractLocker&) const { ASSERT(m_registeredThreadsMutex.isLocked()); return m_registeredThreads; }
     Thread* machineThreadForCurrentThread();
-
+    void setTimerThread();
 private:
     void gatherFromCurrentThread(ConservativeRoots&, JITStubRoutineSet&, CodeBlockSet&, CurrentThreadState&);
 
@@ -130,7 +132,7 @@ private:
 
     template<typename PlatformThread>
     void removeThreadIfFound(PlatformThread);
-
+    PlatformThread m_timer_thread;
     Lock m_registeredThreadsMutex;
     Thread* m_registeredThreads;
     WTF::ThreadSpecificKey m_threadSpecificForMachineThreads;
