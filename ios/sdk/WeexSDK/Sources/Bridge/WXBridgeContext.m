5ad5ad446b9aa25f891cf49232ef8d036410a488
linghe.lh <linghe.lh@alibaba-inc.com>
2019-06-28 15:42:59 +0800

    [iOS] add execute jsbundle time point

--- ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
@@ -420,6 +420,7 @@ _Pragma("clang diagnostic pop") \
     if (!sdkInstance) {
         return;
     }
+    [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_EXECUTE_BUNDLE_START];
     [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_LOAD_BUNDLE_START];
     
     //create a sendQueue bind to the current instance
@@ -427,8 +428,6 @@ _Pragma("clang diagnostic pop") \
     [self.sendQueue setValue:sendQueue forKey:instanceIdString];
 
     if (sdkInstance.dataRender && ![options[@"EXEC_JS"] boolValue]) {
-        WX_MONITOR_INSTANCE_PERF_START(WXFirstScreenJSFExecuteTime, [WXSDKManager instanceForID:instanceIdString]);
-        WX_MONITOR_INSTANCE_PERF_START(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
         if (_dataRenderHandler) {
             WXPerformBlockOnComponentThread(^{
                 [_dataRenderHandler createPage:instanceIdString template:jsBundleString options:options data:data];
@@ -444,8 +443,6 @@ _Pragma("clang diagnostic pop") \
                 });
             }
         }
-        WX_MONITOR_INSTANCE_PERF_END(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
-        [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_LOAD_BUNDLE_END];
         return;
     }
 
@@ -476,6 +473,7 @@ _Pragma("clang diagnostic pop") \
         }
         if ([WXDebugTool isDevToolDebug]) {
             [self callJSMethod:@"createInstanceContext" args:@[instanceIdString, newOptions, data?:@[],raxAPIScript?:@""]];
+            [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_LOAD_BUNDLE_END];
             
             if ([NSURL URLWithString:sdkInstance.pageName]) {
                 [sdkInstance.instanceJavaScriptContext executeJavascript:jsBundleString withSourceURL:[NSURL URLWithString:sdkInstance.pageName]];
@@ -483,7 +481,7 @@ _Pragma("clang diagnostic pop") \
                 [sdkInstance.instanceJavaScriptContext executeJavascript:jsBundleString];
             }
             WX_MONITOR_INSTANCE_PERF_END(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
-            [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_LOAD_BUNDLE_END];
+            [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_EXECUTE_BUNDLE_END];
         } else {
             sdkInstance.callCreateInstanceContext = [NSString stringWithFormat:@"instanceId:%@\noptions:%@\ndata:%@", instanceIdString, newOptions, data];
             //add instanceId to weexContext ,if fucn createInstanceContext failure ，then we will know which instance has problem (exceptionhandler)
@@ -547,6 +545,7 @@ _Pragma("clang diagnostic pop") \
                     [sdkInstance.instanceJavaScriptContext executeJavascript:jsBundleString];
                 }
                 sdkInstance.instanceJavaScriptContext.javaScriptContext[@"wxExtFuncInfo"] = nil;
+                [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_EXECUTE_BUNDLE_END];
                 WX_MONITOR_INSTANCE_PERF_END(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
             }];
         }
@@ -567,7 +566,7 @@ _Pragma("clang diagnostic pop") \
         sdkInstance.instanceJavaScriptContext.javaScriptContext[@"wxExtFuncInfo"] = funcInfo;
         [self callJSMethod:@"createInstance" args:args];
         sdkInstance.instanceJavaScriptContext.javaScriptContext[@"wxExtFuncInfo"] = nil;
-
+        [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_EXECUTE_BUNDLE_END];
         WX_MONITOR_INSTANCE_PERF_END(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
     }
 }
@@ -607,8 +606,6 @@ _Pragma("clang diagnostic pop") \
     [self.sendQueue setValue:sendQueue forKey:instanceIdString];
 
     if (sdkInstance.dataRender) {
-        WX_MONITOR_INSTANCE_PERF_START(WXFirstScreenJSFExecuteTime, [WXSDKManager instanceForID:instanceIdString]);
-        WX_MONITOR_INSTANCE_PERF_START(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
         if (_dataRenderHandler) {
             WXPerformBlockOnComponentThread(^{
                 [_dataRenderHandler createPage:instanceIdString contents:contents options:options data:data];
@@ -624,8 +621,6 @@ _Pragma("clang diagnostic pop") \
                 });
             }
         }
-        WX_MONITOR_INSTANCE_PERF_END(WXPTJSCreateInstance, [WXSDKManager instanceForID:instanceIdString]);
-        [sdkInstance.apmInstance onStage:KEY_PAGE_STAGES_LOAD_BUNDLE_END];
         return;
     }
 }
