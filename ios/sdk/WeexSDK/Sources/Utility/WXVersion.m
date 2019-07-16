9aa55d923dffaa54540c4ea88a25ca396d46fe35
renmin <wrmwindmill@gmail.com>
2019-07-16 15:36:33 +0800

    complete

--- ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
@@ -20,8 +20,8 @@
 #import "WXVersion.h"
 #import "WXDefine.h"
 
-static const char* WeexSDKBuildTime = "2019-07-16 06:07:56 UTC";
-static const unsigned long WeexSDKBuildTimestamp = 1563257276;
+static const char* WeexSDKBuildTime = "2019-07-16 07:08:34 UTC";
+static const unsigned long WeexSDKBuildTimestamp = 1563260914;
 
 NSString* GetWeexSDKVersion(void)
 {
