b02199ee44200627575407816c51a15305580ef0
renmin <wrmwindmill@gmail.com>
2019-08-08 16:19:21 +0800

    fix remaining oclint warning

--- ios/sdk/WeexSDK/Sources/Utility/WXUtility.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXUtility.m
@@ -391,7 +391,7 @@ CGFloat WXFloorPixelValue(CGFloat value)
 
 + (BOOL)isValidPoint:(CGPoint)point
 {
-    return !(isnan(point.x)) && !(isnan(point.y));
+    return !(isnan(point.x)) && !(isnan(point.y)); //!OCLint
 }
 
 + (NSError *)errorWithCode:(NSInteger)code message:(NSString *)message
