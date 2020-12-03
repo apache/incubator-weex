74e324a69a0f406fcc8e29ad9ce72b3c1c887114
剑客 <tianpeng.xtp@alibaba-inc.com>
2020-12-03 17:17:57 +0800

    使用声明方式兼容UIDatePicker

--- ios/sdk/WeexSDK/Sources/Module/WXPickerModule.m
+++ ios/sdk/WeexSDK/Sources/Module/WXPickerModule.m
@@ -24,6 +24,7 @@
 #import <UIKit/UIPickerView.h>
 #import <UIKit/UIDatePicker.h>
 #import <UIKit/UIKit.h>
+#import "WXLegacyAdapter.h"
 
 #define WXPickerHeight 266
 #define WXPickerToolBarHeight 44
@@ -432,11 +433,9 @@ WX_EXPORT_METHOD(@selector(pickTime:callback:))
 {
     self.callback = callback;
     self.datePicker = [[UIDatePicker alloc]init];
-#ifdef __IPHONE_13_4
     if (@available(iOS 13.4, *)) {
         self.datePicker.preferredDatePickerStyle = UIDatePickerStyleWheels;
     }
-#endif
     if (UIDatePickerModeDate == self.datePickerMode) {
         self.datePicker.datePickerMode = UIDatePickerModeDate;
         NSString *value = [WXConvert NSString:options[@"value"]];
---
+++ ios/sdk/WeexSDK/Sources/Utility/WXLegacyAdapter.h
@@ -0,0 +1,18 @@
+//
+//  WXLegacyAdapter.h
+//  WeexSDK
+//
+//  Created by txink on 2020/12/3.
+//  Copyright © 2020 taobao. All rights reserved.
+//
+
+#ifndef WXLegacyAdapter_h
+#define WXLegacyAdapter_h
+
+API_AVAILABLE(ios(13.4))
+@interface UIDatePicker (LegacyXcode)
+@property (nonatomic, readwrite, assign) UIDatePickerStyle preferredDatePickerStyle;
+@end
+
+
+#endif /* WXLegacyAdapter_h */
